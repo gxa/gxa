3dd81f1a8bb03c0470ccd839102f3f4e19c7e84c
mdylag <mdylag@2913f559-6b04-0410-9a09-c530ee9f5186>
2008-03-10 18:08:14 +0000

    Add libraries commons-cli, commons-colection, commons-configuration Created service layer:  uk.ac.ebi.ae3.indexbuilder.service.ConfigurationService                        uk.ac.ebi.ae3.indexbuilder.service.IndexBuilderservice Change structure of resources for the maven build (did not maven conf yet).
    git-svn-id: svn://bar.ebi.ac.uk/sandbox/branches/dev/ae3@4062 2913f559-6b04-0410-9a09-c530ee9f5186

---
+++ indexbuilder/src/test/java/uk/ac/ebi/ae3/indexbuilder/IndexBuilderTest.java
@@ -0,0 +1,81 @@
+/**
+ * 
+ */
+package uk.ac.ebi.ae3.indexbuilder;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author mdylag
+ *
+ */
+public class IndexBuilderTest
+{
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception
+	{
+	}
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception
+	{
+	}
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception
+	{
+	}
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception
+	{
+	}
+	
+	/**
+	 * Test method for {@link uk.ac.ebi.ae3.indexbuilder.IndexBuilder#parse(java.lang.String[])}.
+	 */
+	@Test
+	public void testParse()
+	{
+		
+		fail("Not yet implemented");
+	}
+	
+	/**
+	 * Test method for {@link uk.ac.ebi.ae3.indexbuilder.IndexBuilder#run()}.
+	 */
+	@Test
+	public void testRun()
+	{
+		fail("Not yet implemented");
+	}
+	
+	/**
+	 * Test method for {@link uk.ac.ebi.ae3.indexbuilder.IndexBuilder#main(java.lang.String[])}.
+	 */
+	@Test
+	public void testMain()
+	{
+		fail("Not yet implemented");
+	}
+	
+}
