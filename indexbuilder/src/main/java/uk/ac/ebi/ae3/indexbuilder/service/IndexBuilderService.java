2393d0647e00f871ed882c0f371f3051fabca6a2
mdylag <mdylag@2913f559-6b04-0410-9a09-c530ee9f5186>
2008-03-12 18:31:35 +0000

    Change index structure: Currently index stores fields: - from a idf file: TITLE,"Experiment Description",    "Person Last  Name","Person First Name",    "Experimental Design" - from	a SDFR file: SPECIE,"Array Design REF",    "Protocol REF","Characteristics[CellLine]",    "Factor Value [EF1](genotype)",    "Publication Title","Publication Author List",    "Publication Status","Publication Status Term Source REF"
    git-svn-id: svn://bar.ebi.ac.uk/sandbox/branches/dev/ae3@4118 2913f559-6b04-0410-9a09-c530ee9f5186

--- indexbuilder/src/main/java/uk/ac/ebi/ae3/indexbuilder/service/IndexBuilderService.java
+++ indexbuilder/src/main/java/uk/ac/ebi/ae3/indexbuilder/service/IndexBuilderService.java
@@ -32,8 +32,19 @@ import uk.ac.ebi.ae3.indexbuilder.utils.MageTabUtils;
 
 public class IndexBuilderService
 {
+	/** */
 	private ConfigurationService confService;
+	/** */
 	private static final Log log = LogFactory.getLog(IndexBuilderService.class);
+	/** */
+	private static final String ACCESION_NUMBER="exp_accession";
+	private static final String TITLE="Investigation Title";
+	private static final String SPECIE="Characteristics [Organism]";
+	private static final String[] idfFields={TITLE,"Experiment Description","Person Last Name","Person First Name","Experimental Design"};
+	private static final String[] sdrfFields={SPECIE,"Array Design REF","Protocol REF","Characteristics[CellLine]",
+											  "Factor Value [EF1](genotype)","Publication Title","Publication Author List","Publication Status","Publication Status Term Source REF"};
+
+	
 	/**
 	 * DOCUMENT ME
 	 * @param confService
@@ -70,36 +81,67 @@ public class IndexBuilderService
         MultiCore.getRegistry().shutdown();
     }
 	
+	/**
+	 * DOCUMENT ME
+	 * @param idfFile
+	 * @param solr
+	 * @throws IOException
+	 * @throws SolrServerException
+	 * @throws IndexBuilderException
+	 */
     private static void indexMageTab(File idfFile, SolrServer solr) throws IOException, SolrServerException, IndexBuilderException {
         MageTabParser mtp = MageTabParserFactory.getParser();
+        
+        //Parse IDF file
         MageTabDocument mtd = mtp.parseIDF(new FileReader(idfFile));
         
         SolrInputDocument doc = new SolrInputDocument();
 
-        addMageTabFields(doc, mtd.getFields());
+        addMageTabFields(doc, mtd.getFields(), idfFields);
         File sdrfFile = MageTabUtils.getSdrfFileForIdf(idfFile);
         //File sdrfFile = new File(idfFile.getAbsolutePath().replace(IndexBuilder.IDF_EXTENSION, IndexBuilder.SDRF_EXTENSION));
         if (sdrfFile.exists()) {
             log.info("Found " + sdrfFile.getAbsolutePath());
+            //Parse SDRF file
             MageTabDocument mtd_sdrf = mtp.parseSDRF(new FileReader(sdrfFile));
-            addMageTabFields(doc, mtd_sdrf.getFields());
+            addMageTabFields(doc, mtd_sdrf.getFields(), sdrfFields);
         }
 
-        doc.addField("exp_accession", idfFile.getName().replace(IndexBuilder.IDF_EXTENSION,""));
-        log.info("ALALALALALALALL" + idfFile.getName().replace(IndexBuilder.IDF_EXTENSION,""));
+        doc.addField(IndexBuilderService.ACCESION_NUMBER, idfFile.getName().replace(IndexBuilder.IDF_EXTENSION,""));
         UpdateResponse response = solr.add(doc);
     }
 
-    private static void addMageTabFields(SolrInputDocument doc, Map<String, List<String>> mtFields) {
+    /**
+     * DOCUMENT ME
+     * @param doc       - 
+     * @param mtFields  -
+     * @param idxfields - 
+     */
+    private static void addMageTabFields(SolrInputDocument doc, Map<String, List<String>> mtFields, String[] idxfields) {
         for (Map.Entry<String, List<String>> entry : mtFields.entrySet()) {
             String fieldName = entry.getKey();
             List<String> fieldValues = entry.getValue();
 
             for ( String val : fieldValues ) {
-                doc.addField(fieldName, val);
-                log.info(fieldName);
+            	if (existsInIndex(fieldName, idxfields))
+            	{
+            		doc.addField(fieldName, val);
+            	}
             }
         }
+    }
+    
+    /**
+     * 
+     */
+    private static boolean existsInIndex(final String field, final String[] idxFields)
+    {
+    	for (String val : idxFields)
+    	{
+    		if (val.equals(field))
+    			return true;
+    	}
+    	return false;
     }
 
 }
