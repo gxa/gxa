7aea63bbc03f04291e7f68a98a7ebae05812c2a1
iemam <iemam@2913f559-6b04-0410-9a09-c530ee9f5186>
2009-03-18 18:24:52 +0000

    test for getGeneIdentifier method in AtlasDAO
    git-svn-id: svn://bar.ebi.ac.uk/trunk/atlas@7043 2913f559-6b04-0410-9a09-c530ee9f5186

--- atlas-web/src/test/java/ae3/dao/AtlasDaoTest.java
+++ atlas-web/src/test/java/ae3/dao/AtlasDaoTest.java
@@ -3,12 +3,14 @@ package ae3.dao;
 import java.util.List;
 
 import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.common.SolrDocumentList;
 import org.junit.Test;
 
 import ae3.AtlasAbstractTest;
 import ae3.dao.AtlasDao;
 import ae3.dao.AtlasObjectNotFoundException;
 import ae3.model.AtlasExperiment;
+import ae3.model.AtlasGene;
 
 public class AtlasDaoTest extends AtlasAbstractTest
 {
@@ -54,6 +56,54 @@ public class AtlasDaoTest extends AtlasAbstractTest
 	public void test_getExperimentByAccession() throws AtlasObjectNotFoundException
 	{
 		AtlasExperiment exp=AtlasDao.getExperimentByAccession("E-MEXP-980");
+	}
+	
+	@Test
+	public void test_getGeneByIdentifier(){
+		boolean notFound_thrown = false;
+		boolean multi_thrown = false;
+		
+			
+		//Test normal gene query
+		try{
+			AtlasGene gene = AtlasDao.getGeneByIdentifier("ENSG00000066279");
+			assertNotNull(gene);
+		}catch (AtlasObjectNotFoundException ex){
+			notFound_thrown = true;
+		}catch (MultipleGeneException ex){
+			multi_thrown = true;
+		}
+		assertFalse(notFound_thrown);
+		assertFalse(multi_thrown);
+		
+		//Test multiple gene hit exception
+		multi_thrown = false;
+		notFound_thrown = false;
+		try{
+			AtlasGene gene2 = AtlasDao.getGeneByIdentifier("P08898");
+		}catch (MultipleGeneException ex){
+			multi_thrown = true;
+		}catch (AtlasObjectNotFoundException ex){
+			notFound_thrown = true;
+		}
+		assertTrue(multi_thrown);
+		assertFalse(notFound_thrown);
+		
+		//Test gene not found exception
+		notFound_thrown = false;
+		multi_thrown = false;
+		try{
+			AtlasGene gene2 = AtlasDao.getGeneByIdentifier("mysteriousGene");
+		}catch (AtlasObjectNotFoundException ex){
+			notFound_thrown = true;
+		}catch (MultipleGeneException ex){
+			multi_thrown = true;
+		}
+		assertTrue(notFound_thrown);
+		assertFalse(multi_thrown);
+
+		
+		
 	}
 	
 
