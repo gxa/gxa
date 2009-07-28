f45c2717863c2d181a0b2c1671ca81bb716ade3c
azorin <azorin@2913f559-6b04-0410-9a09-c530ee9f5186>
2009-07-28 10:00:37 +0000

    ListResult..
    git-svn-id: svn://bar.ebi.ac.uk/trunk/atlas@8249 2913f559-6b04-0410-9a09-c530ee9f5186

---
+++ atlas-web/src/main/java/ae3/model/ListResultRowExperiment.java
@@ -0,0 +1,82 @@
+package ae3.model;
+
+import uk.ac.ebi.ae3.indexbuilder.Expression;
+
+/**
+ * @author pashky
+ */
+public class ListResultRowExperiment {
+    private long experimentId;
+    private String experimentAccession;
+    private String experimentDescription;
+    private String experimentName;
+    private Expression updn;
+    private double pvalue;
+
+    /**
+     * Constructor
+     * @param experimentId experiment id
+     * @param experimentName experimnet name
+     * @param experimentAccession experiment accessment
+     * @param experimentDescription experimnet description
+     * @param pvalue p-value
+     * @param updn up or down
+     */
+    public ListResultRowExperiment(long experimentId, String experimentName, String experimentAccession, String experimentDescription,
+                                   double pvalue, Expression updn) {
+        this.experimentId = experimentId;
+        this.experimentAccession = experimentAccession;
+        this.experimentDescription = experimentDescription;
+        this.experimentName = experimentName;
+        this.pvalue = pvalue;
+        this.updn = updn;
+    }
+
+    /**
+     * Returns p-value
+     * @return p-value
+     */
+    public double getPvalue() {
+        return pvalue;
+    }
+
+    /**
+     * Returns experiment id
+     * @return experiment id
+     */
+    public long getExperimentId() {
+        return experimentId;
+    }
+
+    /**
+     * Return experiment accession
+     * @return experiment accession
+     */
+    public String getExperimentAccession() {
+        return experimentAccession;
+    }
+
+    /**
+     * Returns experiment description
+     * @return experiment description
+     */
+    public String getExperimentDescription() {
+        return experimentDescription;
+    }
+
+    /**
+     * Returns experiment name
+     * @return experiment name
+     */
+    public String getExperimentName() {
+        return experimentName;
+    }
+
+    /**
+     * Returns up or down
+     * @return UP or DOWN
+     */
+    public Expression getUpdn() {
+        return updn;
+    }
+}
