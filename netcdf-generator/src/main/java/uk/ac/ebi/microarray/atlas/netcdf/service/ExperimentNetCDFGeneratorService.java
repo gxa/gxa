253f8e8ab76a86ef9c84b3d20ac0fd77634aadb7
tburdett <tburdett@2913f559-6b04-0410-9a09-c530ee9f5186>
2009-09-29 19:46:59 +0000

    Atlas-standalone:  More stuff on ExperimentNetCDFCreator.  For now I'm just trying to replicate old functionality in a slightly cleaner way, and I'll do a thorough cleanup to make this into a better OO design if it all works.
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-external@9142 2913f559-6b04-0410-9a09-c530ee9f5186

--- netcdf-generator/src/main/java/uk/ac/ebi/microarray/atlas/netcdf/service/ExperimentNetCDFGeneratorService.java
+++ netcdf-generator/src/main/java/uk/ac/ebi/microarray/atlas/netcdf/service/ExperimentNetCDFGeneratorService.java
@@ -1,9 +1,6 @@
 package uk.ac.ebi.microarray.atlas.netcdf.service;
 
-import ucar.ma2.ArrayInt;
-import ucar.ma2.DataType;
-import ucar.ma2.IndexIterator;
-import ucar.ma2.InvalidRangeException;
+import ucar.ma2.*;
 import ucar.nc2.Dimension;
 import ucar.nc2.NetcdfFileWriteable;
 import uk.ac.ebi.microarray.atlas.dao.AtlasDAO;
@@ -141,6 +138,9 @@ public class ExperimentNetCDFGeneratorService
                 dataSlice.getExperiment().getAccession());
             writeGeneVariables(netCDF, genes);
 
+            // setup property parts of the netCDF
+            writePropertyVariables(netCDF, assayToSamples);
+
             // actually create the netCDF
             netCDF.create();
 
@@ -162,14 +162,13 @@ public class ExperimentNetCDFGeneratorService
             // write gene data
             writeGeneData(netCDF, genes);
 
+            // write property data
+            writePropertyData(netCDF, assayToSamples);
+
             // todo - still need to write matrices for...
             // BDC - DE/AS
             // PVAL - DE/uEFV
             // TSTAT - DE/uEFV
-            // EF - EF/EFV
-            // EFV - uEFV/EFV
-            // uEFV - uEFV/EFV
-            // uEFVnum - EF
 
             return true;
           }
@@ -376,5 +375,227 @@ public class ExperimentNetCDFGeneratorService
       asIter.setIntNext(gene.getGeneID());
     }
     netCDF.write("GN", gn);
+  }
+
+  private void writePropertyVariables(NetcdfFileWriteable netCDF,
+                                      Map<Assay, List<Sample>> assayToSamples) {
+    // build the mapping of property name to values
+    Map<String, List<String>> propertyNameToValues =
+        new HashMap<String, List<String>>();
+    Map<String, List<String>> samplePropertyNameToValues =
+        new HashMap<String, List<String>>();
+    for (Assay assay : assayToSamples.keySet()) {
+      for (Property prop : assay.getProperties()) {
+        if (propertyNameToValues.containsKey(prop.getName())) {
+          // add to existing
+          propertyNameToValues.get(prop.getName()).add(prop.getValue());
+        }
+        else {
+          // create a new list and add to that
+          List<String> propertyNames = new ArrayList<String>();
+          propertyNames.add(prop.getValue());
+          propertyNameToValues.put(prop.getName(), propertyNames);
+        }
+      }
+
+      for (Sample sample : assayToSamples.get(assay)) {
+        for (Property prop : sample.getProperties()) {
+          if (propertyNameToValues.containsKey(prop.getName())) {
+            // add to existing
+            propertyNameToValues.get(prop.getName()).add(prop.getValue());
+          }
+          else {
+            // create a new list and add to that
+            List<String> propertyNames = new ArrayList<String>();
+            propertyNames.add(prop.getValue());
+            propertyNameToValues.put(prop.getName(), propertyNames);
+          }
+
+          // also add sample properties to the separate map for some reason
+          if (samplePropertyNameToValues.containsKey(prop.getName())) {
+            samplePropertyNameToValues.get(prop.getName()).add(prop.getValue());
+          }
+          else {
+            List<String> propertyNames = new ArrayList<String>();
+            propertyNames.add(prop.getValue());
+            samplePropertyNameToValues.put(prop.getName(), propertyNames);
+          }
+        }
+      }
+    }
+
+    // we now have a list of all properties and values
+    // and a mapping of unique properties to the set of all observed values
+
+    // now stack up property slicing
+
+    // first up, EF - length, number of properties
+    Dimension efDimension =
+        netCDF.addDimension("EF", propertyNameToValues.keySet().size());
+    // next up, EFV length - this is equal to max number of values mapped to one property
+    int maxLength = 0;
+    for (List<String> propertyValues : propertyNameToValues.values()) {
+      if (propertyValues.size() > maxLength) {
+        maxLength = propertyValues.size();
+      }
+    }
+    Dimension efvDimension =
+        netCDF.addDimension("EFlen", maxLength);
+    // next, unique EFVs - actually this is only the same as our keyset
+    Dimension uefvDimension = netCDF.addDimension("uEFV", propertyNameToValues
+        .keySet().size()); // fixme - this is wrong, should be intersection of property and value not just the names again
+
+    // lookup assay dimension
+    Dimension assayDimension = netCDF.findDimension("AS");
+    // now add variables
+    netCDF.addVariable("EF", DataType.CHAR,
+                       new Dimension[]{efDimension, efvDimension});
+    netCDF.addVariable("EFV", DataType.CHAR,
+                       new Dimension[]{efDimension, assayDimension,
+                                       efvDimension});
+    netCDF.addVariable("uEFV", DataType.CHAR,
+                       new Dimension[]{uefvDimension, efvDimension});
+    netCDF.addVariable("uEFVnum", DataType.INT, new Dimension[]{efDimension});
+
+    // finally, do the same thing for sample properties
+    Dimension scDimension =
+        netCDF.addDimension("SC", samplePropertyNameToValues.keySet().size());
+    int maxSCLength = 0;
+    for (List<String> propertyValues : propertyNameToValues.values()) {
+      if (propertyValues.size() > maxSCLength) {
+        maxSCLength = propertyValues.size();
+      }
+    }
+    Dimension sclDimension =
+        netCDF.addDimension("SClen", maxSCLength);
+
+    // lookup sample dimension
+    Dimension sampleDimension = netCDF.findDimension("BS");
+    // and add variables
+    netCDF.addVariable("SC", DataType.CHAR,
+                       new Dimension[]{scDimension, sclDimension});
+    netCDF.addVariable("SCV", DataType.CHAR,
+                       new Dimension[]{scDimension, sampleDimension,
+                                       sclDimension});
+  }
+
+  private void writePropertyData(NetcdfFileWriteable netCDF,
+                                 Map<Assay, List<Sample>> assayToSamples)
+      throws IOException, InvalidRangeException {
+    // build the list of all properties, and the mapping of property name to values
+    Map<String, List<String>> propertyNameToValues =
+        new HashMap<String, List<String>>();
+    Map<String, List<String>> samplePropertyNameToValues =
+        new HashMap<String, List<String>>();
+    for (Assay assay : assayToSamples.keySet()) {
+      for (Property prop : assay.getProperties()) {
+        if (propertyNameToValues.containsKey(prop.getName())) {
+          // add to existing
+          propertyNameToValues.get(prop.getName()).add(prop.getValue());
+        }
+        else {
+          // create a new list and add to that
+          List<String> propertyNames = new ArrayList<String>();
+          propertyNames.add(prop.getValue());
+          propertyNameToValues.put(prop.getName(), propertyNames);
+        }
+      }
+
+      for (Sample sample : assayToSamples.get(assay)) {
+        for (Property prop : sample.getProperties()) {
+          if (propertyNameToValues.containsKey(prop.getName())) {
+            // add to existing
+            propertyNameToValues.get(prop.getName()).add(prop.getValue());
+          }
+          else {
+            // create a new list and add to that
+            List<String> propertyNames = new ArrayList<String>();
+            propertyNames.add(prop.getValue());
+            propertyNameToValues.put(prop.getName(), propertyNames);
+          }
+
+          // also add sample properties to the separate map for some reason
+          if (samplePropertyNameToValues.containsKey(prop.getName())) {
+            samplePropertyNameToValues.get(prop.getName()).add(prop.getValue());
+          }
+          else {
+            List<String> propertyNames = new ArrayList<String>();
+            propertyNames.add(prop.getValue());
+            samplePropertyNameToValues.put(prop.getName(), propertyNames);
+          }
+        }
+      }
+    }
+
+    // we now have a list of all properties and values
+    // and a mapping of unique properties to the set of all observed values
+
+    // use this data to write all the required property data
+    ArrayChar ef = new ArrayChar.D2(
+        netCDF.findDimension("EF").getLength(),
+        netCDF.findDimension("EFlen").getLength());
+    ArrayChar efv = new ArrayChar.D3(
+        netCDF.findDimension("EF").getLength(),
+        netCDF.findDimension("AS").getLength(),
+        netCDF.findDimension("EFlen").getLength());
+    int i = 0;
+    for (String propertyName : propertyNameToValues.keySet()) {
+      ef.setString(i, propertyName);
+
+      int j = 0;
+      for (String propertyValue : propertyNameToValues.get(propertyName)) {
+        efv.setString(efv.getIndex().set(i, j), propertyValue);
+        j++;
+      }
+      i++;
+    }
+
+    ArrayChar uefv = new ArrayChar.D2(
+        netCDF.findDimension("uEFV").getLength(),
+        netCDF.findDimension("EFlen").getLength());
+    int k = 0;
+    for (String propertyName : propertyNameToValues
+        .keySet()) { // fixme: again, this is wrong, need intersection of property/property value not just properties again
+      uefv.setString(k, propertyName);
+      k++;
+    }
+
+    ArrayInt uefvNum = new ArrayInt.D1(
+        netCDF.findDimension("EF").getLength());
+    int l = 0;
+    for (String propertyName : propertyNameToValues
+        .keySet()) { // fixme: again, this is wrong, need intersection of property/property value not just properties again
+      uefvNum.setInt(efv.getIndex().set(l),
+                     propertyNameToValues.get(propertyName).size());
+      k++;
+    }
+
+    // finally write sample data
+    ArrayChar sc = new ArrayChar.D2(
+        netCDF.findDimension("SC").getLength(),
+        netCDF.findDimension("SClen").getLength());
+    ArrayChar scv = new ArrayChar.D3(
+        netCDF.findDimension("SC").getLength(),
+        netCDF.findDimension("BS").getLength(),
+        netCDF.findDimension("SClen").getLength());
+    int x = 0;
+    for (String propertyName : propertyNameToValues.keySet()) {
+      ef.setString(x, propertyName);
+
+      int y = 0;
+      for (String propertyValue : propertyNameToValues.get(propertyName)) {
+        efv.setString(efv.getIndex().set(x, y), propertyValue);
+        y++;
+      }
+      x++;
+    }
+
+
+    netCDF.write("EF", ef);
+    netCDF.write("EFV", efv);
+    netCDF.write("uEFV", uefv);
+    netCDF.write("uEFVnum", uefvNum);
+    netCDF.write("SC", sc);
+    netCDF.write("SCV", scv);
   }
 }
