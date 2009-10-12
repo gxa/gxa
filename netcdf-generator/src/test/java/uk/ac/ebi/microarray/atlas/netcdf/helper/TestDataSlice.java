6310ac859414acb1dc935a3c8d5513c03bf2aab2
tburdett <tburdett@2913f559-6b04-0410-9a09-c530ee9f5186>
2009-10-12 17:00:53 +0000

    Atlas-standalone: More unit testing and minor bug fixing.  Tests for index builder are complete for everything except gene counts, because the table join to do gene counts is complex and not complete yet.  First test for netcdfs added.
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone@9297 2913f559-6b04-0410-9a09-c530ee9f5186

---
+++ netcdf-generator/src/test/java/uk/ac/ebi/microarray/atlas/netcdf/helper/TestDataSlice.java
@@ -0,0 +1,118 @@
+package uk.ac.ebi.microarray.atlas.netcdf.helper;
+
+import junit.framework.TestCase;
+import uk.ac.ebi.microarray.atlas.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Javadocs go here.
+ *
+ * @author Junit Generation Plugin for Maven, written by Tony Burdett
+ * @date 07-10-2009
+ */
+public class TestDataSlice extends TestCase {
+  private DataSlice dataSlice;
+
+  private Experiment experiment;
+  private ArrayDesign arrayDesign;
+
+  public void setUp() {
+    experiment = new Experiment();
+    arrayDesign = new ArrayDesign();
+
+    dataSlice = new DataSlice(experiment, arrayDesign);
+  }
+
+  public void tearDown() {
+    dataSlice = null;
+
+    arrayDesign = null;
+    experiment = null;
+  }
+
+  public void testStoreGenes() {
+    Gene gene1 = new Gene();
+    Gene gene2 = new Gene();
+    List<Gene> storage = new ArrayList<Gene>();
+    storage.add(gene1);
+    storage.add(gene2);
+    dataSlice.storeGenes(storage);
+
+    // now get genes
+    assertSame("Wrong number of genes", dataSlice.getGenes().size(), 2);
+  }
+
+  public void testStoreAssays() {
+    Assay ass1 = new Assay();
+    Assay ass2 = new Assay();
+    List<Assay> storage = new ArrayList<Assay>();
+    storage.add(ass1);
+    storage.add(ass2);
+    dataSlice.storeAssays(storage);
+
+    // now get assays
+    assertSame("Wrong number of assays", dataSlice.getAssays().size(), 2);
+  }
+
+  public void testStoreSamplesAssociatedWithAssay() {
+    // store an assay
+    Assay ass1 = new Assay();
+    ass1.setAccession("test-assay-1");
+    List<Assay> storage = new ArrayList<Assay>();
+    storage.add(ass1);
+    dataSlice.storeAssays(storage);
+
+    // store a sample
+    Sample sample1 = new Sample();
+    Sample sample2 = new Sample();
+    List<Sample> storage2 = new ArrayList<Sample>();
+    storage2.add(sample1);
+    storage2.add(sample2);
+    dataSlice.storeSamplesAssociatedWithAssay(ass1.getAccession(), storage2);
+
+    // check we get 2 samples back
+    assertSame(
+        "Wrong number of assay-associated samples",
+        dataSlice.getSamplesAssociatedWithAssay(ass1.getAccession()).size(),
+        2);
+  }
+
+  public void testStoreDesignElementIDs() {
+    Integer int1 = 1;
+    Integer int2 = 2;
+    List<Integer> storage = new ArrayList<Integer>();
+    storage.add(int1);
+    storage.add(int2);
+    dataSlice.storeDesignElementIDs(storage);
+
+    // now get design elements
+    assertSame("Wrong number of design elements", dataSlice.getDesignElementIDs().size(), 2);
+  }
+
+  public void testReset() {
+    Integer int1 = 1;
+    Integer int2 = 2;
+    List<Integer> storage = new ArrayList<Integer>();
+    storage.add(int1);
+    storage.add(int2);
+    dataSlice.storeDesignElementIDs(storage);
+
+    Assay ass1 = new Assay();
+    Assay ass2 = new Assay();
+    List<Assay> storage2 = new ArrayList<Assay>();
+    storage2.add(ass1);
+    storage2.add(ass2);
+    dataSlice.storeAssays(storage2);
+
+    dataSlice.reset();
+
+    assertNull("Assays was not null", dataSlice.getAssays());
+    assertSame("Sample/Assay association was not empty", dataSlice.getSamplesAssociatedWithAssay("test-1").size(), 0);
+    assertSame("Assays to sample mapping was not empty", dataSlice.getAssayToSampleMapping().size(), 0);
+    assertNull("Design Elements was not null", dataSlice.getDesignElementIDs());
+    assertNull("Genes was not null", dataSlice.getGenes());
+    assertNull("Samples was not null", dataSlice.getSamples());
+  }
+}
