b090e7d4b72c34090e2f47cad2479226b9ca75d8
Tony Burdett <tburdett@ebi.ac.uk>
2010-01-12 17:28:25 +0000

    Atlas Standalone: Logging cleanup, extra test-resources, and fixing unit tests for broken cases.  Better than it was!
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone@10383 2913f559-6b04-0410-9a09-c530ee9f5186

--- atlas-loader/src/test/java/uk/ac/ebi/gxa/loader/handler/sdrf/TestAtlasLoadingHybridizationHandler.java
+++ atlas-loader/src/test/java/uk/ac/ebi/gxa/loader/handler/sdrf/TestAtlasLoadingHybridizationHandler.java
@@ -15,8 +15,10 @@ import uk.ac.ebi.gxa.loader.cache.AtlasLoadCache;
 import uk.ac.ebi.gxa.loader.cache.AtlasLoadCacheRegistry;
 import uk.ac.ebi.gxa.loader.handler.idf.AtlasLoadingAccessionHandler;
 import uk.ac.ebi.microarray.atlas.model.Assay;
+import uk.ac.ebi.microarray.atlas.model.Property;
 
 import java.net.URL;
+import java.util.List;
 
 /**
  * Javadocs go here.
@@ -105,6 +107,31 @@ public class TestAtlasLoadingHybridizationHandler extends TestCase {
         for (Assay assay : cache.fetchAllAssays()) {
             String acc = assay.getAccession();
             assertNotNull("Sample acc is null", acc);
+        }
+
+        // test properties of each assay
+        for (Assay assay : cache.fetchAllAssays()) {
+            List<Property> props = assay.getProperties();
+
+            // should have one property, organism
+            assertNotNull("Assay " + assay.getAccession() + " properties list is null", props);
+            assertEquals("More than one property observed for assay " + assay.getAccession() +
+                    ", should be Factor Value[Ecotype] only", 1, props.size());
+
+            assertEquals("Property name is not 'Ecotype'", "Ecotype", props.get(0).getName());
+
+            // test some property values at random
+            if (assay.getAccession().equals("E-GEOD-3790::hybridizationname::11 CN A")) {
+                assertEquals("Property value should be 'Cape Verde Islands'", "Cape Verde Islands", props.get(0).getValue());
+            }
+
+            if (assay.getAccession().equals("E-GEOD-3790::hybridizationname::81 CB A")) {
+                assertEquals("Property value should be 'Vancouver-0'", "Vancouver-0", props.get(0).getValue());
+            }
+
+            if (assay.getAccession().equals("E-GEOD-3790::hybridizationname::106 FC BA9  A")) {
+                assertEquals("Property value should be 'Shahdara'", "Shahdara", props.get(0).getValue());
+            }
         }
     }
 }
