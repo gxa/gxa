bd8fa80d100e319e1fa22c590b701fd5e5005084
azorin <azorin@2913f559-6b04-0410-9a09-c530ee9f5186>
2010-01-13 11:59:56 +0000

    rename "pvalue" to ExpressionAnalyticsStatistic, remove DesignElementID from it. (if keeping DesignElementID, why not PropertyValueID ?)
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone@10395 2913f559-6b04-0410-9a09-c530ee9f5186

--- atlas-model/src/main/java/uk/ac/ebi/microarray/atlas/model/ExpressionAnalyticsStatistic.java
+++ atlas-model/src/main/java/uk/ac/ebi/microarray/atlas/model/ExpressionAnalyticsStatistic.java
@@ -7,17 +7,9 @@ package uk.ac.ebi.microarray.atlas.model;
  * Time: 11:07:55 AM
  * To change this template use File | Settings | File Templates.
  */
-public class Pvalue {
-    private int designElementId;
+public class ExpressionAnalyticsStatistic {
     private double pvalue;
     private double tstat;
-
-    public int getDesignElementId(){
-        return designElementId;
-    }
-    public void setDesignElementId(int DesignElementId){
-        this.designElementId = DesignElementId;
-    }
 
     public double getPvalue(){
         return pvalue;
