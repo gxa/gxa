193d51f464d5c15462039d85d2411b2a69d34b13
Pavel Kurnosov <pashky@ebi.ac.uk>
2010-01-13 11:37:20 +0000

    - Finally working index update notification - A bit more of logging
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone@10393 2913f559-6b04-0410-9a09-c530ee9f5186

--- indexbuilder/src/main/java/uk/ac/ebi/gxa/index/builder/IndexUpdateHandler.java
+++ indexbuilder/src/main/java/uk/ac/ebi/gxa/index/builder/IndexUpdateHandler.java
@@ -1,9 +1,11 @@
 package uk.ac.ebi.gxa.index.builder;
 
+import uk.ac.ebi.gxa.index.builder.listener.IndexBuilderEvent;
+
 /**
  * Interface for index update call-backs
  * @author pashky
  */
 public interface IndexUpdateHandler {
-    void onIndexUpdate(IndexBuilder builder);
+    void onIndexUpdate(IndexBuilder builder, IndexBuilderEvent event);
 }
