ba76a463c1f4d0b07f294d004444b1cae72ecda5
Andrey Zorin <azorin@ebi.ac.uk>
2010-02-02 09:40:58 +0000

    remove UnknownAccessionThreshold
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone@10689 2913f559-6b04-0410-9a09-c530ee9f5186

--- sql/PKG_ATLASUPG.sql
+++ sql/PKG_ATLASUPG.sql
@@ -22,6 +22,9 @@ CREATE OR REPLACE PACKAGE ATLASUPG IS
   PROCEDURE LoadAssayOntology;
   PROCEDURE LoadExpressionAnalytics;
   
+  PROCEDURE FixSourceMapping;
+  PROCEDURE FixFactorValue;
+  
   PROCEDURE Load;
   PROCEDURE Clean;
 END ATLASUPG;
@@ -504,6 +507,9 @@ BEGIN
 
   LoadExpressionAnalytics();
   dbms_output.put_line('expression analytics: ' || CountRows('a2_ExpressionAnalytics') || ' in ' || ElapsedSec());  
+  
+  FixSourceMapping();
+  FixFactorValue();
 
   commit;
 END;
@@ -535,8 +541,55 @@ BEGIN
   Delete from a2_Property                            
   where PropertyID = (select PropertyID from a2_Property where Name = 'ALL');
   
-  rollback;
+  commit;
 
+END;
+
+PROCEDURE FixSourceMapping
+AS
+BEGIN
+
+EXECUTE IMMEDIATE 'create table tmp_SourceMapping(SampleOntologyID int, SamplePropertyValueID int)'; 
+EXECUTE IMMEDIATE 'create table tmp_SourceMappingAssay(AssayOntologyID int, AssayPropertyValueID int)'; 
+
+EXECUTE IMMEDIATE 'Insert into tmp_SourceMapping(SampleOntologyID, SamplePropertyValueID)
+Select sa.SampleOntologyID, spv.SamplePropertyValueID
+from a2_SampleOntology sa
+join a2_OntologyTerm ot on ot.OntologyTermID = sa.OntologyTermID
+join ontology_annotation@dwprd oa on oa.Sample_ID_key = sa.SampleID and oa.Accession = ot.accession
+join a2_PropertyValue pv on pv.name = ot.orig_value
+join a2_property p on p.propertyid = pv.PropertyID and p.ae1tablename_sample = oa.ORIG_VALUE_SRC
+join a2_samplepropertyvalue spv on spv.SampleID = sa.SampleID and spv.propertyvalueid = pv.propertyvalueid';
+
+EXECUTE IMMEDIATE 'Insert into tmp_SourceMappingAssay(AssayOntologyID, AssayPropertyValueID)
+Select sa.AssayOntologyID, MIN(spv.AssayPropertyValueID)
+from a2_AssayOntology sa
+join a2_OntologyTerm ot on ot.OntologyTermID = sa.OntologyTermID
+join ontology_annotation@dwprd oa on oa.Assay_ID_key = sa.AssayID and oa.Accession = ot.accession
+join a2_PropertyValue pv on pv.name = ot.orig_value
+join a2_property p on p.propertyid = pv.PropertyID and p.ae1tablename_assay = oa.ORIG_VALUE_SRC
+join a2_Assaypropertyvalue spv on spv.AssayID = sa.AssayID and spv.propertyvalueid = pv.propertyvalueid
+group by sa.AssayOntologyID';
+
+EXECUTE IMMEDIATE 'create index IDX_tmp_AssayOntologyID on tmp_SourceMappingAssay(AssayOntologyID)'; 
+EXECUTE IMMEDIATE 'create index IDX_tmp_SampleOntologyID on tmp_SourceMapping(SampleOntologyID)'; 
+
+EXECUTE IMMEDIATE 'update a2_SampleOntology set SourceMapping = (select MAX(SamplePropertyValueID) 
+from tmp_SourceMapping t where t.sampleontologyid = a2_SampleOntology.sampleontologyid)';
+
+EXECUTE IMMEDIATE 'update a2_AssayOntology set SourceMapping = (select MAX(AssayPropertyValueID) 
+from tmp_SourceMappingAssay t where t.assayontologyid = a2_AssayOntology.Assayontologyid)';
+
+EXECUTE IMMEDIATE 'drop table tmp_SourceMapping';
+EXECUTE IMMEDIATE 'drop table tmp_SourceMappingAssay';
+
+END;
+
+PROCEDURE FixFactorValue
+AS
+BEGIN
+  UPDATE a2_AssayPropertyValue set IsFactorValue = 1 where 1=1;
+  COMMIT;
 END;
   
 END;
