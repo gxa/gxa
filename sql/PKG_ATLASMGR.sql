bd7d2e4a6368aa7de6202a24108a90f6767af877
Andrey Zorin <azorin@ebi.ac.uk>
2010-02-16 16:10:54 +0000

    missed FK
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone@10881 2913f559-6b04-0410-9a09-c530ee9f5186

--- sql/PKG_ATLASMGR.sql
+++ sql/PKG_ATLASMGR.sql
@@ -3,6 +3,7 @@ CREATE OR REPLACE PACKAGE ATLASMGR IS
   PROCEDURE EnableConstraints;
   PROCEDURE DisableTriggers;
   PROCEDURE EnableTriggers;
+  PROCEDURE RebuildSequence;
 END ATLASMGR;
 /
 
@@ -34,6 +35,8 @@ begin
 for rec in c1
  loop
     q := 'ALTER TABLE ' || rec.TABLE_NAME  || ' ENABLE CONSTRAINT ' || rec.CONSTRAINT_NAME;
+    
+    dbms_output.put_line(q);
     
     EXECUTE IMMEDIATE q;
  end loop;
@@ -70,6 +73,58 @@ for rec in c1
  end loop;
 END;
 --------------------------------------------------------------------------------
+procedure RebuildSequence
+AS
+ cursor c1 is select SEQUENCE_NAME, LAST_NUMBER from user_sequences;
+ q varchar2(8000);
+ TABLE_NAME varchar2(255);
+ PK_NAME varchar2(255);
+ MaxID int;
+ Delta int;
+ NewID int;
+begin 
+for rec in c1
+ loop
+    Select REPLACE(rec.SEQUENCE_NAME, '_SEQ','') into TABLE_NAME from dual;
+    
+    select c.COLUMN_NAME into PK_NAME 
+    from user_tab_columns c
+    join user_constraints k on k.table_name = c.table_name
+    join user_indexes i on i.INDEX_NAME = k.INDEX_NAME and i.TABLE_NAME = k.TABLE_NAME 
+    join user_ind_columns ic on ic.INDEX_NAME = i.INDEX_NAME and ic.TABLE_NAME = i.TABLE_NAME
+    where ic.COLUMN_NAME = c.COLUMN_NAME
+    and k.constraint_type ='P'
+    and k.TABLE_NAME = REBUILDSEQUENCE.TABLE_NAME;
+    
+    q := 'SELECT MAX($PK_NAME) FROM $TABLE_NAME';
+    q := REPLACE(q,'$PK_NAME',PK_NAME);
+    q := REPLACE(q,'$TABLE_NAME',TABLE_NAME);
+    
+    dbms_output.put_line(q);
+    Execute immediate q INTO MaxID; 
+
+    Delta := MaxID - rec.LAST_NUMBER;
+    
+    if (Delta > 0) then
+    q := 'ALTER SEQUENCE $SEQUENCE_NAME INCREMENT BY $DELTA';
+    q := REPLACE(q,'$SEQUENCE_NAME',rec.SEQUENCE_NAME); 
+    q := REPLACE(q,'$DELTA',DELTA);
+    dbms_output.put_line(q);
+    Execute immediate q;
+    
+    q := 'SELECT $SEQUENCE_NAME.nextval FROM dual';
+    q := REPLACE(q,'$SEQUENCE_NAME',rec.SEQUENCE_NAME); 
+    dbms_output.put_line(q);
+    Execute immediate q into NewID; --!!not called w/o INTO!!
+    
+    q := 'ALTER SEQUENCE $SEQUENCE_NAME INCREMENT BY 1';
+    q := REPLACE(q,'$SEQUENCE_NAME',rec.SEQUENCE_NAME); 
+    dbms_output.put_line(q);
+    Execute immediate q;
+    
+    end if;
+ end loop;
+END;
 
 END;
 /
