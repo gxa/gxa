74281153cf334f5724d8511881a4aa49aba4925d
pashky <pashky@2913f559-6b04-0410-9a09-c530ee9f5186>
2010-02-23 17:13:36 +0000

    Nicer AJAX exception display (do not show windows of hundreds lines)
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone@10946 2913f559-6b04-0410-9a09-c530ee9f5186

--- atlas-web/src/main/java/uk/ac/ebi/gxa/requesthandlers/base/result/ErrorResult.java
+++ atlas-web/src/main/java/uk/ac/ebi/gxa/requesthandlers/base/result/ErrorResult.java
@@ -3,6 +3,11 @@ package uk.ac.ebi.gxa.requesthandlers.base.result;
 import uk.ac.ebi.gxa.requesthandlers.base.restutil.RestOut;
 import uk.ac.ebi.gxa.requesthandlers.base.restutil.RestOut;
 
+import java.io.StringWriter;
+import java.io.PrintWriter;
+
+import org.apache.commons.lang.StringUtils;
+
 /**
  * @author pashky
 */
@@ -11,6 +16,22 @@ public class ErrorResult {
 
     public ErrorResult(String error) {
         this.error = error;
+    }
+
+    private static String exceptionToString(Exception e) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw, true);
+        e.printStackTrace(pw);
+        pw.flush();
+        sw.flush();
+        String[] strings = sw.getBuffer().toString().split("(\r?\n)+");
+        return StringUtils.join(strings, '\n', 0, strings.length > 10 ? 10 : strings.length)
+                + (strings.length > 10 ? "\n..." : "");
+    }
+
+
+    public ErrorResult(Exception e) {
+        this.error = "Exception occured: " + exceptionToString(e);
     }
 
     public @RestOut
