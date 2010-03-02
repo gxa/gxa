30f81e5c84d4da8389919371756e543dbaae8c1a
Tony Burdett <tburdett@ebi.ac.uk>
2010-03-02 15:31:27 +0000

    Atlas-index-api: Updates to make sure we can work with the fluxion-utils 1.0-alpha module.
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone@11072 2913f559-6b04-0410-9a09-c530ee9f5186

--- atlas-index-api/src/main/java/uk/ac/ebi/gxa/efo/Loader.java
+++ atlas-index-api/src/main/java/uk/ac/ebi/gxa/efo/Loader.java
@@ -25,6 +25,7 @@ package uk.ac.ebi.gxa.efo;
 import net.sourceforge.fluxion.utils.OWLTransformationException;
 import net.sourceforge.fluxion.utils.OWLUtils;
 import net.sourceforge.fluxion.utils.ReasonerSession;
+import net.sourceforge.fluxion.utils.ReasonerSessionManager;
 import org.semanticweb.owl.apibinding.OWLManager;
 import org.semanticweb.owl.inference.OWLReasoner;
 import org.semanticweb.owl.inference.OWLReasonerException;
@@ -39,16 +40,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
-     * Ontology loader class reading OWL files
+ * Ontology loader class reading OWL files
  */
 class Loader {
     final private Logger log = LoggerFactory.getLogger(getClass());
     private OWLOntology ontology;
-    private OWLReasoner reasoner;
-    private Map<String,EfoNode> efomap;
+//    private OWLReasoner reasoner;
+    private Map<String, EfoNode> efomap;
 
-    Loader()
-    {
+    Loader() {
 
     }
 
@@ -60,13 +60,17 @@ class Loader {
         public void visit(OWLConstantAnnotation annotation) {
             if (annotation.isLabel()) {
                 OWLConstant c = annotation.getAnnotationValue();
-                if(term == null)
+                if (term == null) {
                     term = c.getLiteral();
-            } else if(annotation.getAnnotationURI().toString().contains("branch_class")) {
+                }
+            }
+            else if (annotation.getAnnotationURI().toString().contains("branch_class")) {
                 branchRoot = Boolean.valueOf(annotation.getAnnotationValue().getLiteral());
-            } else if(annotation.getAnnotationURI().toString().contains("organizational_class")) {
+            }
+            else if (annotation.getAnnotationURI().toString().contains("organizational_class")) {
                 organizational = Boolean.valueOf(annotation.getAnnotationValue().getLiteral());
-            } else if(annotation.getAnnotationURI().toString().contains("ArrayExpress_label")) {
+            }
+            else if (annotation.getAnnotationURI().toString().contains("ArrayExpress_label")) {
                 term = annotation.getAnnotationValue().getLiteral();
             }
         }
@@ -90,16 +94,18 @@ class Loader {
 
     /**
      * Loads ontology into map id -> internal node implementation
+     *
      * @param efo EFO to load into
      * @param uri URI to load ontology from
      */
     void load(Efo efo, URI uri) {
         OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
         try {
-            if(uri.getScheme().equals("resource")) {
+            if (uri.getScheme().equals("resource")) {
                 try {
                     uri = getClass().getClassLoader().getResource(uri.getSchemeSpecificPart()).toURI();
-                } catch (URISyntaxException e) {
+                }
+                catch (URISyntaxException e) {
                     throw new RuntimeException("Can't get resource URI for " + uri);
                 }
             }
@@ -107,18 +113,19 @@ class Loader {
             ontology = manager.loadOntologyFromPhysicalURI(uri);
 
             efo.version = "unknown";
-            
+
             StringBuilder versionInfo = new StringBuilder();
-            for(OWLAnnotationAxiom annotation : ontology.getAnnotations(ontology)) {
+            for (OWLAnnotationAxiom annotation : ontology.getAnnotations(ontology)) {
                 OWLAnnotation a = annotation.getAnnotation();
-                if(a.getAnnotationURI().toString().contains("versionInfo")) {
+                if (a.getAnnotationURI().toString().contains("versionInfo")) {
                     String value = a.getAnnotationValueAsConstant().getLiteral();
                     Matcher m = Pattern.compile(".*?(\\d+(\\.\\d+)+).*").matcher(value);
-                    if(m.matches()) {
+                    if (m.matches()) {
                         efo.version = m.group(1);
                     }
-                    if(versionInfo.length() > 0)
+                    if (versionInfo.length() > 0) {
                         versionInfo.append(" ");
+                    }
                     versionInfo.append(value);
                 }
             }
@@ -127,32 +134,35 @@ class Loader {
 
             log.info("EFO version " + efo.version + " (" + efo.versionInfo + ")");
 
-        } catch(OWLOntologyCreationException e) {
+        }
+        catch (OWLOntologyCreationException e) {
             throw new RuntimeException("Can't load EF Ontology", e);
         }
 
-        ReasonerSession session = OWLUtils.getReasonerSession(ontology);
+        // acquire a reasoner session and use fluxion utils to build the partonomy
+        ReasonerSession session = ReasonerSessionManager.createManager().acquireReasonerSession(ontology);
         try {
+            // first, load each class
             this.efomap = efo.efomap;
-
-            reasoner = session.getReasoner();
-            for(OWLClass cls : ontology.getReferencedClasses()) {
-                loadClass(cls);
+            for (OWLClass cls : ontology.getReferencedClasses()) {
+                loadClass(session.getReasoner(), cls);
             }
-        } catch(OWLReasonerException e) {
+
+            // trhen build part-of map
+            log.info("Building part-of map");
+            buildPartOfMap(session);
+        }
+        catch (OWLReasonerException e) {
             throw new RuntimeException(e);
-        }  finally {
+        }
+        finally {
             session.releaseSession();
         }
 
-        log.info("Building part-of map");
-        buildPartOfMap();
-
         log.info("Loading ontology done");
-      }
+    }
 
-    private OWLObjectProperty getProperty(String propertyName)
-    {
+    private OWLObjectProperty getProperty(String propertyName) {
         OWLObjectProperty result = null;
         for (OWLObjectProperty prpt : ontology.getReferencedObjectProperties()) {
             if (prpt.toString().equals(propertyName)) {
@@ -163,29 +173,30 @@ class Loader {
         return result;
     }
 
-    private void buildPartOfMap()
-    {
+    private void buildPartOfMap(ReasonerSession session) {
         OWLObjectProperty partOfProperty = getProperty("part_of");
         if (partOfProperty != null) {
             for (OWLClass cls : ontology.getReferencedClasses()) {
                 String partId = getId(cls);
                 try {
-                    Set<OWLRestriction> owlRestrictions = OWLUtils.keep(ontology, cls, partOfProperty);
+                    Set<OWLRestriction> owlRestrictions = OWLUtils.keep(session, ontology, cls, partOfProperty);
                     for (OWLRestriction restriction : owlRestrictions) {
-                        for (OWLClass parent : OWLUtils.getReferencedClasses(restriction)) {
+                        for (OWLClass parent : OWLUtils.getReferencedClasses(session, restriction)) {
                             String parentId = getId(parent);
-                            if (parentId.equals(partId))
+                            if (parentId.equals(partId)) {
                                 continue;
+                            }
 
                             EfoNode parentNode = efomap.get(parentId);
                             EfoNode node = efomap.get(partId);
-                            if(parentNode != null && node != null) {
+                            if (parentNode != null && node != null) {
                                 parentNode.children.add(node);
                                 node.parents.add(parentNode);
                             }
                         }
                     }
-                } catch (OWLTransformationException e1) {
+                }
+                catch (OWLTransformationException e1) {
                     throw new RuntimeException(e1);
                 }
             }
@@ -196,13 +207,13 @@ class Loader {
         return cls.getURI().toString().replaceAll("^.*?([^#/=?]+)$", "$1");
     }
 
-    private Collection<EfoNode> loadClass(OWLClass cls) throws OWLReasonerException {
-        if(reasoner.isSatisfiable(cls)) {
+    private Collection<EfoNode> loadClass(OWLReasoner reasoner, OWLClass cls) throws OWLReasonerException {
+        if (reasoner.isSatisfiable(cls)) {
             String id = getId(cls);
             EfoNode en = efomap.get(id);
-            if(en == null) {
+            if (en == null) {
                 ClassAnnoVisitor cannov = new ClassAnnoVisitor();
-                for(OWLAnnotation annotation : cls.getAnnotations(ontology)) {
+                for (OWLAnnotation annotation : cls.getAnnotations(ontology)) {
                     annotation.accept(cannov);
                 }
                 String term = cannov.getTerm();
@@ -212,19 +223,22 @@ class Loader {
                 for (Set<OWLClass> setOfClasses : children) {
                     for (OWLClass child : setOfClasses) {
                         if (!child.equals(cls)) {
-                            Collection<EfoNode> cnc = loadClass(child);
-                            for(EfoNode cn : cnc) {
+                            Collection<EfoNode> cnc = loadClass(reasoner, child);
+                            for (EfoNode cn : cnc) {
                                 en.children.add(cn);
-                                if(!cannov.isOrganizational())
+                                if (!cannov.isOrganizational()) {
                                     cn.parents.add(en);
+                                }
                             }
                         }
                     }
                 }
-                if(cannov.isOrganizational())
+                if (cannov.isOrganizational()) {
                     return en.children;
-                else
+                }
+                else {
                     efomap.put(id, en);
+                }
             }
             return Collections.singletonList(en);
         }
