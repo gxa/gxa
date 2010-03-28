d9c7b6479939c0f0a8df7310e47cc29b8138d0b8
Misha Kapushesky <ostolop@ebi.ac.uk>
2010-03-28 19:23:15 +0000

    - rename atlasdao -> atlassolrdao
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone@11569 2913f559-6b04-0410-9a09-c530ee9f5186

---
+++ atlas-web/src/test/java/ae3/dao/TestAtlasSolrDAO.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2008-2010 Microarray Informatics Team, EMBL-European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * For further details of the Gene Expression Atlas project, including source code,
+ * downloads and documentation, please see:
+ *
+ * http://gxa.github.com/gxa
+ */
+
+package ae3.dao;
+
+import ae3.model.AtlasExperiment;
+import ae3.model.AtlasGene;
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
+import uk.ac.ebi.gxa.index.AbstractOnceIndexTest;
+
+import java.util.List;
+
+public class TestAtlasSolrDAO extends AbstractOnceIndexTest
+{
+    private AtlasSolrDAO atlasSolrDAO;
+
+    @Before
+    public void initDao() {
+        atlasSolrDAO = new AtlasSolrDAO();
+        atlasSolrDAO.setSolrServerAtlas(new EmbeddedSolrServer(getContainer(), "atlas"));
+        atlasSolrDAO.setSolrServerExpt(new EmbeddedSolrServer(getContainer(), "expt"));
+    }
+
+    @Test
+    public void testGetAtlasGene() {
+        AtlasSolrDAO.AtlasGeneResult atlasGene = atlasSolrDAO.getGeneByIdentifier("ENSG00000121075");
+        assertNotNull(atlasGene);
+        assertTrue(atlasGene.isFound());
+        assertFalse(atlasGene.isMulti());
+        assertNotNull(atlasGene.getGene());
+        assertTrue(atlasGene.getGene().getGeneName().equals("TBX4"));
+    }
+
+    @Test
+    public void testRetrieveOrthoGenes() {
+        AtlasSolrDAO.AtlasGeneResult result = atlasSolrDAO.getGeneByIdentifier("ENSG00000121075");
+        assertNotNull(result);
+        assertTrue(result.isFound());
+        assertFalse(result.isMulti());
+        assertNotNull(result.getGene());
+
+        AtlasGene atlasGene = result.getGene();
+        assertFalse(atlasGene.getOrthologs().isEmpty());
+
+        List<AtlasGene> orthos = atlasSolrDAO.getOrthoGenes(atlasGene);
+
+        //Test successful retrieval of gene documents from the index corresponding to the gene's list of orthologs
+        assertNotNull(orthos);
+
+        for (AtlasGene ortho: orthos){
+            String orthoid = ortho.getGeneIdentifier();
+            //Test retrieved gene documents retrieved to match those in the ortholog list of the gene
+            assertTrue("Gene mismatch between gene ortholog ids and the corresponding retrieved genes from index",
+                    atlasGene.getOrthologs().contains(orthoid));
+        }
+
+        //Make sure the gene's id is not listed in its own ortholog list
+        assertFalse("Gene's id is listed in its own orthologs list",atlasGene.getOrthologs().contains("ENSG00000121075"));
+    }
+
+	@Test
+	public void test_getExperimentByIdDw()
+	{
+		  AtlasExperiment exp = atlasSolrDAO.getExperimentById("334420710");
+		  assertNotNull(exp);
+		  assertNotNull(exp.getAccession());
+	}
+
+	@Test	
+	public void test_getExperimentByAccession()
+	{
+		AtlasExperiment exp = atlasSolrDAO.getExperimentByAccession("E-MEXP-980");
+        assertNotNull(exp);
+        assertEquals("E-MEXP-980", exp.getAccession());
+	}
+
+    @Test
+    public void testGetAtlasGeneUnknown() {
+        AtlasSolrDAO.AtlasGeneResult atlasGene = atlasSolrDAO.getGeneByIdentifier("noName");
+        assertNotNull(atlasGene);
+        assertFalse(atlasGene.isFound());
+        assertFalse(atlasGene.isMulti());
+        assertNull(atlasGene.getGene());
+    }
+
+    @Test
+    public void test_getRankedGeneExperiments() {
+        AtlasSolrDAO.AtlasGeneResult r = atlasSolrDAO.getGeneByIdentifier("ENSG00000066279");
+
+        List<AtlasExperiment> list = atlasSolrDAO.getRankedGeneExperiments(r.getGene(), null, null, -1, -1);
+        assertNotNull(list);
+        assertTrue(list.size() > 0);
+
+        List<AtlasExperiment> list2 = atlasSolrDAO.getRankedGeneExperiments(r.getGene(), null, null, 1, 5);
+        assertNotNull(list2);
+        assertEquals(5, list2.size());
+
+        List<AtlasExperiment> list3 = atlasSolrDAO.getRankedGeneExperiments(r.getGene(), "diseasestate", "normal", -1, -1);
+        assertNotNull(list3);
+        assertTrue(list3.size() > 0);
+    }
+}
