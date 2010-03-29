70e709a615308e11cd785154cbb0f7a48a38f83f
Pavel Kurnosov <pashky@ebi.ac.uk>
2010-03-29 16:11:21 +0000

    Standalone NetCDF migrator command line app added to integrate into database migration process.
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone@11577 2913f559-6b04-0410-9a09-c530ee9f5186

---
+++ netcdf-generator/src/main/java/uk/ac/ebi/gxa/netcdf/migrator/App.java
@@ -0,0 +1,68 @@
+package uk.ac.ebi.gxa.netcdf.migrator;
+
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.apache.commons.cli.*;
+
+import java.util.Properties;
+
+public class App {
+    public static void main(String[] args) {
+        SLF4JBridgeHandler.install();
+        Logger log = LoggerFactory.getLogger(App.class);
+
+        CommandLineParser parser = new BasicParser();
+        Options options = new Options();
+
+        Option url = new Option("d", "url", true, "Database URL");
+        url.setArgName("url");
+        url.setRequired(true);
+        Option username = new Option("u", "user", true, "Database username");
+        username.setArgName("username");                               
+        username.setRequired(true);
+        Option password = new Option("p", "password", true, "Database password");
+        password.setArgName("password");
+        password.setRequired(true);
+        Option netcdf = new Option("n", "netcdf", true, "NetCDF directory path");
+        netcdf.setArgName("path");
+        netcdf.setRequired(true);
+
+        options.addOption(url).addOption(username).addOption(password).addOption(netcdf);
+
+        // Parse the arguments
+        try {
+            CommandLine commandLine = parser.parse(options, args);
+
+            Properties props = new Properties();
+            props.setProperty("jdbc.url", commandLine.getOptionValue('d'));
+            props.setProperty("jdbc.username", commandLine.getOptionValue('u'));
+            props.setProperty("jdbc.password", commandLine.getOptionValue('p'));
+            props.setProperty("netcdf.path", commandLine.getOptionValue('n'));
+
+            log.info(props.toString());
+
+            XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
+            PropertyPlaceholderConfigurer cfg = new PropertyPlaceholderConfigurer();
+            cfg.setProperties(props);
+            cfg.postProcessBeanFactory(factory);
+
+            AtlasNetCDFMigrator service = (AtlasNetCDFMigrator)factory.getBean("service");
+
+            service.generateNetCDFForAllExperiments();
+        } catch(ParseException e) {
+            HelpFormatter helpFormatter = new HelpFormatter();
+            helpFormatter.setWidth(1000);
+            helpFormatter.printHelp("java -Xmx2048m -jar netcdf-generator-X.XX-jar-with-dependencies.jar", "", options, "\n" + e.getMessage(), true);
+            System.exit(-1);
+        } catch(Throwable e) {
+            log.error("Error", e);
+            System.exit(-1);
+        }
+
+        System.exit(0);
+    }
+}
