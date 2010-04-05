08a2c73942adaf24aa91504923010b1f5bda28b5
ostolop <ostolop@2913f559-6b04-0410-9a09-c530ee9f5186>
2010-04-05 08:55:51 +0000

    Still need to fix sim result test (need to use existing netcdf)
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone@11687 2913f559-6b04-0410-9a09-c530ee9f5186

--- atlas-web/src/test/java/uk/ac/ebi/gxa/requesthandlers/experimentpage/SimilarGeneListTest.java
+++ atlas-web/src/test/java/uk/ac/ebi/gxa/requesthandlers/experimentpage/SimilarGeneListTest.java
@@ -74,7 +74,8 @@ public class SimilarGeneListTest extends TestCase {
     @Test
     public void testComputeSimilarityTask() {
         // do a similarity over E-AFMX-5 for an arbitrary design element/array design
-        final SimilarityResultSet simRS = new SimilarityResultSet("226010852", "153094131", "153069949");
+        // TODO: fix similarity result test!
+        final SimilarityResultSet simRS = new SimilarityResultSet("226010852", "153094131", "153069949", "");
         final String callSim = "sim.nc(" + simRS.getTargetDesignElementId() + ",'" + simRS.getSourceNetCDF() + "')";
 
         RDataFrame sim = null;
