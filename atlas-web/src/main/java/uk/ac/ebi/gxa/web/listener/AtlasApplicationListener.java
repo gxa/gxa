7cb2c2287d59aeec292a76b7ad9f7069253a9441
ostolop <ostolop@2913f559-6b04-0410-9a09-c530ee9f5186>
2010-04-06 13:04:34 +0000

    Added a more informative startup failure message on missing context.
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone@11702 2913f559-6b04-0410-9a09-c530ee9f5186

--- atlas-web/src/main/java/uk/ac/ebi/gxa/web/listener/AtlasApplicationListener.java
+++ atlas-web/src/main/java/uk/ac/ebi/gxa/web/listener/AtlasApplicationListener.java
@@ -72,8 +72,14 @@ public class AtlasApplicationListener implements ServletContextListener, HttpSes
 
         // get context, driven by config
         ServletContext application = sce.getServletContext();
-        WebApplicationContext context =
+        WebApplicationContext context = null;
+        try {
+            context =
                 WebApplicationContextUtils.getWebApplicationContext(application);
+        } catch (Throwable e) {
+            log.error("\n\n**** FATAL STARTUP ERROR ****\nFailed to get web application context! Is your context set up correctly?\n\n");
+            return;
+        }
 
         // fetch services from the context
         AtlasDAO atlasDAO = (AtlasDAO) context.getBean("atlasDAO");
