37de68226a21b258fc9610029b91d955c4cd9ed8
Andrey Zorin <azorin@ebi.ac.uk>
2010-04-23 10:49:42 +0000

    CUR_OntologyMapping
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone@12006 2913f559-6b04-0410-9a09-c530ee9f5186

---
+++ sql/Schema/CUR_OntologyMapping.sql
@@ -0,0 +1,40 @@
+/*******************************************************************************
+
+select * from CUR_OntologyMapping 
+
+*******************************************************************************/
+create or replace view CUR_OntologyMapping as 
+SELECT distinct e.accession Experiment 
+         ,p.name AS Property 
+         ,pv.name AS Value 
+         ,ot.accession AS OntologyTerm 
+         --,spv.SampleID AS SampleID
+         --,NULL AS AssayID
+ FROM a2_experiment e -- on e.ExperimentID = ev.ExperimentID 
+ JOIN a2_assay ass ON ass.ExperimentID = e.ExperimentID 
+ JOIN a2_assaysample asss ON asss.AssayID = ass.AssayID 
+ JOIN a2_sample s ON s.SampleID = asss.SampleID 
+ JOIN a2_samplePV spv ON spv.SampleID = s.SampleID
+ JOIN a2_samplePVontology so ON so.SamplePVID = spv.SamplePVID 
+ JOIN a2_propertyvalue pv ON pv.PropertyValueID = spv.PropertyValueID 
+ JOIN a2_property p ON p.PropertyID = pv.PropertyID 
+ JOIN a2_ontologyterm ot ON ot.OntologyTermID = so.OntologyTermID 
+ JOIN a2_ontology o ON o.OntologyID = ot.OntologyID 
+ UNION ALL 
+SELECT distinct e.accession Experiment 
+         ,p.name AS Property 
+         ,pv.name AS Value 
+         ,ot.accession AS OntologyTerm
+         --,NULL as SampleID
+         --,apv.AssayID as AssayID
+  FROM a2_experiment e -- on e.ExperimentID = ev.ExperimentID 
+  JOIN a2_assay ass ON ass.ExperimentID = e.ExperimentID 
+  JOIN a2_assayPV apv ON apv.assayID = ass.AssayID
+  JOIN a2_assayPVontology ao ON ao.AssayPVID = apv.assayPVID
+  JOIN a2_propertyvalue pv ON pv.PropertyValueID = apv.PropertyValueID 
+  JOIN a2_property p ON p.PropertyID = pv.PropertyID 
+  JOIN a2_ontologyterm ot ON ot.OntologyTermID = ao.OntologyTermID 
+  JOIN a2_ontology o ON o.OntologyID = ot.OntologyID;
+/
+exit;
+/
