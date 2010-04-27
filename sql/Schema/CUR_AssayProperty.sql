e8997f159bc841c255b106c573f81b71a7f66a07
azorin <azorin@2913f559-6b04-0410-9a09-c530ee9f5186>
2010-04-27 17:51:26 +0000

    add experiment accession to views
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone@12078 2913f559-6b04-0410-9a09-c530ee9f5186

--- sql/Schema/CUR_AssayProperty.sql
+++ sql/Schema/CUR_AssayProperty.sql
@@ -2,14 +2,17 @@
 CUR_AssayProperty
 *******************************************************************************/
 create or replace view CUR_AssayProperty as 
-select a.Accession 
+select
+e.Accession Experiment 
+,a.Accession 
 ,p.Name property
 ,pv.Name value
 ,NVL(apv.IsFactorValue,0) IsFactorValue
 from a2_Assay a
 join a2_AssayPV apv on apv.AssayID = a.AssayID
 join a2_PropertyValue pv on pv.PropertyValueID = apv.PropertyValueID
-join a2_Property p on p.PropertyID = pv.PropertyID;
+join a2_Property p on p.PropertyID = pv.PropertyID
+join a2_Experiment e on e.ExperimentID = a.ExperimentID;
 /
 exit;
 /
--- sql/Schema/CUR_SampleProperty.sql
+++ sql/Schema/CUR_SampleProperty.sql
@@ -1,13 +1,19 @@
 /*******************************************************************************
-CUR_SampleProperty
+select * from CUR_SampleProperty
 *******************************************************************************/
 create or replace view CUR_SampleProperty as 
-select a.Accession 
+select
+ (select MIN(e.Accession) 
+  from a2_Experiment e
+  join a2_Assay a on a.ExperimentID = e.ExperimentID
+  join a2_AssaySample asa on asa.AssayID = a.AssayID
+  where asa.SampleID = s.SampleID) Experiment
+,s.Accession 
 ,p.Name property
 ,pv.Name value
 ,NVL(apv.IsFactorValue,0) IsFactorValue
-from a2_Sample a
-join a2_SamplePV apv on apv.SampleID = a.SampleID
+from a2_Sample s
+join a2_SamplePV apv on apv.SampleID = s.SampleID
 join a2_PropertyValue pv on pv.PropertyValueID = apv.PropertyValueID
 join a2_Property p on p.PropertyID = pv.PropertyID;
 /
