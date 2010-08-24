d2c5fca3b80c849ccaa3b04381a4db3da1536659
Robert Petryszak <rpetry@ebi.ac.uk>
2010-08-24 09:43:38 +0000

    Further simplified code
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone@13619 2913f559-6b04-0410-9a09-c530ee9f5186

--- atlas-utils/src/main/java/uk/ac/ebi/gxa/utils/NumberFormatUtil.java
+++ atlas-utils/src/main/java/uk/ac/ebi/gxa/utils/NumberFormatUtil.java
@@ -12,14 +12,14 @@ import java.util.List;
  */
 public class NumberFormatUtil {
 
-    // P-values with exponents less than MIN_EXPONENT are shown as '< 10^-10'
-    private static final Integer MIN_EXPONENT = -10;
+    // P-values less than 10E-10 are shown as '< 10^-10'
+    private static final Float MIN_REPORTED_VALUE = 10E-10f;
+    private static final String MIN_EXPONENT = "-10";
     private static final String LESS_THAN = "< ";
-    private static final String E = "E";
-    private static final String ZERO = "0";
     private static final String TEN = "10";
-    private static final String MULTIPLY_HTML_CODE = " &#0215 ";
-    private static final String E_PATTERN = "#.##" + E + ZERO;
+    private static final String MULTIPLY_HTML_CODE = " &#0215; ";
+    private static final String E_PATTERN = "#.##E0";
+    private static final String E = "E";
     private static final String SUP_PRE = "<span style=\"vertical-align: super;\">";
     private static final String SUP_POST = "</span>";
     private static final String NOBR_START = "<nobr>";
@@ -39,11 +39,11 @@ public class NumberFormatUtil {
         // We now convert this format to 6.2*10^-3 (and 0 in the case of 0E0 specifically)
         List<String> formatParts = new ArrayList<String>(Arrays.asList(auxFormat.split(E)));
         String mantissa = formatParts.get(0); // in 6.2E-3, mantissa = 6.2
-        Integer exponent = Integer.parseInt(formatParts.get(1)); // // in 6.2E-3, exponent= -3
+        String exponent = formatParts.get(1); // // in 6.2E-3, exponent= -3
 
-        String pre = mantissa + MULTIPLY_HTML_CODE; // e.g 6.2 * 10
-        if ((mantissa.equals(ZERO) && exponent == 0) || (exponent < MIN_EXPONENT)) {
-            // if number < 10E-10 (including a round 0E0) forget its mantissa and show it simply as '< 10^-10'
+        String pre = mantissa + MULTIPLY_HTML_CODE; // e.g '6.2 * '
+        if (number < MIN_REPORTED_VALUE) {
+            // if number < 10E-10 forget its mantissa and show it simply as '< 10^-10'
             pre = LESS_THAN;
             exponent = MIN_EXPONENT;
         }
