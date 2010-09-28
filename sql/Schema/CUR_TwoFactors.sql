a8b208381287f42a28838fa7c0e79b65fe15bed9
Andrey Zorin <azorin@ebi.ac.uk>
2010-09-28 11:04:30 +0000

    CUR_MergeFactors -do not delete old properties, merge samples as well
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone@14319 2913f559-6b04-0410-9a09-c530ee9f5186

--- sql/Schema/CUR_TwoFactors.sql
+++ sql/Schema/CUR_TwoFactors.sql
@@ -9,14 +9,15 @@ FUNCTION CUR_ShowTwoValues(Experiment varchar2
  AS
 r CUR_TwoValues_Table;
 BEGIN
-  SELECT CUR_TwoValues(Assay,Value1,Value2)
+  SELECT CUR_TwoValues(Assay,Value1,Value2,SampleID)
   BULK COLLECT INTO r
   FROM ( Select distinct a.Accession Assay
                 ,pv1.Name Value1
                 ,pv2.Name Value2
+                ,ap1.SampleID
          from CUR_AllPropertyID ap1
-         join CUR_AllPropertyID ap2 on 1=1
-         join a2_assay a on a.AssayID = ap1.AssayID and a.AssayID = ap2.AssayID 
+         join CUR_AllPropertyID ap2 on ap2.AssayID=ap1.AssayID and ap2.SampleID=ap1.SampleID
+         join a2_assay a on a.AssayID = ap1.AssayID  
          join a2_experiment e on e.ExperimentID = a.experimentid
          join a2_PropertyValue pv1 on pv1.PropertyValueID = ap1.PropertyValueID
          join a2_PropertyValue pv2 on pv2.PropertyValueID = ap2.PropertyValueID
--- sql/Schema/CUR_TwoValues.sql
+++ sql/Schema/CUR_TwoValues.sql
@@ -1,7 +1,9 @@
+--DROP TYPE CUR_TwoValues_Table;
 CREATE OR REPLACE TYPE CUR_TwoValues AS OBJECT
        (Assay   VARCHAR(255),
         Value1  VARCHAR2(255),
-        Value2  VARCHAR2(255));
+        Value2  VARCHAR2(255),
+        SampleID INTEGER);
 /
 CREATE OR REPLACE TYPE CUR_TwoValues_Table AS TABLE OF CUR_TwoValues;
 /
