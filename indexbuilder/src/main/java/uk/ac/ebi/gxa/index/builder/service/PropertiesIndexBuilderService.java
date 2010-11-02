b6d3d38dc651869cb720fac983950f11740a0bb0
Robert Petryszak <rpetry@ebi.ac.uk>
2010-11-02 14:15:46 +0000

    Modified to extend SolrIndexBuilderService instead of IndexBuilderService
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone-conciseindex@15094 2913f559-6b04-0410-9a09-c530ee9f5186

--- indexbuilder/src/main/java/uk/ac/ebi/gxa/index/builder/service/PropertiesIndexBuilderService.java
+++ indexbuilder/src/main/java/uk/ac/ebi/gxa/index/builder/service/PropertiesIndexBuilderService.java
@@ -36,7 +36,7 @@ import java.util.List;
 /**
  * @author pashky
  */
-public class PropertiesIndexBuilderService extends IndexBuilderService {
+public class PropertiesIndexBuilderService extends SolrIndexBuilderService {
 
     @Override
     public void processCommand(IndexAllCommand indexAll, ProgressUpdater progressUpdater) throws IndexBuilderException {
