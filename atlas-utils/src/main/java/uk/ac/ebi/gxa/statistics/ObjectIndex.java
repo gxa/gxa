0a41cc0c3c0bb6f0ac34730b12b4f353a0f1db01
Robert Petryszak <rpetry@ebi.ac.uk>
2010-11-02 09:30:30 +0000

    Class for objects of type ObjectIdType to unique Integer values for the purpose of bit storage
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone-conciseindex@15071 2913f559-6b04-0410-9a09-c530ee9f5186

---
+++ atlas-utils/src/main/java/uk/ac/ebi/gxa/statistics/ObjectIndex.java
@@ -0,0 +1,67 @@
+package uk.ac.ebi.gxa.statistics;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: rpetry
+ * Date: Oct 26, 2010
+ * Time: 5:32:58 PM
+ * Class for objects of type ObjectIdType to unique Integer values for the purpose of either:
+ * - storage in ConciseSet (Gene ids)
+ * - minimising of space consumption in StatisticsStorage (Experiments)
+ */
+public class ObjectIndex<ObjectIdType> implements Serializable {
+    private ConcurrentMap<ObjectIdType,Integer> object2pos = new ConcurrentHashMap<ObjectIdType,Integer>();
+    private ConcurrentMap<Integer,ObjectIdType> pos2object = new ConcurrentHashMap<Integer,ObjectIdType>();
+    private static final long serialVersionUID = -8050952027393810684L;
+
+    synchronized public Integer addObject(ObjectIdType objectid) {
+        if(object2pos.containsKey(objectid)) {
+            return object2pos.get(objectid);
+        } else {
+            Integer pos = object2pos.size() + 1;
+
+            object2pos.put(objectid, pos);
+            pos2object.put(pos, objectid);
+
+            return pos;
+        }
+    }
+
+    public Integer getIndexForObject(ObjectIdType objectid) {
+        return object2pos.get(objectid);
+    }
+
+    public Integer getNumberOfObjects() {
+        return object2pos.size();
+    }
+
+    public Collection<ObjectIdType> getObjectsForIndexes(Collection<Integer> index) {
+        Collection<ObjectIdType>    objects = new ArrayList<ObjectIdType>(index.size());
+        for (Integer pos : index) objects.add(pos2object.get(pos));
+
+        return objects;
+    }
+
+    public Collection<ObjectIdType> getObjectsForIndexes(Collection<Integer> index, final Set<ObjectIdType> objectIds) {
+        Collection<ObjectIdType> objects = new ArrayList<ObjectIdType>(index.size());
+        for (Integer pos : index) {
+            ObjectIdType objectId = pos2object.get(pos);
+            if (objectIds.contains(objectId)) {
+                objects.add(objectId);
+            }
+        }
+
+        return objects;
+    }
+
+    public Set<ObjectIdType> getObjectIdsInIndex() {
+        return object2pos.keySet();
+    }
+}
