f26bad391ce62618efe970f6bd5289adeaae61dc
Robert Petryszak <rpetry@ebi.ac.uk>
2010-11-02 09:37:04 +0000

    Enumeration of bit-stored statistics types
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone-conciseindex@15072 2913f559-6b04-0410-9a09-c530ee9f5186

---
+++ atlas-utils/src/main/java/uk/ac/ebi/gxa/statistics/StatisticsType.java
@@ -0,0 +1,15 @@
+package uk.ac.ebi.gxa.statistics;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: rpetry
+ * Date: Oct 26, 2010
+ * Time: 5:35:31 PM
+ * Enumeration of bit-stored statistics types
+ */
+public enum StatisticsType {
+    UP,
+    DOWN,
+    NONDE
+}
+
