e35bcd9bd2f7fdcdfbef610a71d8993fce80fe20
Robert Petryszak <rpetry@ebi.ac.uk>
2010-11-02 14:13:18 +0000

    Moved Solr-specific functionality to subclass, SolrIndexBuilderService
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone-conciseindex@15091 2913f559-6b04-0410-9a09-c530ee9f5186

--- indexbuilder/src/main/java/uk/ac/ebi/gxa/index/builder/service/IndexBuilderService.java
+++ indexbuilder/src/main/java/uk/ac/ebi/gxa/index/builder/service/IndexBuilderService.java
@@ -22,29 +22,30 @@
 
 package uk.ac.ebi.gxa.index.builder.service;
 
-import org.apache.solr.client.solrj.SolrServer;
-import org.apache.solr.client.solrj.SolrServerException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.ac.ebi.gxa.index.builder.*;
 import uk.ac.ebi.gxa.dao.AtlasDAO;
+import uk.ac.ebi.microarray.atlas.model.OntologyMapping;
 
 import java.io.IOException;
+import java.util.*;
 
 /**
  * An abstract IndexBuilderService, that provides convenience methods for getting and setting parameters required across
- * all SOLR index building implementations.   Implementing classes have
- * access to an {@link org.apache.solr.client.solrj.embedded.EmbeddedSolrServer} to update the index, and an {@link
- * uk.ac.ebi.gxa.dao.AtlasDAO} that provides interaction with the Atlas database (following an Atlas 2
+ * all (SOLR and other) index building implementations.   Implementing classes have
+ * access to an {@link uk.ac.ebi.gxa.dao.AtlasDAO} that provides interaction with the Atlas database (following an Atlas 2
  * schema).
  * <p/>
+ *
  * @author Miroslaw Dylag (original version)
  * @author Tony Burdett (atlas 2 revision)
  */
 public abstract class IndexBuilderService {
     final private Logger log = LoggerFactory.getLogger(this.getClass());
     private AtlasDAO atlasDAO;
-    private SolrServer solrServer;
+
+    private Map<String, Collection<String>> ontologyMap = new HashMap<String, Collection<String>>();
 
     final public AtlasDAO getAtlasDAO() {
         return atlasDAO;
@@ -52,14 +53,6 @@ public abstract class IndexBuilderService {
 
     final public void setAtlasDAO(AtlasDAO atlasDAO) {
         this.atlasDAO = atlasDAO;
-    }
-
-    final public SolrServer getSolrServer() {
-        return solrServer;
-    }
-
-    final public void setSolrServer(SolrServer solrServer) {
-        this.solrServer = solrServer;
     }
 
     final protected Logger getLog() {
@@ -74,12 +67,12 @@ public abstract class IndexBuilderService {
      * Build the index for this particular IndexBuilderService implementation. Once the index has been built, this
      * method will automatically commit any changes and release any resources held by the SOLR server.
      *
-     * @param command command
+     * @param command         command
      * @param progressUpdater listener for passing progress updates
-     *
-     * @throws IndexBuilderException if the is a problem whilst generating the index
+     * @throws uk.ac.ebi.gxa.index.builder.IndexBuilderException
+     *          if the is a problem whilst generating the index
      */
-    final public void build(final IndexBuilderCommand command, final ProgressUpdater progressUpdater) throws IndexBuilderException {
+    public void build(final IndexBuilderCommand command, final ProgressUpdater progressUpdater) throws IndexBuilderException {
         command.visit(new IndexBuilderCommandVisitor() {
             public void process(IndexAllCommand cmd) throws IndexBuilderException {
                 processCommand(cmd, progressUpdater);
@@ -93,68 +86,53 @@ public abstract class IndexBuilderService {
         });
     }
 
-    final protected void deleteAll() throws IndexBuilderException {
-        try {
-            getSolrServer().deleteByQuery("*:*");
-        } catch (IOException e) {
-            throw new IndexBuilderException(e);
-        }
-        catch (SolrServerException e) {
-            throw new IndexBuilderException(e);
-        }
 
+    protected Map<String, Collection<String>> getOntologyMap() {
+        if (ontologyMap.isEmpty()) {
+            loadEfoMapping();
+        }
+        return ontologyMap;
     }
 
-    final protected void commit() throws IndexBuilderException {
-        try {
-            getSolrServer().commit();
-        } catch (IOException e) {
-            throw new IndexBuilderException(
-                    "Cannot commit changes to the SOLR server", e);
-        }
-        catch (SolrServerException e) {
-            throw new IndexBuilderException(
-                    "Cannot commit changes to the SOLR server - server threw exception",
-                    e);
-        }
+    private void loadEfoMapping() {
+        getLog().info("Fetching ontology mappings...");
 
-    }
+        // we don't support enything else yet
+        List<OntologyMapping> mappings = getAtlasDAO().getOntologyMappingsByOntology("EFO");
+        for (OntologyMapping mapping : mappings) {
+            String mapKey = mapping.getExperimentId() + "_" +
+                    mapping.getProperty() + "_" +
+                    mapping.getPropertyValue();
 
-    final protected void optimize() throws IndexBuilderException {
-        try {
-            getSolrServer().optimize();
-        } catch (IOException e) {
-            throw new IndexBuilderException(
-                    "Cannot commit changes to the SOLR server", e);
-        }
-        catch (SolrServerException e) {
-            throw new IndexBuilderException(
-                    "Cannot commit changes to the SOLR server - server threw exception",
-                    e);
+            if (ontologyMap.containsKey(mapKey)) {
+                // fetch the existing array and add this term
+                // fixme: should actually add ontology term accession
+                ontologyMap.get(mapKey).add(mapping.getOntologyTerm());
+            } else {
+                // add a new array
+                Collection<String> values = new HashSet<String>();
+                // fixme: should actually add ontology term accession
+                values.add(mapping.getOntologyTerm());
+                ontologyMap.put(mapKey, values);
+            }
         }
 
+        getLog().info("Ontology mappings loaded");
     }
 
-    public void processCommand(IndexAllCommand indexAll, ProgressUpdater progressUpdater) throws IndexBuilderException {
-        deleteAll();
-    }
+    public abstract void processCommand(IndexAllCommand indexAll, ProgressUpdater progressUpdater) throws IndexBuilderException;
 
-    public void processCommand(UpdateIndexForExperimentCommand updateIndexForExperimentCommand, ProgressUpdater progressUpdater) throws IndexBuilderException {
-    }
+    public abstract void processCommand(UpdateIndexForExperimentCommand updateIndexForExperimentCommand, ProgressUpdater progressUpdater) throws IndexBuilderException;
 
-    public void finalizeCommand(IndexAllCommand indexAll, ProgressUpdater progressUpdater) throws IndexBuilderException {
-        commit();
-        optimize();
-    }
+    public abstract void finalizeCommand(IndexAllCommand indexAll, ProgressUpdater progressUpdater) throws IndexBuilderException;
 
-    public void finalizeCommand(UpdateIndexForExperimentCommand updateIndexForExperimentCommand, ProgressUpdater progressUpdater) throws IndexBuilderException {
-        commit();
-        optimize();
-    }
+    public abstract void finalizeCommand(UpdateIndexForExperimentCommand updateIndexForExperimentCommand, ProgressUpdater progressUpdater) throws IndexBuilderException;
 
     /**
      * Returns index name, which this service builds
-      * @return text string
+     *
+     * @return text string
      */
     public abstract String getName();
+
 }
