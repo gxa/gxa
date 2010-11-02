fa1106d3241b679225f9b8f5e83d65e26b97a499
Robert Petryszak <rpetry@ebi.ac.uk>
2010-11-02 14:17:21 +0000

    Moved out bitIndexNetCDFs() to GeneAtlasBitIndexBuilderService
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone-conciseindex@15095 2913f559-6b04-0410-9a09-c530ee9f5186

--- netcdf-reader/src/main/java/uk/ac/ebi/gxa/netcdf/reader/AtlasNetCDFDAO.java
+++ netcdf-reader/src/main/java/uk/ac/ebi/gxa/netcdf/reader/AtlasNetCDFDAO.java
@@ -2,16 +2,10 @@ package uk.ac.ebi.gxa.netcdf.reader;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ucar.ma2.ArrayFloat;
-import uk.ac.ebi.gxa.statistics.*;
 import uk.ac.ebi.microarray.atlas.model.ExpressionAnalysis;
 
 import java.io.*;
 import java.util.*;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * This class wraps the functionality of retrieving values across multiple instances of NetCDFProxy
@@ -226,211 +220,14 @@ public class AtlasNetCDFDAO {
     }
 
     /**
-     * Generates a ConciseSet-based index for all statistics types in StatisticsType enum, across all Atlas ncdfs
      *
-     * @param ontomap Map experimentid_ef_efv -> Collection of efo terms
-     * @param fnoth how many threads to parallelise thsi tas over
-     * @param progressLogFreq how often this operation should be logged (i.e. every progressLogFreq ncfds processed)
-     * @return StatisticsStorage containing statistics for all statistics types in StatisticsType enum - collected over all Atlas ncdfs
-     * @throws InterruptedException
+     * @return List of all NetCDF Files in atlasNetCDFRepo
      */
-    public StatisticsStorage bitIndexNetCDFs(
-            final Map<String, Collection<String>> ontomap,
-            final Integer fnoth,
-            final Integer progressLogFreq) throws InterruptedException {
-        StatisticsStorage statisticsStorage = new StatisticsStorage();
-
-        final ObjectIndex<Long> geneIndex = new ObjectIndex<Long>();
-        final ObjectIndex<Experiment> experimentIndex = new ObjectIndex<Experiment>();
-
-        final Statistics upStats = new Statistics();
-        final Statistics dnStats = new Statistics();
-        final Statistics updnStats = new Statistics();
-        final Statistics noStats = new Statistics();
-
-
-        List<File> ncdfs = Arrays.asList(atlasNetCDFRepo.listFiles(new FilenameFilter() {
+    public List<File> getAllNcdfs() {
+                return Arrays.asList(atlasNetCDFRepo.listFiles(new FilenameFilter() {
             public boolean accept(File dir, String name) {
                 return (name.endsWith(".nc"));
             }
         }));
-
-        final AtomicInteger totalStatCount = new AtomicInteger();
-        final Integer total = ncdfs.size();
-        final AtomicInteger processedNcdfsCoCount = new AtomicInteger(0);
-        // Count of ncdfs in which no efvs were found
-        final AtomicInteger noEfvsNcdfCount = new AtomicInteger(0);
-
-        final long timeStart = System.currentTimeMillis();
-
-        List<Callable<Void>> tasks = new ArrayList<Callable<Void>>(ncdfs.size());
-        for (final File nc : ncdfs)
-            tasks.add(new Callable<Void>() {
-                public Void call() throws IOException {
-                    try {
-                        NetCDFProxy ncdf = new NetCDFProxy(nc);
-
-                        String[] uefvs = ncdf.getUniqueFactorValues();
-                        int car = 0; // count of all Statistics records added for this ncdf
-
-                        if (uefvs.length == 0) {
-                            ncdf.close();
-                            processedNcdfsCoCount.incrementAndGet();
-                            noEfvsNcdfCount.incrementAndGet();
-                            return null;
-                        }
-
-
-                        long[] genes = ncdf.getGenes();
-                        for (long gene : genes) if (gene != 0) geneIndex.addObject(gene);
-
-                        ArrayFloat.D2 tstat = ncdf.getTStatistics();
-                        ArrayFloat.D2 pvals = ncdf.getPValues();
-                        int[] shape = tstat.getShape();
-
-                        Experiment experiment = new Experiment(ncdf.getExperiment(), ncdf.getExperimentId() + "");
-                        Integer expIdx = experimentIndex.addObject(experiment);
-                        Set<Attribute> efAttrs = new HashSet<Attribute>();
-                        Set<Attribute> efoAttrs = new HashSet<Attribute>();
-
-
-                        for (int j = 0; j < uefvs.length; j++) {
-                            String[] efefv = uefvs[j].split("\\|\\|");
-                            Attribute efvAttribute = new Attribute(uefvs[j]);
-                            Attribute efAttribute = new Attribute(efefv[0]);
-                            efAttrs.add(efAttribute);
-                            Collection<String> efos =
-                                    ontomap.get(ncdf.getExperimentId() + "_" + efefv[0] + (efefv.length > 1 ? "_" + efefv[1] : ""));
-
-                            SortedSet<Integer> upGeneIndexes = new TreeSet<Integer>();
-                            SortedSet<Integer> dnGeneIndexes = new TreeSet<Integer>();
-                            SortedSet<Integer> noGeneIndexes = new TreeSet<Integer>();
-
-                            for (int i = 0; i < shape[0]; i++) {
-                                if (genes[i] == 0) continue;
-
-                                float t = tstat.get(i, j);
-                                float p = pvals.get(i, j);
-
-                                Integer idx = geneIndex.getIndexForObject(genes[i]);
-                                if (null == idx) continue;
-
-                                if (p > 0.05) {
-                                    noGeneIndexes.add(idx);
-                                    car++;
-                                } else {
-                                    if (t > 0) {
-                                        upGeneIndexes.add(idx);
-                                        car++;
-                                    } else {
-                                        dnGeneIndexes.add(idx);
-                                        car++;
-                                    }
-                                }
-                            }
-
-                            upStats.addStatistics(efvAttribute, expIdx, upGeneIndexes);
-                            dnStats.addStatistics(efvAttribute, expIdx, dnGeneIndexes);
-                            updnStats.addStatistics(efvAttribute, expIdx, upGeneIndexes);
-                            updnStats.addStatistics(efvAttribute, expIdx, dnGeneIndexes);
-                            noStats.addStatistics(efvAttribute, expIdx, noGeneIndexes);
-
-                            upStats.addStatistics(efAttribute, expIdx, upGeneIndexes);
-                            dnStats.addStatistics(efAttribute, expIdx, dnGeneIndexes);
-                            updnStats.addStatistics(efAttribute, expIdx, upGeneIndexes);
-                            updnStats.addStatistics(efAttribute, expIdx, dnGeneIndexes);
-                            noStats.addStatistics(efAttribute, expIdx, noGeneIndexes);
-
-                            if (efos != null) {
-                                for (String efo : efos) {
-                                    Attribute efoAttribute = new Attribute(efo);
-                                    efoAttrs.add(efoAttribute);
-                                    upStats.addStatistics(efoAttribute, expIdx, upGeneIndexes);
-                                    dnStats.addStatistics(efoAttribute, expIdx, dnGeneIndexes);
-                                    updnStats.addStatistics(efoAttribute, expIdx, upGeneIndexes);
-                                    updnStats.addStatistics(efoAttribute, expIdx, dnGeneIndexes);
-                                    noStats.addStatistics(efoAttribute, expIdx, noGeneIndexes);
-                                }
-                            }
-                        }
-
-                        // Add data count for aggregations of efvs also (ef, efo)
-                        for (Attribute efAttribute : efAttrs) {
-                            car += upStats.getNumStatistics(efAttribute, expIdx);
-                            car += dnStats.getNumStatistics(efAttribute, expIdx);
-                            car += updnStats.getNumStatistics(efAttribute, expIdx);
-                            car += noStats.getNumStatistics(efAttribute, expIdx);
-                        }
-                        for (Attribute efoAttribute : efoAttrs) {
-                            car += upStats.getNumStatistics(efoAttribute, expIdx);
-                            car += dnStats.getNumStatistics(efoAttribute, expIdx);
-                            car += updnStats.getNumStatistics(efoAttribute, expIdx);
-                            car += noStats.getNumStatistics(efoAttribute, expIdx);
-                        }
-
-
-                        tstat = null;
-                        pvals = null;
-                        ncdf.close();
-
-                        totalStatCount.addAndGet(car);
-                        if (car == 0) {
-                            log.debug(nc.getName() + " num uefvs : " + uefvs.length + " [" + car + "]");
-                        }
-
-
-                        int processedNow = processedNcdfsCoCount.incrementAndGet();
-                        if (processedNow % progressLogFreq == 0 || processedNow == total) {
-                            long timeNow = System.currentTimeMillis();
-                            long elapsed = timeNow - timeStart;
-                            double speed = (processedNow / (elapsed / Double.valueOf(progressLogFreq)));  // (item/s)
-                            double estimated = (total - processedNow) / (speed * 60);
-
-                            log.info(
-                                    String.format("Processed %d/%d (# ncdfs with no EFVs so far: %d) ncdfs %d%%, %.1f ncdfs/sec overall, estimated %.1f min remaining",
-                                            processedNow, total, noEfvsNcdfCount.get(), (processedNow * 100 / total), speed, estimated));
-
-                            if (processedNow == total) {
-                                log.info("Overall processing time: " + (elapsed / 60000) + " min");
-                            }
-                        }
-                    } catch (Throwable t) {
-                        log.error("Error occurred: ", t);
-                    }
-                    return null;
-                }
-            });
-
-        ExecutorService svc = Executors.newFixedThreadPool(fnoth);
-        svc.invokeAll(tasks);
-        svc.shutdown();
-
-        log.info("Total set " + (totalStatCount.get() * 8L) / 1024 + " kB");
-
-        statisticsStorage.addStatistics(StatisticsType.UP, upStats);
-        statisticsStorage.addStatistics(StatisticsType.DOWN, dnStats);
-        statisticsStorage.addStatistics(StatisticsType.UPDOWN, updnStats);
-        statisticsStorage.addStatistics(StatisticsType.NONDE, noStats);
-        statisticsStorage.setGeneIndex(geneIndex);
-        statisticsStorage.setExperimentIndex(experimentIndex);
-
-
-        // Serialize to a byte array TODO testing only - move out of here
-        try {
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            ObjectOutput out = new ObjectOutputStream(bos);
-            out.writeObject(statisticsStorage);
-            out.close();
-
-            // Get the bytes of the serialized object
-            byte[] buf = bos.toByteArray();
-
-            log.info("Serialized statisticsStorage size: " + ((long) (buf.length) / 1024) + " kB");
-
-        } catch (IOException ioe) {
-            log.error("Problem serializing statisticsStorage: ", ioe);
-        }
-
-        return statisticsStorage;
     }
 }
