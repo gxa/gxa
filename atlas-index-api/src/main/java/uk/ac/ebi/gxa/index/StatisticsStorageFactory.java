ba2a10adf95b0d425f6b7aced3ce7f868f5fe027
Robert Petryszak <rpetry@ebi.ac.uk>
2010-11-02 17:22:58 +0000

    This factory class returns de-serialized bit index of gene expression data
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone-conciseindex@15110 2913f559-6b04-0410-9a09-c530ee9f5186

---
+++ atlas-index-api/src/main/java/uk/ac/ebi/gxa/index/StatisticsStorageFactory.java
@@ -0,0 +1,56 @@
+package uk.ac.ebi.gxa.index;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.ac.ebi.gxa.statistics.StatisticsStorage;
+
+import java.io.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: rpetry
+ * Date: Nov 2, 2010
+ * Time: 2:58:59 PM
+ * This factory class returns de-serialized bit index of gene expression data
+ */
+public class StatisticsStorageFactory {
+
+    final private Logger log = LoggerFactory.getLogger(getClass());
+
+    private File atlasIndex;
+    private String indexFileName;
+    private StatisticsStorage<Long> statisticsStorage = null;
+
+    public StatisticsStorageFactory(String indexFileName) {
+        this.indexFileName = indexFileName;
+    }
+
+    public File getAtlasIndex() {
+        return atlasIndex;
+    }
+
+    public void setAtlasIndex(File atlasIndexDir) {
+        this.atlasIndex = atlasIndexDir;
+    }
+
+    /**
+     *
+     * @return StatisticsStorage containing indexes of all types in StatisticType enum
+     * @throws IOException
+     */
+    public StatisticsStorage createStatisticsStorage() throws IOException {
+
+        File indexFile = new File(atlasIndex + File.separator + indexFileName);
+        ObjectInputStream obj = new ObjectInputStream(new FileInputStream(indexFile));
+        try {
+            statisticsStorage = (StatisticsStorage<Long>) obj.readObject();
+        } catch (ClassNotFoundException cnfe) {
+            log.error("Failed to de-serialize: " + indexFile.getAbsolutePath());
+        } finally {
+            obj.close();
+        }
+
+        return statisticsStorage;
+    }
+
+}
