b4a4663dd7e49a716adc93d437d954358fd2a694
Robert Petryszak <rpetry@ebi.ac.uk>
2010-11-04 14:47:06 +0000

    Uncommented logging
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone@15199 2913f559-6b04-0410-9a09-c530ee9f5186

--- scripts/cron/ebeye_export.sh
+++ scripts/cron/ebeye_export.sh
@@ -13,7 +13,7 @@ do
   curl www.ebi.ac.uk/gxa/download/ebeye_export.xml -o /nas/microarray/home/cronjobs/public/xml/gxa/ebeye_export.xml.zip.tmp
   format=$(file /nas/microarray/home/cronjobs/public/xml/gxa/ebeye_export.xml.zip.tmp | awk '{ print $2}')
   s=$(ls -lah /nas/microarray/home/cronjobs/public/xml/gxa/ebeye_export.xml.zip.tmp | awk '{ print $5}')
-  # echo "attempt $attempt_num : Downloaded ebeye_export.xml.zip size: $s and format: $format"
+  echo "attempt $attempt_num : Downloaded ebeye_export.xml.zip size: $s and format: $format"
   # Send an error email and re-try after 10 mins if ebeye_export.xml.zip.tmp size is less than 1 MB
   if [ $format != 'Zip' ] ; then
     mailx -s"attempt $attempt_num : `whoami`@`hostname` cron error : $0 downloaded ebeye_export.xml.zip in incorrect format: $format - re-trying after 10 mins" rpetry@ebi.ac.uk < /dev/null
@@ -21,7 +21,7 @@ do
   else 
     mv /nas/microarray/home/cronjobs/public/xml/gxa/ebeye_export.xml.zip.tmp /nas/microarray/home/cronjobs/public/xml/gxa/ebeye_export.xml.zip
     success_msg="attempt $attempt_num: successfully saved /nas/microarray/home/cronjobs/public/xml/gxa/ebeye_export.xml.zip file"
-    # echo "$success_msg"
+    echo "$success_msg"
     if [ $attempt_num -gt 1 ] ; then # there had been a failure that's now been fixed - email $success_msg
 	mailx -s"$success_msg" rpetry@ebi.ac.uk < /dev/null
     fi
