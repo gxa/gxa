7efbedc91340c013d4125290aca13de3cbffb6cd
Alexey Filippov <alf@ebi.ac.uk>
2010-11-23 10:45:14 +0000

    Copy-n-pase code extracted to methods
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone@15511 2913f559-6b04-0410-9a09-c530ee9f5186

--- atlas-web/src/main/java/ae3/dao/AtlasSolrDAO.java
+++ atlas-web/src/main/java/ae3/dao/AtlasSolrDAO.java
@@ -24,24 +24,24 @@ package ae3.dao;
 
 import ae3.model.AtlasExperiment;
 import ae3.model.AtlasGene;
-import org.apache.solr.common.params.FacetParams;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowCallbackHandler;
-import uk.ac.ebi.gxa.utils.FilterIterator;
-import uk.ac.ebi.gxa.utils.StringUtil;
-import uk.ac.ebi.gxa.utils.EmptyIterator;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.client.solrj.response.FacetField;
+import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
+import org.apache.solr.common.params.FacetParams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowCallbackHandler;
 import uk.ac.ebi.gxa.index.GeneExpressionAnalyticsTable;
-import uk.ac.ebi.gxa.utils.EscapeUtil;
 import uk.ac.ebi.gxa.properties.AtlasProperties;
+import uk.ac.ebi.gxa.utils.EmptyIterator;
+import uk.ac.ebi.gxa.utils.EscapeUtil;
+import uk.ac.ebi.gxa.utils.FilterIterator;
+import uk.ac.ebi.gxa.utils.StringUtil;
 import uk.ac.ebi.microarray.atlas.model.ExpressionAnalysis;
 
 import java.sql.ResultSet;
@@ -73,7 +73,7 @@ public class AtlasSolrDAO {
         this.atlasProperties = atlasProperties;
     }
 
-    public void setAtlasJdbcTemplate(JdbcTemplate jdbcTemplate){
+    public void setAtlasJdbcTemplate(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
@@ -137,7 +137,8 @@ public class AtlasSolrDAO {
 
         /**
          * Constructor
-         * @param experiments list of experiments
+         *
+         * @param experiments  list of experiments
          * @param totalResults total number of results
          * @param startingFrom start position of the list returned in full list of found results
          */
@@ -149,6 +150,7 @@ public class AtlasSolrDAO {
 
         /**
          * Returns list of experiments
+         *
          * @return list of experiments
          */
         public List<AtlasExperiment> getExperiments() {
@@ -157,6 +159,7 @@ public class AtlasSolrDAO {
 
         /**
          * Returns total number of found results
+         *
          * @return total number of found results
          */
         public int getTotalResults() {
@@ -165,6 +168,7 @@ public class AtlasSolrDAO {
 
         /**
          * Returns starting position of the list
+         *
          * @return starting position of the list
          */
         public int getStartingFrom() {
@@ -173,6 +177,7 @@ public class AtlasSolrDAO {
 
         /**
          * Returns number of results in returned list
+         *
          * @return number of results in returned list
          */
         public int getNumberOfResults() {
@@ -182,10 +187,11 @@ public class AtlasSolrDAO {
 
     /**
      * Search experiments by SOLR query
+     *
      * @param query SOLR query string
      * @param start starting position
-     * @param rows number of rows to fetch
-     * @return 
+     * @param rows  number of rows to fetch
+     * @return
      */
     public AtlasExperimentsResult getExperimentsByQuery(String query, int start, int rows) {
         SolrQuery q = new SolrQuery(query);
@@ -199,10 +205,10 @@ public class AtlasSolrDAO {
             List<AtlasExperiment> result = new ArrayList<AtlasExperiment>();
 
             if (documentList != null)
-                for(SolrDocument exptDoc : documentList)
+                for (SolrDocument exptDoc : documentList)
                     result.add(new AtlasExperiment(exptDoc));
 
-            return new AtlasExperimentsResult(result, documentList == null ? 0 : (int)documentList.getNumFound(), start);
+            return new AtlasExperimentsResult(result, documentList == null ? 0 : (int) documentList.getNumFound(), start);
         } catch (SolrServerException e) {
             throw new RuntimeException("Error querying for experiments", e);
         }
@@ -210,6 +216,7 @@ public class AtlasSolrDAO {
 
     /**
      * List all experiments
+     *
      * @return list of all experiments
      */
     public List<AtlasExperiment> getExperiments() {
@@ -255,6 +262,7 @@ public class AtlasSolrDAO {
 
     /**
      * Finds gene by id
+     *
      * @param gene_id_key numerical gene id (yes, in string)
      * @return atlas gene result
      */
@@ -305,6 +313,7 @@ public class AtlasSolrDAO {
 
     /**
      * Returns number of genes in index
+     *
      * @return total number of indexed genes
      */
     public long getGeneCount() {
@@ -323,76 +332,12 @@ public class AtlasSolrDAO {
 
     /**
      * Returns genes that can be iterated
+     *
      * @return Iterable<AtlasGene>
      */
     public Iterable<AtlasGene> getAllGenes() {
         final SolrQuery q = new SolrQuery("*:*");
-        q.setRows(0);
-        final long total;
-
-        try {
-            QueryResponse queryResponse = solrServerAtlas.query(q);
-            SolrDocumentList documentList = queryResponse.getResults();
-
-            total = documentList.getNumFound();
-        } catch (SolrServerException e) {
-            throw new RuntimeException("Error querying list of genes");
-        }
-
-        return new Iterable<AtlasGene>() {
-            public Iterator<AtlasGene> iterator() {
-                return new Iterator<AtlasGene> () {
-                    private Iterator<AtlasGene> genes = null;
-                    private int totalSeen = 0;
-
-                    public boolean hasNext() {
-                        if(null == genes
-                                ||
-                           (!genes.hasNext() && totalSeen < total)) {
-                            getNextGeneBatch();
-                        }
-
-                        return totalSeen < total && genes.hasNext();
-                    }
-
-                    public AtlasGene next() {
-                        if(null == genes
-                                ||
-                           (!genes.hasNext() && totalSeen < total)) {
-                            getNextGeneBatch();
-                        }
-
-                        totalSeen++;
-                        return genes.next();
-                    }
-
-                    public void remove() {}
-
-                    private void getNextGeneBatch() {
-                        try {
-                            log.debug("Loading next batch of genes, seen " + totalSeen + " out of " + total);
-                            List<AtlasGene> geneList = new ArrayList<AtlasGene>();
-
-                            q.setRows(50);
-                            q.setStart(totalSeen);
-
-                            QueryResponse queryResponse = solrServerAtlas.query(q);
-                            SolrDocumentList documentList = queryResponse.getResults();
-
-                            for (SolrDocument d : documentList) {
-                                AtlasGene g = new AtlasGene(d);
-                                geneList.add(g);
-                            }
-
-                            genes = geneList.iterator();
-                        } catch (SolrServerException e) {
-                            throw new RuntimeException("Error querying list of genes");
-                        }
-
-                    }
-                };
-            };
-        };
+        return createIteratorForQuery(q);
     }
 
     /**
@@ -407,7 +352,7 @@ public class AtlasSolrDAO {
         return getGeneByQuery("id:" + id + " identifier:" + id);
     }
 
-        /**
+    /**
      * Returns the AtlasGene corresponding to the specified gene identifier, i.e. matching one of the terms in the
      * "gene_ids" field in Solr schema.
      *
@@ -422,14 +367,15 @@ public class AtlasSolrDAO {
     /**
      * Searches gene by id (numerical), identifier (primary) or any of specified set of properties
      * supposedly containing other identifiers
+     *
      * @param gene_identifier identifier
-     * @param additionalIds additional properties to search for
+     * @param additionalIds   additional properties to search for
      * @return atlas gene search result
      */
     public AtlasGeneResult getGeneByAnyIdentifier(String gene_identifier, List<String> additionalIds) {
         final String id = EscapeUtil.escapeSolr(gene_identifier);
         StringBuilder sb = new StringBuilder("id:" + id + " identifier:" + id);
-        for(String idprop : additionalIds)
+        for (String idprop : additionalIds)
             sb.append(" property_").append(idprop).append(":").append(id);
         return getGeneByQuery(sb.toString());
     }
@@ -437,17 +383,22 @@ public class AtlasSolrDAO {
     /**
      * Returns AtlasGenes corresponding to the specified gene identifiers, i.e. matching one of the terms in the
      * "gene_ids" field in Solr schema.
+     *
      * @param ids Collection of ids
      * @return Iterable<AtlasGene>
      */
     public Iterable<AtlasGene> getGenesByIdentifiers(Collection ids) {
-        if(ids.isEmpty()) return Collections.emptyList();
+        if (ids.isEmpty()) return Collections.emptyList();
 
         StringBuilder sb = new StringBuilder();
         for (Object id : ids)
             sb.append(" id:").append(id).append(" identifier:").append(id);
 
         final SolrQuery q = new SolrQuery(sb.toString());
+        return createIteratorForQuery(q);
+    }
+
+    private Iterable<AtlasGene> createIteratorForQuery(final SolrQuery q) {
         q.setRows(0);
         final long total;
 
@@ -462,14 +413,14 @@ public class AtlasSolrDAO {
 
         return new Iterable<AtlasGene>() {
             public Iterator<AtlasGene> iterator() {
-                return new Iterator<AtlasGene> () {
+                return new Iterator<AtlasGene>() {
                     private Iterator<AtlasGene> genes = null;
                     private int totalSeen = 0;
 
                     public boolean hasNext() {
-                        if(null == genes
+                        if (null == genes
                                 ||
-                           (!genes.hasNext() && totalSeen < total)) {
+                                (!genes.hasNext() && totalSeen < total)) {
                             getNextGeneBatch();
                         }
 
@@ -477,9 +428,9 @@ public class AtlasSolrDAO {
                     }
 
                     public AtlasGene next() {
-                        if(null == genes
+                        if (null == genes
                                 ||
-                           (!genes.hasNext() && totalSeen < total)) {
+                                (!genes.hasNext() && totalSeen < total)) {
                             getNextGeneBatch();
                         }
 
@@ -487,7 +438,8 @@ public class AtlasSolrDAO {
                         return genes.next();
                     }
 
-                    public void remove() {}
+                    public void remove() {
+                    }
 
                     private void getNextGeneBatch() {
                         try {
@@ -512,13 +464,16 @@ public class AtlasSolrDAO {
 
                     }
                 };
-            };
+            }
+
+            ;
         };
     }
 
 
     /**
      * Fetch list of orthologs for specified gene
+     *
      * @param atlasGene specified gene to look orthologs for
      * @return list of ortholog genes
      */
@@ -539,11 +494,12 @@ public class AtlasSolrDAO {
 
     /**
      * Get list of ranked gene experiments for gene, ef and efv
+     *
      * @param atlasGene atlas gene
-     * @param ef factor string
-     * @param efv factor value
-     * @param minRows starting position of resultset to return
-     * @param maxRows finish position of resultset to return
+     * @param ef        factor string
+     * @param efv       factor value
+     * @param minRows   starting position of resultset to return
+     * @param maxRows   finish position of resultset to return
      * @return list of atlas experiments
      */
     public List<AtlasExperiment> getRankedGeneExperiments(AtlasGene atlasGene, String ef, String efv, int minRows,
@@ -552,7 +508,7 @@ public class AtlasSolrDAO {
         GeneExpressionAnalyticsTable etable = atlasGene.getExpressionAnalyticsTable();
         Iterable<ExpressionAnalysis> eas;
 
-        if(ef != null && !"".equals(ef) && efv != null) {
+        if (ef != null && !"".equals(ef) && efv != null) {
             eas = etable.findByEfEfv(ef, efv);
         } else if (ef != null && !"".equals(ef) && efv == null) {
             eas = etable.findByFactor(ef);
@@ -568,9 +524,9 @@ public class AtlasSolrDAO {
             }
         }
 
-       if(exps.size() == 0) 
-          return Collections.emptyList();
-       
+        if (exps.size() == 0)
+            return Collections.emptyList();
+
         Object[] aexps = exps.entrySet().toArray();
         Arrays.sort(aexps, new Comparator<Object>() {
             public int compare(Object o1, Object o2) {
@@ -582,58 +538,43 @@ public class AtlasSolrDAO {
             }
         });
 
-        List<AtlasExperiment> atlasExps = new ArrayList<AtlasExperiment>();
-        for (int i = minRows > 0 ? minRows - 1 : 0;
-             i < (maxRows > 0 ? (maxRows > aexps.length ? aexps.length : maxRows) : aexps.length);
-             ++i) {
-            @SuppressWarnings("unchecked")
-            Long experimentId = ((Map.Entry<Long, Float>) aexps[i]).getKey();
-            AtlasExperiment atlasExperiment = getExperimentById(experimentId);
-            if (atlasExperiment != null) {
-                atlasExperiment
-                        .addHighestRankEF(atlasGene.getGeneId(), atlasGene.getHighestRankEF(experimentId).getFirst());
-                atlasExps.add(atlasExperiment);
-            }
-        }
-
-        return atlasExps;
+        return readExperiments(atlasGene, minRows, maxRows, aexps);
     }
 
     public List<AtlasExperiment> getRankedGeneExperimentsForEfo(AtlasGene atlasGene, String efo, int minRows,
-                                                          int maxRows) {
-        List<AtlasExperiment> atlasExps = new ArrayList<AtlasExperiment>();
+                                                                int maxRows) {
 
         GeneExpressionAnalyticsTable etable = atlasGene.getExpressionAnalyticsTable();
         Map<Long, Float> exps = new HashMap<Long, Float>();
 
-        class ef{
+        class ef {
             public String Property;
             public String Value;
         }
 
         final List<ef> mappedEfs = new ArrayList<ef>();
-        jdbcTemplate.query("select distinct Property, Value from CUR_ONTOLOGYMAPPING where ONTOLOGYTERM LIKE ?", new Object[] {efo}, new RowCallbackHandler(){
-            public void processRow(ResultSet rs){
-                try{
-                ef ef = new ef();
-                ef.Property = rs.getString("Property");
-                ef.Value = rs.getString("Value");
-                mappedEfs.add(ef);
-                }catch(Exception ex){
+        jdbcTemplate.query("select distinct Property, Value from CUR_ONTOLOGYMAPPING where ONTOLOGYTERM LIKE ?", new Object[]{efo}, new RowCallbackHandler() {
+            public void processRow(ResultSet rs) {
+                try {
+                    ef ef = new ef();
+                    ef.Property = rs.getString("Property");
+                    ef.Value = rs.getString("Value");
+                    mappedEfs.add(ef);
+                } catch (Exception ex) {
                     System.out.println(ex.getMessage());
                 }
             }
         });
 
-        for(ef mappedEf:mappedEfs){
-        String ef  = mappedEf.Property;
-        String efv = mappedEf.Value;
+        for (ef mappedEf : mappedEfs) {
+            String ef = mappedEf.Property;
+            String efv = mappedEf.Value;
 
-        for (ExpressionAnalysis e : (ef != null && efv != null ? etable.findByEfEfv(ef, efv) : etable.getAll())) {
-            if (exps.get(e.getExperimentID()) == null || exps.get(e.getExperimentID()) > e.getPValAdjusted()) {
-                exps.put(e.getExperimentID(), e.getPValAdjusted());
+            for (ExpressionAnalysis e : (ef != null && efv != null ? etable.findByEfEfv(ef, efv) : etable.getAll())) {
+                if (exps.get(e.getExperimentID()) == null || exps.get(e.getExperimentID()) > e.getPValAdjusted()) {
+                    exps.put(e.getExperimentID(), e.getPValAdjusted());
+                }
             }
-        }
         }
 
         Object[] aexps = exps.entrySet().toArray();
@@ -647,6 +588,11 @@ public class AtlasSolrDAO {
             }
         });
 
+        return readExperiments(atlasGene, minRows, maxRows, aexps);
+    }
+
+    private List<AtlasExperiment> readExperiments(AtlasGene atlasGene, int minRows, int maxRows, Object[] aexps) {
+        List<AtlasExperiment> atlasExps = new ArrayList<AtlasExperiment>();
         //AZ: crashed at aexps.length=0
         for (int i = minRows > 0 ? minRows - 1 : 0;
              i < (maxRows > 0 ? (maxRows > aexps.length ? aexps.length : maxRows) : aexps.length); ++i) {
@@ -664,6 +610,7 @@ public class AtlasSolrDAO {
 
     /**
      * Returns list of species studied in particular experiment
+     *
      * @param experimentId experiment id
      * @return list of species strings
      */
@@ -676,13 +623,13 @@ public class AtlasSolrDAO {
         q.addFacetField("species");
         try {
             QueryResponse qr = solrServerAtlas.query(q);
-            if(qr.getFacetFields() != null && qr.getFacetFields().get(0) != null && qr.getFacetFields().get(0).getValues() != null) {
+            if (qr.getFacetFields() != null && qr.getFacetFields().get(0) != null && qr.getFacetFields().get(0).getValues() != null) {
                 final Iterator<FacetField.Count> iterator = qr.getFacetFields().get(0).getValues().iterator();
                 return new Iterable<String>() {
                     public Iterator<String> iterator() {
                         return new FilterIterator<FacetField.Count, String>(iterator) {
                             public String map(FacetField.Count c) {
-                                if(c.getName() != null)
+                                if (c.getName() != null)
                                     return StringUtil.upcaseFirst(c.getName());
                                 return null;
                             }
