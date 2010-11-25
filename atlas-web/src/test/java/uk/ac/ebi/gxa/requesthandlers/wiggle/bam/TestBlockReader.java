3ebb38d8563ce6c1cdbfc07e01f60ec4483e6ba6
Alexey Filippov <alf@ebi.ac.uk>
2010-11-25 17:25:06 +0000

    Improved diagnostic message
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone@15631 2913f559-6b04-0410-9a09-c530ee9f5186

--- atlas-web/src/test/java/uk/ac/ebi/gxa/requesthandlers/wiggle/bam/TestBlockReader.java
+++ atlas-web/src/test/java/uk/ac/ebi/gxa/requesthandlers/wiggle/bam/TestBlockReader.java
@@ -30,34 +30,32 @@ import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class TestBlockReader {
-        @Test
+    @Test
     public void test0() {
         runTest("bam/accepted_hits.sorted.bam", "3", 1, 7381, answer0);
     }
 
-        @Test
+    @Test
     public void test1() {
         runTest("bam/accepted_hits.sorted.bam", "1", 983000, 983500, answer1);
     }
 
-    private static String answer0 =
-        "65492:19081:35316\n";
-    private static String answer1 =
-        "65480:3657:12873\n" +
-        "65506:0:35319\n" +
-        "65498:61706:63410\n" +
-        "65475:20022:20886\n" +
-        "65492:1657:1801\n";
+    private static String answer0 = "65492:19081:35316\n";
+    private static String answer1 = "65480:3657:12873\n" +
+            "65506:0:35319\n" +
+            "65498:61706:63410\n" +
+            "65475:20022:20886\n" +
+            "65492:1657:1801\n";
 
     public static void runTest(String fileName, String chromosomeName, long start, long end, String answer) {
         try {
             final StringBuilder builder = new StringBuilder();
-                final File file = new File(TestBlockReader.class.getClassLoader().getResource(fileName).toURI());
+            final File file = new File(TestBlockReader.class.getClassLoader().getResource(fileName).toURI());
             final BAMReader reader = new BAMReader(file);
             for (BAMBlock b : reader.readBAMBlocks(chromosomeName, start, end)) {
                 builder.append(b.buffer.length).append(":").append(b.from).append(":").append(b.to).append("\n");
             }
-            assertEquals(answer, builder.toString());
+            assertEquals("Non-matching results", answer, builder.toString());
         } catch (Exception e) {
             fail(e.getMessage());
         }
