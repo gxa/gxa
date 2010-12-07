ddd4953d6b8e85b90c2491928e49f1fbcbfb7431
Robert Petryszak <rpetry@ebi.ac.uk>
2010-12-07 12:05:31 +0000

    Removed compuation optimisation on Misha's advice.
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone@15847 2913f559-6b04-0410-9a09-c530ee9f5186

--- atlas-analytics/src/main/resources/R/analytics.R
+++ atlas-analytics/src/main/resources/R/analytics.R
@@ -176,7 +176,6 @@ allupdn <- function (eset, alpha=0.01, evars=varLabels(eset) ) {
     try({
       print(paste("Calculating lmFit and F-stats for", varLabel))
       if( length(levels(eset[[varLabel, exact=TRUE]]))<2
-       || length(levels(eset[[varLabel, exact=TRUE]]))>200
        || length(levels(eset[[varLabel, exact=TRUE]])) == ncol(exprs) ) { next }
       thisFit = fstat.eset(eset,varLabel=varLabel)
       
