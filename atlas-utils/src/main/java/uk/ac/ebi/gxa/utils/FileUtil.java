4b94d0d165205f217213705d43dc792f266f8fdd
Alexey Filippov <alf@ebi.ac.uk>
2010-12-17 14:15:47 +0000

    Dropping unused code
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone@16069 2913f559-6b04-0410-9a09-c530ee9f5186

--- atlas-utils/src/main/java/uk/ac/ebi/gxa/utils/FileUtil.java
+++ atlas-utils/src/main/java/uk/ac/ebi/gxa/utils/FileUtil.java
@@ -23,16 +23,12 @@
 package uk.ac.ebi.gxa.utils;
 
 import java.io.File;
-import java.io.InputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 /**
  * File utility functions
  * @author pashky
  */
 public class FileUtil {
-
     /**
      * Creates guaranteed unqiue temporary directory in java.io.tmpdir space
      * The directory will have name containing specified prefix and some unique number
@@ -59,43 +55,5 @@ public class FileUtil {
                 deleteDirectory(file);
         }
         return dir.delete();
-    }
-
-    /**
-     * Creates directory and throws exception if fails
-     * @param path parent path
-     * @param local directory name
-     */
-    public static void createDirectory(File path, String local) {
-        if(!new File(path, local).mkdirs())
-            throw new RuntimeException("Can't create temporary directory: " + local + " in :" + path);
-    }
-
-    /**
-     * Writes file from resource, associated with class to specified path on disk
-     * @param clazz class to read source resource (by classloader)
-     * @param resource resource name
-     * @param basePath base path to write file to
-     * @param target local file name for target file
-     */
-    public static void writeFileFromResource(final Class clazz, String resource, File basePath, String target) {
-        File file = new File(basePath, target);
-
-        File path = file.getParentFile();
-        if(!path.exists() && !path.mkdirs())
-            throw new RuntimeException("Can't create target directories: " + path);
-
-        InputStream istream = clazz.getClassLoader().getResourceAsStream("META-INF/" + resource);
-        try {
-            FileOutputStream ostream = new FileOutputStream(file);
-            byte b[] = new byte[2048];
-            int i;
-            while((i = istream.read(b)) >= 0)
-                ostream.write(b, 0, i);
-            ostream.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
     }
 }
