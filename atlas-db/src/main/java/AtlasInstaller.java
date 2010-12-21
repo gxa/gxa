87ac137e7fb7e4d9e8d86eb9ea0a2ea86e368cea
Robert Petryszak <rpetry@ebi.ac.uk>
2010-12-21 14:26:48 +0000

    Modified to reflect changes in branches/atlas-standalone
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone-conciseindex@16140 2913f559-6b04-0410-9a09-c530ee9f5186

--- atlas-db/src/main/java/AtlasInstaller.java
+++ atlas-db/src/main/java/AtlasInstaller.java
@@ -1,126 +1,91 @@
+import com.google.common.io.Closeables;
 import org.apache.commons.dbcp.BasicDataSource;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * Created by IntelliJ IDEA.
- * User: Andrey
- * Date: Apr 26, 2010
- * Time: 1:12:20 PM
- * To change this template use File | Settings | File Templates.
- */
 public class AtlasInstaller {
-    public DataSource getDataSource() {
-        return dataSource;
-    }
+    private DataSource dataSource = null;
+    private JdbcTemplate jdbcTemplate = null;
 
     public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
     public JdbcTemplate getJdbcTemplate() {
-        if((null==jdbcTemplate)&&(null!=dataSource)){
+        if ((null == jdbcTemplate) && (null != dataSource)) {
             this.jdbcTemplate = new JdbcTemplate(dataSource);
         }
         return jdbcTemplate;
     }
 
-    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    private DataSource dataSource = null;
-    private JdbcTemplate jdbcTemplate = null;
-
-    private void runProcess(String workingFolder, String ... args) throws Exception{
+    private void runProcess(String workingFolder, String... args) throws Exception {
 
         ProcessBuilder pb = new ProcessBuilder(args); //,pathToInstall,connectionString);
 
-        if(null!=workingFolder){
+        if (null != workingFolder) {
             pb.directory(new File(workingFolder));
         }
 
         Process child = pb.start();
 
-        BufferedReader stdInput = new BufferedReader(new
-                InputStreamReader(child.getInputStream()));
+        BufferedReader stdInput = null;
+        BufferedReader stdError = null;
 
-        BufferedReader stdError = new BufferedReader(new
-                InputStreamReader(child.getInputStream()));
+        try {
+            stdInput = new BufferedReader(new
+                    InputStreamReader(child.getInputStream()));
+            stdError = new BufferedReader(new
+                    InputStreamReader(child.getErrorStream()));
 
-        String s;
-        while ((s = stdInput.readLine()) != null) {
-                 System.out.println(s);
-        }
+            String s;
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+            }
 
-        while ((s = stdError.readLine()) != null) {
-            System.out.println(s);
+            while ((s = stdError.readLine()) != null) {
+                System.err.println(s);
+            }
+        } finally {
+            Closeables.closeQuietly(stdError);
+            Closeables.closeQuietly(stdInput);
         }
 
         int retCode = child.waitFor();
 
-        if(0!=retCode)
-            throw new Exception(String.format("process executed returned %1$s",retCode));
+        if (0 != retCode)
+            throw new Exception(String.format("process executed returned %1$s", retCode));
     }
 
-    public void install() throws Exception{
-        Runtime rtime = Runtime.getRuntime();
-
-        ClassPathResource r = new ClassPathResource("install.sh");
-
-        //(new ClassPathResource("install.sh"))?
+    public void install() throws Exception {
         String pathToInstall = getClass().getResource("install.sh").getPath();
         String workingFolder = getClass().getResource("").getPath();
         String connectionUrl = ((BasicDataSource) dataSource).getUrl();
-        String connectionUsername =  ((BasicDataSource) dataSource).getUsername();
-        String connectionPassword =  ((BasicDataSource) dataSource).getPassword();
+        String connectionUsername = ((BasicDataSource) dataSource).getUsername();
+        String connectionPassword = ((BasicDataSource) dataSource).getPassword();
 
-        String connectionString =  String.format("%1$s/%2$s@%3$s"
-                                                  ,connectionUsername
-                                                  ,connectionPassword
-                                                  ,connectionUrl);
+        String connectionString = String.format("%1$s/%2$s@%3$s", connectionUsername, connectionPassword, connectionUrl);
 
         Pattern pattern = Pattern.compile("^jdbc:oracle:thin:@([\\w|.]+):(\\d+):(\\w+)$");
         Matcher matcher = pattern.matcher(connectionUrl);
-        if(matcher.find()){
+        if (matcher.find()) {
             String serverUrl = matcher.group(1);
             String serverPort = matcher.group(2);
             String serverServiceName = matcher.group(3);
 
-            connectionString =  String.format("%1$s/%2$s@%3$s:%4$s/%5$s"
-                                              ,connectionUsername
-                                              ,connectionPassword
-                                              ,serverUrl
-                                              ,serverPort
-                                              ,serverServiceName);
+            connectionString = String.format("%1$s/%2$s@%3$s:%4$s/%5$s", connectionUsername,
+                    connectionPassword, serverUrl, serverPort, serverServiceName);
         }
-
-        /*
-        String command = String.format("sh %1$s %2$s"
-                                       , pathToInstall
-                                       , connectionString);
-        */
 
         System.out.println(pathToInstall);
 
-        String command = String.format("sh %1$s %2$s\n",pathToInstall,connectionString);
-        command = String.format("chmod +x %1$s",pathToInstall);
+        runProcess(null, "chmod", "+x", pathToInstall);
 
-        int retCode = 0;
-
-        runProcess(null,"chmod","+x",pathToInstall);
-
-        runProcess(workingFolder,pathToInstall,connectionString);
+        runProcess(workingFolder, pathToInstall, connectionString);
     }
 }
--- atlas-db/src/test/java/AtlasInstallerTest.java
+++ atlas-db/src/test/java/AtlasInstallerTest.java
@@ -1,36 +1,16 @@
-import junit.framework.TestCase;
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.SingleConnectionDataSource;
-
-import javax.sql.DataSource;
-
-import java.io.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Properties;
-
-import static org.junit.Assert.assertTrue;
-
-/**
- * Created by IntelliJ IDEA.
- * User: Andrey
- * Date: Apr 26, 2010
- * Time: 1:17:20 PM
- * To change this template use File | Settings | File Templates.
- */
 
 public class AtlasInstallerTest {
 
     private static AtlasInstaller atlasInstaller;
 
     @BeforeClass
-    public static void setUpGlobal() throws Exception{
+    public static void setUpGlobal() throws Exception {
 
         BeanFactory factory =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
@@ -44,12 +24,6 @@ public class AtlasInstallerTest {
         atlasInstaller.install();
     }
 
-    @After
-    public void tearDown() throws Exception {
-
-
-    }
-
     @Test
     public void testInstall() throws Exception {
         String sql = "select count(1) from a2_experiment";
@@ -60,15 +34,15 @@ public class AtlasInstallerTest {
     }
 
     @Test
-    public void testVwOntology()  throws Exception{
+    public void testVwOntology() throws Exception {
         atlasInstaller.getJdbcTemplate().execute("update CUR_PropertyValue set Value = 'pseudoheart' where Property = 'organismpart'");
         atlasInstaller.getJdbcTemplate().execute("commit");
 
         String sql = "select PropertyValue from vwassayproperty where assayid=2 and property='organismpart'";
 
-        String result = (String) atlasInstaller.getJdbcTemplate().queryForObject(sql, String.class);
+        String result = atlasInstaller.getJdbcTemplate().queryForObject(sql, String.class);
 
-        Assert.assertEquals("property value", "pseudoheart" , result);
+        Assert.assertEquals("property value", "pseudoheart", result);
 
         atlasInstaller.getJdbcTemplate().execute("update CUR_PropertyValue set Value = 'heart' where Property = 'organismpart'");
         atlasInstaller.getJdbcTemplate().execute("commit");
