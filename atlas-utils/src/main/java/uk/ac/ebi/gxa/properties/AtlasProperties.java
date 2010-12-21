04a517da212834f100c6bcff2630ba6321f4b8cd
Robert Petryszak <rpetry@ebi.ac.uk>
2010-12-21 13:51:02 +0000

    Modified to reflect changes in branches/atlas-standalone
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone-conciseindex@16132 2913f559-6b04-0410-9a09-c530ee9f5186

--- atlas-utils/src/main/java/uk/ac/ebi/gxa/properties/AtlasProperties.java
+++ atlas-utils/src/main/java/uk/ac/ebi/gxa/properties/AtlasProperties.java
@@ -22,10 +22,19 @@
 package uk.ac.ebi.gxa.properties;
 
 import org.apache.commons.lang.StringUtils;
-
-import java.util.*;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.ReflectionUtils;
 import uk.ac.ebi.gxa.utils.LazyKeylessMap;
+import uk.ac.ebi.mydas.configuration.DataSourceConfiguration;
+import uk.ac.ebi.mydas.configuration.GlobalConfiguration;
+import uk.ac.ebi.mydas.configuration.Mydasserver;
+import uk.ac.ebi.mydas.configuration.ServerConfiguration;
+import uk.ac.ebi.mydas.controller.DataSourceManager;
+import uk.ac.ebi.mydas.controller.MydasServlet;
+
+import java.lang.reflect.Field;
+import java.util.*;
 
 /**
  * Atlas properties container class
@@ -34,6 +43,8 @@ import uk.ac.ebi.gxa.utils.LazyKeylessMap;
 public class AtlasProperties  {
 
     private Storage storage;
+
+    private final Logger log = LoggerFactory.getLogger(AtlasProperties.class);
 
     /**
      * Set storage for use
@@ -62,13 +73,17 @@ public class AtlasProperties  {
 
     /**
      * Sets new (or removes) property value
-     * @param key property name
+     *
+     * @param key      property name
      * @param newValue property value or null if property customization should be deleted
      */
     public void setProperty(String key, String newValue) {
-        storage.setProperty(key, newValue);
-        cache.put(key, newValue);
-        notifyListeners();
+        if (!key.equals("atlas.dasbase") || updateDasBaseURL(newValue)) {
+            // Update atlas.dasbase property only if we managed to MydasServer code was updated successfully
+            storage.setProperty(key, newValue);
+            cache.put(key, newValue);
+            notifyListeners();
+        }
     }
 
     /**
@@ -146,6 +161,10 @@ public class AtlasProperties  {
     /* Data release */
     public String getDataRelease() {
         return getProperty("atlas.data.release");
+    }
+
+    public String getDasBase() {
+        return getProperty("atlas.dasbase");
     }
 
     public String getLastReleaseDate() {
@@ -232,10 +251,6 @@ public class AtlasProperties  {
         return getIntProperty("atlas.query.listsize");
     }
 
-    public int getPlottedGeneListSize() {
-        return getIntProperty("atlas.query.plottedGeneListSize");
-    }
-
     public int getQueryExperimentsPerGene() {
         return getIntProperty("atlas.query.expsPerGene");
     }
@@ -269,6 +284,10 @@ public class AtlasProperties  {
         return getProperty("atlas.experiments.dump.ebeye.filename");
     }
 
+    public String getExperimentsToPropertiesDumpFilename() {
+        return getProperty("atlas.experiments.properties.dump.filename");
+    }
+
     public String getGenesDumpEbeyeFilename() {
         return getProperty("atlas.genes.dump.ebeye.filename");
     }
@@ -281,10 +300,6 @@ public class AtlasProperties  {
 
     public List<String> getAnyConditionIgnoredEfs() {
         return getListProperty("atlas.ignore.efs.anycondition");
-    }
-
-    public List<String> getFacetIgnoredEfs() {
-        return getListProperty("atlas.ignore.efs.facet");
     }
 
     public List<String> getGeneHeatmapIgnoredEfs() {
@@ -350,7 +365,8 @@ public class AtlasProperties  {
 
     /* R & Biocep */
     public String getRMode() {
-        return getProperty("atlas.rservice.mode");
+        return "local"; // TODO
+        // return getProperty("atlas.rservice.mode");
     }
 
     public Properties getRProperties() {
@@ -358,7 +374,7 @@ public class AtlasProperties  {
         Properties result = new Properties();
         for(String property : getAvailablePropertyNames()) {
             if(property.startsWith(prefix))
-                result.setProperty(property.substring(prefix.length()), getProperty(property));
+                result.setProperty(property.substring(prefix.length()), "local"); // TODO getProperty(property)
         }
         return result;
     }
@@ -375,6 +391,7 @@ public class AtlasProperties  {
         return getBoolProperty("atlas.look.cache.enabled");
 	}
 
+    @SuppressWarnings("unused") // used in look/footer.html file, with homegrown {@link ae3.util.TemplateTag}
     public String getGoogleAnalyticsAccount() {
         return getProperty("atlas.googleanalytics.account");
     }
@@ -401,5 +418,61 @@ public class AtlasProperties  {
 
     public List<String> getDasFactors() {
 	return getListProperty("atlas.dasfactors");
+    }
+
+    /**
+     * MydasServlet, used by Atlas to expose its data as a DAS source, is configured at start up via MydasServerConfig.xml.
+     * Maven build replaces atlas.dasbase placeholder in MydasServerConfig.xml with a value set in atlas-web/pom.xml
+     * atlas.dasbase property is also configurable via AtlasProperties, but since MydasServlet code does not currently
+     * provide access to its internal fields using atlas.dasbase, the only current way to re-configure MydasServlet code after
+     * an AtlasProperties change to atlas.dasbase is vai the reflection hack below.
+     * TODO replace this method with direct calls to MydasServlet code once setter methods are provided by the DAS team
+     *
+     * @param dasBaseURL base URL for DAS
+     * @return true if all fields were updated via reflection successfully; false otherwise
+     */
+    public boolean updateDasBaseURL(String dasBaseURL) {
+        boolean success = false;
+        try {
+            Field field = ReflectionUtils.findField(MydasServlet.class, "DATA_SOURCE_MANAGER");
+            field.setAccessible(true);
+            Object dataSourceManager = ReflectionUtils.getField(field, null);
+            if (dataSourceManager != null) {
+                // GxaS4DasDataSource has been accessed at least once since Atlas startup and MydasServerConfig.xml was already
+                // read in by MydasServlet - need to update the relevant object fields via reflection
+                // web.xml is now configured to load MydasServlet at Atlas startup - if it is not loaded by the time
+                // this method runs 
+                ServerConfiguration serverConfiguration = ((DataSourceManager) dataSourceManager).getServerConfiguration();
+
+                // Set baseUrl to dasBaseURL - c.f. <baseurl>${atlas.dasbase}/</baseurl> in MydasServerConfig.xml
+                GlobalConfiguration globalConfiguration = serverConfiguration.getGlobalConfiguration();
+                Field baseUrl = globalConfiguration.getClass().getDeclaredField("baseURL");
+                baseUrl.setAccessible(true);
+                baseUrl.set(globalConfiguration, dasBaseURL);
+                log.debug("Setting <baseurl> MydasServerConfig.xml to: dasBaseURL");
+
+                // Update all capability fields with the new dasBaseURL - c.f. (in MydasServerConfig.xml)
+                //    <capability type="das1:sources" query_uri="${atlas.dasbase}/s4" />
+                //    <capability type="das1:types" query_uri="${atlas.dasbase}/s4/types" />
+                //    <capability type="das1:features" query_uri="${atlas.dasbase}/s4/features?segment=ENSG00000162552" />
+                Map<String, DataSourceConfiguration> dataSourceConfigMap = serverConfiguration.getDataSourceConfigMap();
+                for (DataSourceConfiguration dsConfig : dataSourceConfigMap.values()) {
+                    List<Mydasserver.Datasources.Datasource.Version> versions = dsConfig.getConfig().getVersion();
+                    for (Mydasserver.Datasources.Datasource.Version version : versions) {
+                        List<Mydasserver.Datasources.Datasource.Version.Capability> capabilities = version.getCapability();
+                        for (Mydasserver.Datasources.Datasource.Version.Capability capability : capabilities) {
+                            String queryUri = capability.getQueryUri();
+                            String newQueryUri = queryUri.replaceFirst(".*/", dasBaseURL + "/");
+                            log.debug("Setting query_uri of capability type: " + capability.getType() + " in MydasServerConfig.xml to " + newQueryUri);
+                            capability.setQueryUri(newQueryUri);
+                        }
+                    }
+                }
+                success = true;
+            }
+        } catch (Exception e) {
+            log.error("Failed to update dasBaseUrl to : " + dasBaseURL, e);
+        }
+        return success;
     }
 }
