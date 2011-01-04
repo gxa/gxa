a6f6b31f5ec3dcfb13f30ba48ee291eaa7fd463a
Robert Petryszak <rpetry@ebi.ac.uk>
2011-01-04 11:00:54 +0000

    Removed serialVersionUID
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone-conciseindex@16214 2913f559-6b04-0410-9a09-c530ee9f5186

--- atlas-utils/src/main/java/uk/ac/ebi/gxa/statistics/Attribute.java
+++ atlas-utils/src/main/java/uk/ac/ebi/gxa/statistics/Attribute.java
@@ -11,7 +11,6 @@ import java.io.Serializable;
  */
 public class Attribute implements Serializable {
     private String value;
-    private static final long serialVersionUID = 2857942714074833933L;
     private transient boolean isEfo;
     private transient StatisticsType statType;
 
