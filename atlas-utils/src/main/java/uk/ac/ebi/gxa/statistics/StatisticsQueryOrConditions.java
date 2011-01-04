43c000b98146ed15281761f6b259a6ecd4a28c73
Robert Petryszak <rpetry@ebi.ac.uk>
2011-01-04 17:27:09 +0000

    Remove efoTerm, as we now group multiple efos' mappings into a single StatisticsQueryOrConditions
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone-conciseindex@16237 2913f559-6b04-0410-9a09-c530ee9f5186

--- atlas-utils/src/main/java/uk/ac/ebi/gxa/statistics/StatisticsQueryOrConditions.java
+++ atlas-utils/src/main/java/uk/ac/ebi/gxa/statistics/StatisticsQueryOrConditions.java
@@ -13,9 +13,7 @@ import java.util.Set;
  */
 public class StatisticsQueryOrConditions<ConditionType> {
     private Set<ConditionType> orConditions = new HashSet<ConditionType>();
-    // If orConditions represents an OR collection of experiment-efv combinations
-    // corresponding to an efo term, efoTerm is used to hold that term's name.
-    String efoTerm = null;
+
     // Set of gene ids of interest to which this query is restricted
     private Set<Long> geneRestrictionSet = null;
 
@@ -23,14 +21,6 @@ public class StatisticsQueryOrConditions<ConditionType> {
      * Constructor
      */
     public StatisticsQueryOrConditions() {
-    }
-
-    public String getEfoTerm() {
-        return efoTerm;
-    }
-
-    public void setEfoTerm(String efoTerm) {
-        this.efoTerm = efoTerm;
     }
 
     public void orCondition(ConditionType condition) {
