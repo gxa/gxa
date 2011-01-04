3f902d175a0058de3528d3541447e0eb98325df0
Robert Petryszak <rpetry@ebi.ac.uk>
2011-01-04 11:30:15 +0000

    Made private recursive prettyPrint(offset) method; Added comments; removed unused methods
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone-conciseindex@16216 2913f559-6b04-0410-9a09-c530ee9f5186

--- atlas-utils/src/main/java/uk/ac/ebi/gxa/statistics/StatisticsQueryCondition.java
+++ atlas-utils/src/main/java/uk/ac/ebi/gxa/statistics/StatisticsQueryCondition.java
@@ -13,29 +13,45 @@ import java.util.Set;
  */
 public class StatisticsQueryCondition {
     // StatisticsQueryCondition in practice will have either:
-    // 1. just andConditions or
-    // 2. experiments AND attributes (AND query - with experiments and attributes in themselves being OR queries)
+    // 1. just andConditions, or
+    // 2. experiments AND attributes (i.e. an AND query - with experiments and attributes in themselves being OR queries)
     private Set<StatisticsQueryOrConditions<StatisticsQueryCondition>> andConditions = new HashSet<StatisticsQueryOrConditions<StatisticsQueryCondition>>();
     private Set<Experiment> experiments = new HashSet<Experiment>();  // OR set of experiments
     private Set<Attribute> attributes = new HashSet<Attribute>(); // OR set of attributes
     private StatisticsType statisticsType;
     private Set<Long> geneRestrictionSet = null;
-    
 
 
+    private static final String INITIAL_PRETTY_PRINT_OFFSET = "";
     private static final String PRETTY_PRINT_OFFSET = "  ";
 
+    /**
+     * Constructor
+     */
     public StatisticsQueryCondition() {
     }
 
+    /**
+     * Constructor
+     *
+     * @param statisticsType
+     */
     public StatisticsQueryCondition(StatisticsType statisticsType) {
         this.statisticsType = statisticsType;
     }
 
+    /**
+     * Constructor
+     *
+     * @param geneRestrictionSet
+     */
     public StatisticsQueryCondition(Set<Long> geneRestrictionSet) {
         this.geneRestrictionSet = geneRestrictionSet;
     }
 
+    /**
+     * @param geneRestrictionSet Set of gene Ids of interest, to which this query condition should be restricted. 
+     */
     public void setGeneRestrictionSet(Set<Long> geneRestrictionSet) {
         this.geneRestrictionSet = geneRestrictionSet;
     }
@@ -48,23 +64,13 @@ public class StatisticsQueryCondition {
         return statisticsType;
     }
 
-    public StatisticsQueryCondition and(StatisticsQueryCondition condition, String efoTerm) {
-        StatisticsQueryOrConditions<StatisticsQueryCondition> StatisticsQueryOrConditions = new StatisticsQueryOrConditions<StatisticsQueryCondition>(condition);
-        StatisticsQueryOrConditions.setEfoTerm(efoTerm);
-        andConditions.add(StatisticsQueryOrConditions);
-        return this;
-    }
-
+    /**
+     *
+     * @param statisticsQueryOrConditions
+     * @return StatisticsQueryCondition containing an OR clause of statisticsQueryOrConditions, restricted to geneRestrictionSet
+     */
     public StatisticsQueryCondition and(StatisticsQueryOrConditions<StatisticsQueryCondition> statisticsQueryOrConditions) {
         statisticsQueryOrConditions.setGeneRestrictionSet(geneRestrictionSet);
-        andConditions.add(statisticsQueryOrConditions);
-        return this;
-    }
-
-    public StatisticsQueryCondition and(Collection<StatisticsQueryCondition> conditions, String efoTerm) {
-        StatisticsQueryOrConditions<StatisticsQueryCondition> statisticsQueryOrConditions = new StatisticsQueryOrConditions<StatisticsQueryCondition>(conditions);
-        statisticsQueryOrConditions.setGeneRestrictionSet(geneRestrictionSet);
-        statisticsQueryOrConditions.setEfoTerm(efoTerm);
         andConditions.add(statisticsQueryOrConditions);
         return this;
     }
@@ -73,11 +79,11 @@ public class StatisticsQueryCondition {
         return andConditions;
     }
 
-    public StatisticsQueryCondition inExperiment(Experiment experiment) {
-        experiments.add(experiment);
-        return this;
-    }
-
+    /**
+     *
+     * @param experiments
+     * @return this query condition with experiments added to its experiments OR clause
+     */
     public StatisticsQueryCondition inExperiments(Collection<Experiment> experiments) {
         this.experiments.addAll(experiments);
         return this;
@@ -87,6 +93,11 @@ public class StatisticsQueryCondition {
         return experiments;
     }
 
+    /**
+     * 
+     * @param attribute
+     * @return this query condition with attribute added to its attributes OR clause
+     */
     public StatisticsQueryCondition inAttribute(Attribute attribute) {
         this.attributes.add(attribute);
         return this;
@@ -96,11 +107,22 @@ public class StatisticsQueryCondition {
         return attributes;
     }
 
-    public String prettyPrint(String offset) {
+
+    public String prettyPrint() {
+        return prettyPrint(INITIAL_PRETTY_PRINT_OFFSET);
+    }
+
+    /**
+     * Recursive method to pretty print this StatisticsQueryCondition
+     *
+     * @param offset
+     * @return
+     */
+    private String prettyPrint(String offset) {
         StringBuilder sb = new StringBuilder();
         Set<StatisticsQueryOrConditions<StatisticsQueryCondition>> andGeneConditions = getConditions();
         if (getGeneRestrictionSet() != null) {
-           sb.append("\n(GeneRestrictionSet size = " + getGeneRestrictionSet().size()).append(") "); 
+            sb.append("\n(GeneRestrictionSet size = " + getGeneRestrictionSet().size()).append(") ");
         }
         if (!andGeneConditions.isEmpty()) {
             sb.append("\n").append(offset).append(" [ ");
