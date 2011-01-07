5f15c5e544196c0f3bd69a3710fb97ac9f11c5ec
Robert Petryszak <rpetry@ebi.ac.uk>
2011-01-07 15:23:54 +0000

    Added efvToEfoIndex
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone-conciseindex@16271 2913f559-6b04-0410-9a09-c530ee9f5186

--- atlas-utils/src/main/java/uk/ac/ebi/gxa/statistics/EfoIndex.java
+++ atlas-utils/src/main/java/uk/ac/ebi/gxa/statistics/EfoIndex.java
@@ -18,6 +18,9 @@ public class EfoIndex implements Serializable {
     // efoTerm -> experiment index -> Set<Attribute Index>
     private Map<String, Map<Integer, Set<Integer>>> efoIndex = new HashMap<String, Map<Integer, Set<Integer>>>();
 
+    // Attribute -> experiment index -> efoTerm
+    private Map<Integer, Map<Integer, String>> efvToEfoIndex = new HashMap<Integer, Map<Integer, String>>();
+
     public void addMapping(String efoTerm, Integer attributeIndex, Integer experimentIndex) {
         if (!efoIndex.containsKey(efoTerm)) {
             Map<Integer, Set<Integer>> expToAttrs = new HashMap<Integer, Set<Integer>>();
@@ -29,11 +32,32 @@ public class EfoIndex implements Serializable {
             efoIndex.get(efoTerm).put(experimentIndex, attrs);
         }
         efoIndex.get(efoTerm).get(experimentIndex).add(attributeIndex);
+
+        if (!efvToEfoIndex.containsKey(attributeIndex)) {
+            Map<Integer, String> expToEfo = new HashMap<Integer, String>();
+            efvToEfoIndex.put(attributeIndex, expToEfo);
+        }
+        efvToEfoIndex.get(attributeIndex).put(experimentIndex, efoTerm);
     }
 
     public Map<Integer, Set<Integer>> getMappingsForEfo(String efoTerm) {
         return efoIndex.get(efoTerm);
     }
+
+    /**
+     *
+     * @param attributeIndex
+     * @param expIndex
+     * @return an efo term one of whose mapping is an efv referenced by attributeIndex in a an experiment expIndex
+     */
+    public String getEfoTerm(Integer attributeIndex, Integer expIndex) {
+        Map<Integer, String> expToEfo = efvToEfoIndex.get(attributeIndex);
+        if (expToEfo != null) {
+            return expToEfo.get(expIndex);
+        }
+        return null;
+    }
+
 
     public Set<String> getEfos() {
         return efoIndex.keySet();
