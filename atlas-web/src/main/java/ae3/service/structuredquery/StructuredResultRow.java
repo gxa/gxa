5f0938dc3f5c50079fe2f34ccc79712f9547e719
Robert Petryszak <rpetry@ebi.ac.uk>
2011-01-18 08:49:32 +0000

    Added totalUpDnStudies and totalNonDEStudies - to speed up insertion of objects of this class to a SortedSet
    git-svn-id: svn://bar.ebi.ac.uk/branches/atlas-standalone-conciseindex@16391 2913f559-6b04-0410-9a09-c530ee9f5186

--- atlas-web/src/main/java/ae3/service/structuredquery/StructuredResultRow.java
+++ atlas-web/src/main/java/ae3/service/structuredquery/StructuredResultRow.java
@@ -34,6 +34,8 @@ public class StructuredResultRow implements Comparable<StructuredResultRow>{
     private AtlasGene gene;
 
     private List<UpdownCounter> updownCounters;
+    private Integer totalUpDnStudies;
+    private Integer totalNonDEStudies;
 
     public StructuredResultRow(AtlasGene gene, List<UpdownCounter> updownCounters) {
         this.gene = gene;
@@ -52,22 +54,26 @@ public class StructuredResultRow implements Comparable<StructuredResultRow>{
      * @return sum total of studies from updownCounters
      */
     public Integer getTotalUpDnStudies() {
-        Integer totalStudies = 0;
-        for (UpdownCounter counter : updownCounters) {
-            totalStudies += counter.getNoStudies();
+        if (totalUpDnStudies == null) {
+            totalUpDnStudies = 0;
+            for (UpdownCounter counter : updownCounters) {
+                totalUpDnStudies += counter.getNoStudies();
+            }
         }
-        return totalStudies;
+        return totalUpDnStudies;
     }
 
      /**
      * @return sum total of studies from updownCounters
      */
     public Integer getTotalNoneDEStudies() {
-        Integer totalNoneDEStudies = 0;
-        for (UpdownCounter counter : updownCounters) {
-            totalNoneDEStudies += counter.getNones();
-        }
-        return totalNoneDEStudies;
+         if (totalNonDEStudies == null) {
+             totalNonDEStudies = 0;
+             for (UpdownCounter counter : updownCounters) {
+                 totalNonDEStudies += counter.getNones();
+             }
+         }
+         return totalNonDEStudies;
     }
 
     public boolean isZero() {
@@ -75,21 +81,22 @@ public class StructuredResultRow implements Comparable<StructuredResultRow>{
     }
 
     /**
-     * TODO
+     * StructuredResultRows are compared
      * @param o
      * @return
      */
     public int compareTo(StructuredResultRow o) {
-        if (getTotalUpDnStudies() == o.getTotalUpDnStudies() && getTotalUpDnStudies() == 0)
-            return -Integer.valueOf(getTotalNoneDEStudies()).compareTo(o.getTotalNoneDEStudies());
-        else if (getTotalUpDnStudies() == o.getTotalUpDnStudies()) {
-            if (getGene().getGeneName() == null) {
-                return 1;
-            } else if (o.getGene().getGeneName() == null) {
-                return -1;
-            } else
-                return Integer.valueOf(getGene().getGeneName().compareTo(o.getGene().getGeneName()));
-        } else
+        if (getTotalUpDnStudies() != o.getTotalUpDnStudies())
             return -Integer.valueOf(getTotalUpDnStudies()).compareTo(o.getTotalUpDnStudies());
+        else if (getTotalNoneDEStudies() != null && o.getTotalNoneDEStudies() != null && getTotalNoneDEStudies() != o.getTotalNoneDEStudies()) {
+           return -Integer.valueOf(getTotalNoneDEStudies()).compareTo(o.getTotalNoneDEStudies());
+        }
+
+        if (getGene().getGeneName() == null) {
+            return 1;
+        } else if (o.getGene().getGeneName() == null) {
+            return -1;
+        } else
+            return Integer.valueOf(getGene().getGeneName().compareTo(o.getGene().getGeneName()));
     }
 }
