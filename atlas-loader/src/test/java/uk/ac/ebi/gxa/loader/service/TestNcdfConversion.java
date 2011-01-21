2ec00f441ab7243d9d3e03adb0c4f42874100b74
Alexey Filippov <alf@ebi.ac.uk>
2011-01-21 14:04:34 +0000

    Test scenario to start with

---
+++ atlas-loader/src/test/java/uk/ac/ebi/gxa/loader/service/TestNcdfConversion.java
@@ -0,0 +1,17 @@
+package uk.ac.ebi.gxa.loader.service;
+
+import org.junit.Test;
+
+/**
+ */
+public class TestNcdfConversion {
+    @Test
+    public void testExportExperiment() {
+        // Get an experiment from DB/NetCDF
+        // run update on it - not it'll rewrite the file
+        // make sure all the DB fields are shown in the NetCDF and IDF files properly
+        // release the experiment
+        // import the experiment
+        // make sure all the fields are still the same
+    }
+}
