85813017662b2763799d4ba479f37c274eaf36e6
Alexey Filippov <alf@ebi.ac.uk>
2011-01-25 16:24:59 +0000

    Code clarified: proper logging, not swallowing the exception

--- netcdf-reader/src/test/java/uk/ac/ebi/gxa/netcdf/reader/TestResourceHandling.java
+++ netcdf-reader/src/test/java/uk/ac/ebi/gxa/netcdf/reader/TestResourceHandling.java
@@ -1,14 +1,19 @@
 package uk.ac.ebi.gxa.netcdf.reader;
 
-import com.google.common.io.Closeables;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import static com.google.common.io.Closeables.close;
+
 public class TestResourceHandling {
+    private static final Logger log = LoggerFactory.getLogger(TestResourceHandling.class);
+
     @Test
     public void testOpenClose() throws IOException, URISyntaxException {
         File netCDFfile = new File(getClass().getClassLoader().getResource("MEXP/1500/E-MEXP-1586/1036804667_160588088.nc").toURI());
@@ -18,10 +23,10 @@ public class TestResourceHandling {
                 netCDF = new NetCDFProxy(netCDFfile);
                 netCDF.getArrayDesignID();
             } catch (FileNotFoundException e) {
-                System.out.println("i = " + i);
+                log.error("Out of file handles on attempt #" + i, e);
                 throw e;
             } finally {
-                Closeables.closeQuietly(netCDF);
+                close(netCDF, false);
             }
         }
     }
