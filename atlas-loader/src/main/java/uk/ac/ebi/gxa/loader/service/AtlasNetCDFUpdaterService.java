2766cae2e48436210aad99d870fe650220976d71
Alexey Filippov <alf@ebi.ac.uk>
2011-01-27 17:51:25 +0000

    Refactoring the code which preserves T and P stats during the update

--- atlas-loader/src/main/java/uk/ac/ebi/gxa/loader/service/AtlasNetCDFUpdaterService.java
+++ atlas-loader/src/main/java/uk/ac/ebi/gxa/loader/service/AtlasNetCDFUpdaterService.java
@@ -1,6 +1,5 @@
 package uk.ac.ebi.gxa.loader.service;
 
-import com.google.common.base.Function;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.ac.ebi.gxa.loader.AtlasLoaderException;
@@ -10,24 +9,25 @@ import uk.ac.ebi.gxa.loader.datamatrix.DataMatrixStorage;
 import uk.ac.ebi.gxa.netcdf.generator.NetCDFCreator;
 import uk.ac.ebi.gxa.netcdf.generator.NetCDFCreatorException;
 import uk.ac.ebi.gxa.netcdf.reader.NetCDFProxy;
+import uk.ac.ebi.gxa.utils.CBitSet;
+import uk.ac.ebi.gxa.utils.CPair;
 import uk.ac.ebi.gxa.utils.EfvTree;
-import uk.ac.ebi.gxa.utils.Maker;
 import uk.ac.ebi.gxa.utils.Pair;
 import uk.ac.ebi.microarray.atlas.model.ArrayDesign;
 import uk.ac.ebi.microarray.atlas.model.Assay;
 import uk.ac.ebi.microarray.atlas.model.Experiment;
 import uk.ac.ebi.microarray.atlas.model.Sample;
 
-import javax.annotation.Nonnull;
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 import static com.google.common.collect.Iterators.concat;
-import static com.google.common.collect.Lists.transform;
 import static com.google.common.io.Closeables.closeQuietly;
 import static com.google.common.primitives.Floats.asList;
 import static uk.ac.ebi.gxa.netcdf.reader.AtlasNetCDFDAO.getNetCDFLocation;
+import static uk.ac.ebi.gxa.utils.CollectionUtil.distinct;
+import static uk.ac.ebi.gxa.utils.CollectionUtil.multiget;
 
 /**
  * NetCDF updater service which preserves expression values information, but updates all properties
@@ -39,94 +39,6 @@ public class AtlasNetCDFUpdaterService extends AtlasLoaderService {
 
     public AtlasNetCDFUpdaterService(DefaultAtlasLoader atlasLoader) {
         super(atlasLoader);
-    }
-
-    static class CBitSet extends BitSet implements Comparable<CBitSet> {
-        private CBitSet(int nbits) {
-            super(nbits);
-        }
-
-        public int compareTo(CBitSet o) {
-            for (int i = 0; i < Math.max(size(), o.size()); ++i) {
-                boolean b1 = get(i);
-                boolean b2 = o.get(i);
-                if (b1 != b2)
-                    return b1 ? 1 : -1;
-            }
-            return 0;
-        }
-    }
-
-    static class CPair<T1 extends Comparable<T1>, T2 extends Comparable<T2>> extends Pair<T1, T2> implements Comparable<CPair<T1, T2>> {
-        private CPair(T1 first, T2 second) {
-            super(first, second);
-        }
-
-        public int compareTo(CPair<T1, T2> o) {
-            int d = getFirst().compareTo(o.getFirst());
-            return d != 0 ? d : getSecond().compareTo(o.getSecond());
-        }
-    }
-
-    private EfvTree<CPair<String, String>> matchEfvs(EfvTree<CBitSet> from, EfvTree<CBitSet> to) {
-        final List<EfvTree.Ef<CBitSet>> fromTree = matchEfvsSort(from);
-        final List<EfvTree.Ef<CBitSet>> toTree = matchEfvsSort(to);
-
-        EfvTree<CPair<String, String>> result = new EfvTree<CPair<String, String>>();
-        for (EfvTree.Ef<CBitSet> toEf : toTree) {
-            boolean matched = false;
-            for (EfvTree.Ef<CBitSet> fromEf : fromTree)
-                if (fromEf.getEfvs().size() == toEf.getEfvs().size()) {
-                    int i;
-                    for (i = 0; i < fromEf.getEfvs().size(); ++i)
-                        if (!fromEf.getEfvs().get(i).getPayload().equals(toEf.getEfvs().get(i).getPayload()))
-                            break;
-                    if (i == fromEf.getEfvs().size()) {
-                        for (i = 0; i < fromEf.getEfvs().size(); ++i)
-                            result.put(toEf.getEf(), toEf.getEfvs().get(i).getEfv(),
-                                    new CPair<String, String>(fromEf.getEf(), fromEf.getEfvs().get(i).getEfv()));
-                        matched = true;
-                    }
-                }
-            if (!matched)
-                return null;
-        }
-        return result;
-    }
-
-    private List<EfvTree.Ef<CBitSet>> matchEfvsSort(EfvTree<CBitSet> from) {
-        final List<EfvTree.Ef<CBitSet>> fromTree = from.getNameSortedTree();
-        for (EfvTree.Ef<CBitSet> ef : fromTree) {
-            Collections.sort(ef.getEfvs(), new Comparator<EfvTree.Efv<CBitSet>>() {
-                public int compare(EfvTree.Efv<CBitSet> o1, EfvTree.Efv<CBitSet> o2) {
-                    return o1.getPayload().compareTo(o2.getPayload());
-                }
-            });
-        }
-        return fromTree;
-    }
-
-
-    private EfvTree<CBitSet> getEfvPatternsFromAssays(final List<Assay> assays) {
-        Set<String> efs = new HashSet<String>();
-        for (Assay assay : assays)
-            efs.addAll(assay.getPropertyNames());
-
-        EfvTree<CBitSet> efvTree = new EfvTree<CBitSet>();
-        int i = 0;
-        for (Assay assay : assays) {
-            for (final String propName : efs) {
-                String value = assay.getPropertySummary(propName);
-                efvTree.getOrCreate(propName, value, new Maker<CBitSet>() {
-                    public CBitSet make() {
-                        return new CBitSet(assays.size());
-                    }
-                }).set(i, true);
-            }
-            ++i;
-        }
-
-        return efvTree;
     }
 
     public void process(UpdateNetCDFForExperimentCommand cmd, AtlasLoaderServiceListener listener) throws AtlasLoaderException {
@@ -158,7 +70,7 @@ public class AtlasNetCDFUpdaterService extends AtlasLoaderService {
             log.info("Starting NetCDF for " + experimentAccession +
                     " and " + arrayDesignAccession + " (" + arrayDesignAssays.size() + " assays)");
 
-            NetCDFData netCDF = readNetCDF(this, netCDFLocation, arrayDesignAssays);
+            NetCDFData netCDF = readNetCDF(netCDFLocation, arrayDesignAssays);
 
             listener.setProgress("Writing updated NetCDF");
 
@@ -168,7 +80,7 @@ public class AtlasNetCDFUpdaterService extends AtlasLoaderService {
         }
     }
 
-    private static NetCDFData readNetCDF(AtlasNetCDFUpdaterService atlasNetCDFUpdaterService, File source, Map<Long, Assay> arrayDesignAssays) throws AtlasLoaderException {
+    private static NetCDFData readNetCDF(File source, Map<Long, Assay> arrayDesignAssays) throws AtlasLoaderException {
         NetCDFProxy reader = null;
         try {
             reader = new NetCDFProxy(source);
@@ -186,21 +98,7 @@ public class AtlasNetCDFUpdaterService extends AtlasLoaderService {
             }
 
             if (assays.length == result.assays.size()) {
-                // We depend on the enclosing class from here
-                EfvTree<CBitSet> oldEfvPats = new EfvTree<CBitSet>();
-                for (String ef : reader.getFactors()) {
-                    String[] efvs = reader.getFactorValues(ef);
-                    for (String efv : new HashSet<String>(Arrays.asList(efvs))) {
-                        CBitSet pattern = new CBitSet(efvs.length);
-                        for (int i = 0; i < efvs.length; ++i)
-                            pattern.set(i, efvs[i].equals(efv));
-                        oldEfvPats.put(ef, efv, pattern);
-                    }
-                }
-
-                EfvTree<CBitSet> newEfvPats = atlasNetCDFUpdaterService.getEfvPatternsFromAssays(result.assays);
-                result.matchedEfvs = atlasNetCDFUpdaterService.matchEfvs(oldEfvPats, newEfvPats);
-                // We depend on the enclosing class up to here
+                result.matchEfvPatterns(getEfvPatterns(reader));
             }
 
             result.uEFVs = reader.getUniqueFactorValues();
@@ -281,19 +179,17 @@ public class AtlasNetCDFUpdaterService extends AtlasLoaderService {
         }
     }
 
-    /**
-     * Picks a list of elements according to the list of indices supplied
-     *
-     * @param source  the source to pick elements from
-     * @param indices the indices to get
-     * @return a list of floats according to the list of indices supplied
-     */
-    private static <T> List<T> multiget(final List<T> source, List<Integer> indices) {
-        return transform(indices,
-                new Function<Integer, T>() {
-                    public T apply(@Nonnull Integer j) {
-                        return source.get(j);
-                    }
-                });
+    private static EfvTree<CBitSet> getEfvPatterns(NetCDFProxy reader) throws IOException {
+        EfvTree<CBitSet> patterns = new EfvTree<CBitSet>();
+        for (String ef : reader.getFactors()) {
+            String[] efvs = reader.getFactorValues(ef);
+            for (String efv : distinct(Arrays.asList(efvs))) {
+                CBitSet pattern = new CBitSet(efvs.length);
+                for (int i = 0; i < efvs.length; i++)
+                    pattern.set(i, efvs[i].equals(efv));
+                patterns.put(ef, efv, pattern);
+            }
+        }
+        return patterns;
     }
 }
--- atlas-loader/src/main/java/uk/ac/ebi/gxa/loader/service/NetCDFData.java
+++ atlas-loader/src/main/java/uk/ac/ebi/gxa/loader/service/NetCDFData.java
@@ -1,15 +1,82 @@
 package uk.ac.ebi.gxa.loader.service;
 
 import uk.ac.ebi.gxa.loader.datamatrix.DataMatrixStorage;
+import uk.ac.ebi.gxa.utils.CBitSet;
+import uk.ac.ebi.gxa.utils.CPair;
 import uk.ac.ebi.gxa.utils.EfvTree;
+import uk.ac.ebi.gxa.utils.Maker;
 import uk.ac.ebi.microarray.atlas.model.Assay;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
+import static java.util.Collections.sort;
 
 public class NetCDFData {
-    EfvTree<AtlasNetCDFUpdaterService.CPair<String, String>> matchedEfvs = null;
+    EfvTree<CPair<String, String>> matchedEfvs = null;
     List<Assay> assays = new ArrayList<Assay>();
     DataMatrixStorage storage;
     String[] uEFVs;
+
+    void matchEfvPatterns(EfvTree<CBitSet> oldEfvPats) {
+        matchedEfvs = matchEfvs(oldEfvPats, getEfvPatternsFromAssays());
+    }
+
+    EfvTree<CBitSet> getEfvPatternsFromAssays() {
+        Set<String> efs = new HashSet<String>();
+        for (Assay assay : assays)
+            efs.addAll(assay.getPropertyNames());
+
+        EfvTree<CBitSet> efvTree = new EfvTree<CBitSet>();
+        int i = 0;
+        for (Assay assay : assays) {
+            for (final String propName : efs) {
+                String value = assay.getPropertySummary(propName);
+                efvTree.getOrCreate(propName, value, new Maker<CBitSet>() {
+                    public CBitSet make() {
+                        return new CBitSet(assays.size());
+                    }
+                }).set(i, true);
+            }
+            ++i;
+        }
+
+        return efvTree;
+    }
+
+    private EfvTree<CPair<String, String>> matchEfvs(EfvTree<CBitSet> from, EfvTree<CBitSet> to) {
+        final List<EfvTree.Ef<CBitSet>> fromTree = matchEfvsSort(from);
+        final List<EfvTree.Ef<CBitSet>> toTree = matchEfvsSort(to);
+
+        EfvTree<CPair<String, String>> result = new EfvTree<CPair<String, String>>();
+        for (EfvTree.Ef<CBitSet> toEf : toTree) {
+            for (EfvTree.Ef<CBitSet> fromEf : fromTree) {
+                if (fromEf.getEfvs().size() != toEf.getEfvs().size()) {
+                    continue;
+                }
+                int i;
+                for (i = 0; i < fromEf.getEfvs().size(); ++i)
+                    if (!fromEf.getEfvs().get(i).getPayload().equals(toEf.getEfvs().get(i).getPayload()))
+                        return null;
+                if (i == fromEf.getEfvs().size()) {
+                    for (i = 0; i < fromEf.getEfvs().size(); ++i)
+                        result.put(toEf.getEf(), toEf.getEfvs().get(i).getEfv(),
+                                new CPair<String, String>(fromEf.getEf(), fromEf.getEfvs().get(i).getEfv()));
+                }
+            }
+        }
+        return result;
+    }
+
+    private List<EfvTree.Ef<CBitSet>> matchEfvsSort(EfvTree<CBitSet> from) {
+        final List<EfvTree.Ef<CBitSet>> fromTree = from.getNameSortedTree();
+        for (EfvTree.Ef<CBitSet> ef : fromTree) {
+            sort(ef.getEfvs(), new Comparator<EfvTree.Efv<CBitSet>>() {
+                public int compare(EfvTree.Efv<CBitSet> o1, EfvTree.Efv<CBitSet> o2) {
+                    return o1.getPayload().compareTo(o2.getPayload());
+                }
+            });
+        }
+        return fromTree;
+    }
+
 }
