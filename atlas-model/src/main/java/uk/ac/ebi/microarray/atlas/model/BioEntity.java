f316b2a8f72569c77f4485034dae25c369bddb1f
Nataliya Sklyar <nsklyar@ebi.ac.uk>
2011-02-03 11:38:18 +0000

    testing bioentities loading with Java

---
+++ atlas-model/src/main/java/uk/ac/ebi/microarray/atlas/model/BioEntity.java
@@ -0,0 +1,11 @@
+package uk.ac.ebi.microarray.atlas.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nsklyar
+ * Date: 02/02/2011
+ * Time: 12:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class BioEntity {
+}
