4041a5ffdcce82d2662378ee4ffe00a1e4d32c87
Alexey Filippov <alf@ebi.ac.uk>
2011-02-07 13:56:27 +0000

    Generifyed

--- atlas-web/src/test/java/ae3/service/AtlasStatisticsQueryServiceTest.java
+++ atlas-web/src/test/java/ae3/service/AtlasStatisticsQueryServiceTest.java
@@ -5,7 +5,6 @@ import org.junit.Before;
 import org.junit.Test;
 import uk.ac.ebi.gxa.index.StatisticsStorageFactory;
 import uk.ac.ebi.gxa.statistics.Experiment;
-import uk.ac.ebi.gxa.statistics.StatisticsQueryUtils;
 import uk.ac.ebi.gxa.statistics.StatisticsStorage;
 import uk.ac.ebi.gxa.statistics.StatisticsType;
 
@@ -27,14 +26,14 @@ public class AtlasStatisticsQueryServiceTest {
             File bitIndexResourcePath = new File(AtlasGene.class.getClassLoader().getResource(bitIndexResourceName).toURI());
             StatisticsStorageFactory statisticsStorageFactory = new StatisticsStorageFactory(bitIndexResourceName);
             statisticsStorageFactory.setAtlasIndex(new File(bitIndexResourcePath.getParent()));
-            StatisticsStorage statisticsStorage = statisticsStorageFactory.createStatisticsStorage();
+            StatisticsStorage<Long> statisticsStorage = statisticsStorageFactory.createStatisticsStorage();
             atlasStatisticsQueryService = new AtlasBitIndexQueryService(bitIndexResourceName);
             atlasStatisticsQueryService.setStatisticsStorage(statisticsStorage);
         } catch (Exception e) {
         }
     }
 
-        @Before
+    @Before
     public void initGene() throws Exception {
         geneId = 169968252l;
     }
@@ -43,7 +42,7 @@ public class AtlasStatisticsQueryServiceTest {
     public void test_getExperimentsSortedByPvalueTRank() {
 
         List<Experiment> list = atlasStatisticsQueryService.getExperimentsSortedByPvalueTRank(
-                geneId, StatisticsType.UP_DOWN, null, null, !StatisticsQueryUtils.EFO, -1, -1);
+                geneId, StatisticsType.UP_DOWN, null, null, false, -1, -1);
         assertNotNull(list);
         assertTrue(list.size() > 0);
         Experiment bestExperiment = list.get(0);
@@ -51,12 +50,12 @@ public class AtlasStatisticsQueryServiceTest {
         assertNotNull(bestExperiment.getHighestRankAttribute().getEf());
 
         List<Experiment> list2 = atlasStatisticsQueryService.getExperimentsSortedByPvalueTRank(
-                geneId, StatisticsType.UP_DOWN, null, null, !StatisticsQueryUtils.EFO, 1, 5);
+                geneId, StatisticsType.UP_DOWN, null, null, false, 1, 5);
         assertNotNull(list2);
         assertEquals(5, list2.size());
 
         List<Experiment> list3 = atlasStatisticsQueryService.getExperimentsSortedByPvalueTRank(
-                geneId, StatisticsType.UP_DOWN, "organism_part", "liver", !StatisticsQueryUtils.EFO, -1, -1);
+                geneId, StatisticsType.UP_DOWN, "organism_part", "liver", false, -1, -1);
         assertNotNull(list3);
         assertTrue(list3.size() > 0);
     }
@@ -65,5 +64,4 @@ public class AtlasStatisticsQueryServiceTest {
     public void getExperimentsForGeneAndEf() {
         assertTrue(atlasStatisticsQueryService.getExperimentsForGeneAndEf(geneId, null, StatisticsType.UP_DOWN).size() > 0);
     }
-
 }
