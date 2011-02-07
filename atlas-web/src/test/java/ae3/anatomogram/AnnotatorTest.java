7930f283ade1304048949934888041b147f370ae
Robert Petryszak <rpetry@ebi.ac.uk>
2011-02-07 16:59:36 +0000

    Fixed test

--- atlas-web/src/test/java/ae3/anatomogram/AnnotatorTest.java
+++ atlas-web/src/test/java/ae3/anatomogram/AnnotatorTest.java
@@ -24,47 +24,26 @@ public class AnnotatorTest extends AbstractOnceIndexTest {
     private AtlasSolrDAO atlasSolrDAO;
     private Annotator annotator;
     private List<String> efoTerms;
+    private long geneId;
 
     @Before
     public void createService() throws Exception {
-        long start = System.currentTimeMillis();
+
+        geneId = 169968252l; // ENSMUSG00000020275
         EmbeddedSolrServer solrServerAtlas = new EmbeddedSolrServer(getContainer(), "atlas");
         atlasSolrDAO = new AtlasSolrDAO();
         atlasSolrDAO.setSolrServerAtlas(solrServerAtlas);
-        System.out.println("Solr index mocked in: " + (System.currentTimeMillis() - start) + " ms");
-        start = System.currentTimeMillis();
 
         // List of efo terms displayable on a mouse anatomogram
         String[] efoTermsArr = {
                 "EFO_0000827","EFO_0000793","EFO_0000803","EFO_0000815","EFO_0000265",
                 "EFO_0000110","EFO_0000943","EFO_0000792","EFO_0000800","EFO_0000889",
                 "EFO_0000934","EFO_0000935","EFO_0000968","EFO_0001385","EFO_0001412",
-                "EFO_0001413","EFO_0001937","EFO_0000302" };
-
+                "EFO_0001413","EFO_0001937","EFO_0000302"};
         efoTerms = Arrays.asList(efoTermsArr);
 
-
-        // Mock AtlasStatisticsQueryService object
-        AtlasStatisticsQueryService atlasStatisticsQueryService = EasyMock.createMock(AtlasStatisticsQueryService.class);
-        for (String efoTermStr : efoTerms) {
-            EasyMock.expect(atlasStatisticsQueryService.getExperimentCountsForGene(efoTermStr, StatisticsType.UP, StatisticsQueryUtils.EFO, 169968252l)).andReturn(1);
-            EasyMock.expect(atlasStatisticsQueryService.getExperimentCountsForGene(efoTermStr, StatisticsType.DOWN, StatisticsQueryUtils.EFO, 169968252l)).andReturn(1);
-        }
-
-        // Mock efo
-        Efo efo = EasyMock.createMock(Efo.class);
-        for (String efoTermStr : efoTerms) {
-            EfoNode efoNode = new EfoNode(efoTermStr, efoTermStr, true, Collections.<String>emptyList());
-            EasyMock.expect(efo.getTermById(efoTermStr)).andReturn(new EfoTerm(efoNode, 1, true));
-        }
-
-        System.out.println("AtlasStatisticsQueryService and efo mocks created in : " + (System.currentTimeMillis() - start) + " ms");
-        start = System.currentTimeMillis();
         annotator = new Annotator();
-        annotator.setAtlasStatisticsQueryService(atlasStatisticsQueryService);
-        annotator.setEfo(efo);
         annotator.load(); // load svg templates
-        System.out.println("Annotator init in: " + (System.currentTimeMillis() - start) + " ms");
     }
 
     @Test
@@ -84,6 +63,8 @@ public class AnnotatorTest extends AbstractOnceIndexTest {
 
             for (Annotator.AnatomogramType type : new Annotator.AnatomogramType[]{Annotator.AnatomogramType.Web, Annotator.AnatomogramType.Das}) {
 
+                mockObjects();
+
                 if (null == atlasGene)
                     continue;
 
@@ -93,6 +74,29 @@ public class AnnotatorTest extends AbstractOnceIndexTest {
             }
             assertTrue(success);
         }
+    }
+
+
+    // Create and populate required functionality of mock objects: atlasStatisticsQueryService and efo
+    private void mockObjects() {
+        // Mock AtlasStatisticsQueryService object
+        AtlasStatisticsQueryService atlasStatisticsQueryService = EasyMock.createMock(AtlasStatisticsQueryService.class);
+        for (String efoTermStr : efoTerms) {
+            EasyMock.expect(atlasStatisticsQueryService.getExperimentCountsForGene(efoTermStr, StatisticsType.UP, StatisticsQueryUtils.EFO, geneId)).andReturn(1);
+            EasyMock.expect(atlasStatisticsQueryService.getExperimentCountsForGene(efoTermStr, StatisticsType.DOWN, StatisticsQueryUtils.EFO, geneId)).andReturn(1);
+        }
+        EasyMock.replay(atlasStatisticsQueryService);
+
+        // Mock Efo object
+        Efo efo = EasyMock.createMock(Efo.class);
+        for (String efoTermStr : efoTerms) {
+            EfoNode efoNode = new EfoNode(efoTermStr, efoTermStr, false, Collections.<String>emptyList());
+            EfoTerm efoTerm = new EfoTerm(efoNode, 0, false);
+            EasyMock.expect(efo.getTermById(EasyMock.eq(efoTermStr))).andReturn(efoTerm);
+        }
+        EasyMock.replay(efo);
+        annotator.setAtlasStatisticsQueryService(atlasStatisticsQueryService);
+        annotator.setEfo(efo);
     }
 }
 
