823e20329cbceb77f63b9d09095085971817b869
Robert Petryszak <rpetry@ebi.ac.uk>
2011-02-09 10:33:02 +0000

    Reflected modifications in AtlasNetCDFDAO stemming from method signature changes to listNetCDFs()

--- atlas-web/src/main/java/ae3/dao/NetCDFReader.java
+++ atlas-web/src/main/java/ae3/dao/NetCDFReader.java
@@ -55,9 +55,9 @@ public class NetCDFReader {
      * @return either constructed object or null, if no data files was found for this accession
      * @throws IOException if i/o error occurs
      */
-    public static ExperimentalData loadExperiment(AtlasNetCDFDAO atlasNetCDFDAO, String experimentAccession) throws IOException {
+    public static ExperimentalData loadExperiment(AtlasNetCDFDAO atlasNetCDFDAO, String experimentAccession, String experimentId) throws IOException {
         ExperimentalData experiment = null;
-        for (File file : atlasNetCDFDAO.listNetCDFs(experimentAccession)) {
+        for (File file : atlasNetCDFDAO.listNetCDFs(experimentAccession, experimentId)) {
             if (experiment == null)
                 experiment = new ExperimentalData();
             loadArrayDesign(file.getAbsolutePath(), experiment);
