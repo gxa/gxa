9d78aae0a7421566763c6532701dfafac3d27110
Robert Petryszak <rpetry@ebi.ac.uk>
2011-02-22 14:49:53 +0000

    Reflected changes to method signatures in AtlasStatisticsQueryService

--- atlas-web/src/test/java/ae3/model/AtlasGeneTest.java
+++ atlas-web/src/test/java/ae3/model/AtlasGeneTest.java
@@ -31,10 +31,7 @@ import org.easymock.EasyMock;
 import org.junit.Before;
 import org.junit.Test;
 import uk.ac.ebi.gxa.index.AbstractOnceIndexTest;
-import uk.ac.ebi.gxa.statistics.Attribute;
-import uk.ac.ebi.gxa.statistics.Experiment;
-import uk.ac.ebi.gxa.statistics.PvalTstatRank;
-import uk.ac.ebi.gxa.statistics.StatisticsType;
+import uk.ac.ebi.gxa.statistics.*;
 import uk.ac.ebi.gxa.utils.EfvTree;
 
 import java.util.*;
@@ -104,13 +101,15 @@ public class AtlasGeneTest extends AbstractOnceIndexTest {
         AtlasStatisticsQueryService atlasStatisticsQueryService = EasyMock.createMock(AtlasStatisticsQueryService.class);
 
         // Inject required functionality
-        Attribute attr = new Attribute("cell_type", "B220+ b cell");
+        Attribute attr = StatisticsQueryUtils.getAttribute("cell_type", "B220+ b cell", !StatisticsQueryUtils.EFO, StatisticsType.UP_DOWN);
         Experiment exp = new Experiment("E-MTAB-25", 411512559l);
         exp.setPvalTstatRank(new PvalTstatRank(0.007f, (short) 0));
         EasyMock.expect(atlasStatisticsQueryService.getScoringEfvsForGene(gene.getGeneId(), StatisticsType.UP_DOWN)).andReturn(Collections.<Attribute>singletonList(attr));
-        EasyMock.expect(atlasStatisticsQueryService.getExperimentsSortedByPvalueTRank(
-                gene.getGeneId(), StatisticsType.UP_DOWN, attr.getEf(), attr.getEfv(), false, -1, -1)).andReturn(Collections.<Experiment>singletonList(exp));
-        EasyMock.expect(atlasStatisticsQueryService.getExperimentCountsForGene(attr.getValue(), StatisticsType.NON_D_E, false, gene.getGeneId())).andReturn(1);
+
+        EasyMock.expect(atlasStatisticsQueryService.getExperimentsSortedByPvalueTRank(gene.getGeneId(), attr, -1, -1)).andReturn(Collections.<Experiment>singletonList(exp));
+        attr.setStatType(StatisticsType.NON_D_E);
+
+        EasyMock.expect(atlasStatisticsQueryService.getExperimentCountsForGene(attr, gene.getGeneId())).andReturn(1);
 
         // Prepare injected functionality for test
         EasyMock.replay(atlasStatisticsQueryService);
