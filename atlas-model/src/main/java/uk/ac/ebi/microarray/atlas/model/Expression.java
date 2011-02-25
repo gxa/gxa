b813e859acdf1633b026645442cc3bb51f61f010
Robert Petryszak <rpetry@ebi.ac.uk>
2011-02-25 09:42:26 +0000

    Moving class to atlas-model so that it can be also used in netcdf-reader module

---
+++ atlas-model/src/main/java/uk/ac/ebi/microarray/atlas/model/Expression.java
@@ -0,0 +1,11 @@
+package uk.ac.ebi.microarray.atlas.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: rpetry
+ * Date: 2/25/11
+ * Time: 8:21 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Expression {
+}
