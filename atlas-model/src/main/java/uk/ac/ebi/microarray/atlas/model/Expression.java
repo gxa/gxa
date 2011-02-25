72d99eaf58ab2691372d1e0c271d9ecf61bb6bf1
Robert Petryszak <rpetry@ebi.ac.uk>
2011-02-25 13:22:05 +0000

    Moved in from atlas-web; modified to included Expression.ANY - this is used by AtlasPlotter to retrieve best expression analyses irrespective of the type of expression they are

--- atlas-model/src/main/java/uk/ac/ebi/microarray/atlas/model/Expression.java
+++ atlas-model/src/main/java/uk/ac/ebi/microarray/atlas/model/Expression.java
@@ -7,5 +7,31 @@ package uk.ac.ebi.microarray.atlas.model;
  * Time: 8:21 AM
  * To change this template use File | Settings | File Templates.
  */
-public class Expression {
+
+import com.sun.xml.bind.v2.schemagen.xmlschema.Any;
+
+/**
+ * Expression of gene in condition - up, down or non-differentially expressed - moved from atlas-web by rpetry
+ * @author pashky
+ */
+public enum Expression {
+    UP {
+        public boolean isUp() { return true; }
+        public boolean isNo() { return false; }
+    },
+    DOWN {
+        public boolean isUp() { return false; }
+        public boolean isNo() { return false; }
+    },
+    NONDE {
+        public boolean isUp() { return false; }
+        public boolean isNo() { return true; }
+    },
+    ANY {
+        public boolean isUp() { return true; }
+        public boolean isNo() { return true; }
+
+    };
+    public abstract boolean isUp();
+    public abstract boolean isNo();
 }
