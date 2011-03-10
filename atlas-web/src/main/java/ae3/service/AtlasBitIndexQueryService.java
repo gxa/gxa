2203feb588f4dbdf1010f654a9e0348c73ba05e6
Robert Petryszak <rpetry@ebi.ac.uk>
2011-03-10 15:40:17 +0000

    Ticket #2821 - modified StatisticsStorage to return clones of Experiments and EfvAttributes instead of the actual objects stored in the index. Because Experiment and EfvAttribute classes are mutable, returning the actual objects was causing errors with at least 3 concurrent user requests.

--- atlas-web/src/main/java/ae3/service/AtlasBitIndexQueryService.java
+++ atlas-web/src/main/java/ae3/service/AtlasBitIndexQueryService.java
@@ -98,13 +98,15 @@ public class AtlasBitIndexQueryService implements AtlasStatisticsQueryService {
     /**
      * @param attribute
      * @param geneId
+     * @param geneRestrictionSet
+     * @param scoresCache
      * @return Experiment count for statisticsType, attributes and geneId
      */
     public Integer getExperimentCountsForGene(
             Attribute attribute,
             Long geneId,
             Set<Long> geneRestrictionSet,
-            HashMap<String, Multiset<Integer>> scoresCacheForStatType) {
+            Map<StatisticsType, HashMap<String, Multiset<Integer>>> scoresCache) {
 
         if (geneRestrictionSet == null) { // By default restrict the experiment count query to geneId
             geneRestrictionSet = Collections.singleton(geneId);
@@ -115,8 +117,8 @@ public class AtlasBitIndexQueryService implements AtlasStatisticsQueryService {
         Multiset<Integer> scores = StatisticsQueryUtils.getExperimentCounts(statsQuery, statisticsStorage, null);
 
         // Cache geneRestrictionSet's scores for efvOrEfo - this cache will be re-used in heatmaps for rows other than the first one
-        if (scoresCacheForStatType != null) {
-            scoresCacheForStatType.put(attribute.getValue(), scores);
+        if (scoresCache != null && attribute.getStatType() != null) {
+            scoresCache.get(attribute.getStatType()).put(attribute.getValue(), scores);
         }
         Integer geneIndex = statisticsStorage.getIndexForGeneId(geneId);
 
