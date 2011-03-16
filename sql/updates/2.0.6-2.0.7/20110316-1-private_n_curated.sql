54d9b649e069e761ebddc8aa0bf6ac74a84f43a9
Alexey Filippov <alf@ebi.ac.uk>
2011-03-16 15:36:16 +0000

    dropping "NOT NULL" clause

--- sql/updates/2.0.6-2.0.7/20110316-1-private_n_curated.sql
+++ sql/updates/2.0.6-2.0.7/20110316-1-private_n_curated.sql
@@ -1,3 +1,3 @@
-alter table A2_EXPERIMENT add curated NUMBER(1) NOT NULL DEFAULT 0;
-alter table A2_EXPERIMENT add private NUMBER(1) NOT NULL DEFAULT 1;
+alter table A2_EXPERIMENT add curated NUMBER(1) DEFAULT 0;
+alter table A2_EXPERIMENT add private NUMBER(1) DEFAULT 1;
 update A2_EXPERIMENT set curated = 1, private = 0;
