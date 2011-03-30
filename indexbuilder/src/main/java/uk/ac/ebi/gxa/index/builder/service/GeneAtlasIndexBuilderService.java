920cb0ddfdfa9225169e76288dd499bb0b25b169
Nataliya Sklyar <nsklyar@ebi.ac.uk>
2011-03-30 16:35:23 +0100

    roll back to use "gene-based" vs "bioentity-based" DAOs

--- indexbuilder/src/main/java/uk/ac/ebi/gxa/index/builder/service/GeneAtlasIndexBuilderService.java
+++ indexbuilder/src/main/java/uk/ac/ebi/gxa/index/builder/service/GeneAtlasIndexBuilderService.java
@@ -87,9 +87,6 @@ public class GeneAtlasIndexBuilderService extends IndexBuilderService {
         final int total = bioEntities.size();
         getLog().info("Found " + total + " genes to index");
 
-        final ArrayListMultimap<Long,DesignElement> allDesignElementsForGene = bioEntityDAOInterface.getAllDesignElementsForGene();
-        getLog().info("Found " + allDesignElementsForGene.asMap().size() + " genes with de");
-
         loadEfoMapping();
 
         final AtomicInteger processed = new AtomicInteger(0);
@@ -119,8 +116,7 @@ public class GeneAtlasIndexBuilderService extends IndexBuilderService {
                             SolrInputDocument solrInputDoc = createGeneSolrInputDocument(gene);
 
                             Set<String> designElements = new HashSet<String>();
-                            for (DesignElement de : allDesignElementsForGene.get(gene.getId())) {
-//                            for (DesignElement de : bioEntityDAOInterface.getDesignElementsByGeneID(gene.getId())) {
+                            for (DesignElement de : bioEntityDAOInterface.getDesignElementsByGeneID(gene.getId())) {
                                 designElements.add(de.getName());
                                 designElements.add(de.getAccession());
                             }
