69f2a10ae20a0c737a774f302b8b03c6d6c4e60f
Nataliya Sklyar <nsklyar@ebi.ac.uk>
2011-04-04 13:52:35 +0100

    file renamed

--- sql/updates/2.0.6-2.0.7/20110404-1-bioentity.sql
+++ sql/updates/2.0.6-2.0.7/20110404-1-bioentity.sql
@@ -614,6 +614,7 @@ AS
   join a2_arraydesign ad on ad.arraydesignid = de.arraydesignid
   where debe.softwareid = ad.mappingswid
   and betype.ID_FOR_INDEX = 1
+  ;
 /
 
 CREATE OR REPLACE VIEW VWDESIGNELEMENTGENEDIRECT
@@ -632,7 +633,7 @@ AS
   join a2_bioentitytype betype on betype.bioentitytypeid = frombe.bioentitytypeid
   join a2_arraydesign ad on ad.arraydesignid = de.arraydesignid
   where debe.softwareid = ad.mappingswid
-  and betype.ID_FOR_INDEX = 1
+  and betype.ID_FOR_INDEX = 1;
  /
 
 
