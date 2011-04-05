c7922848781c260013097a29011962510225afd1
Robert Petryszak <rpetry@ebi.ac.uk>
2011-04-05 14:21:29 +0100

    Rollback of local set up checked-in by mistake

--- atlas-utils/src/main/java/uk/ac/ebi/gxa/properties/AtlasProperties.java
+++ atlas-utils/src/main/java/uk/ac/ebi/gxa/properties/AtlasProperties.java
@@ -369,16 +369,15 @@ public class AtlasProperties {
 
     /* R & Biocep */
     public String getRMode() {
-        return "local"; // TODO
-        // return getProperty("atlas.rservice.mode");
+        return getProperty("atlas.rservice.mode");
     }
 
     public Properties getRProperties() {
         final String prefix = "atlas.rservice.";
         Properties result = new Properties();
-        for(String property : getAvailablePropertyNames()) {
-            if(property.startsWith(prefix))
-                result.setProperty(property.substring(prefix.length()), "local"); // TODO getProperty(property)
+        for (String property : getAvailablePropertyNames()) {
+            if (property.startsWith(prefix))
+                result.setProperty(property.substring(prefix.length()), getProperty(property));
         }
         return result;
     }
