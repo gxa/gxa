edc35e3b36bc84024b96b904139c657fe682602f
Alexey Filippov <alf@ebi.ac.uk>
2011-04-13 11:40:31 +0100

    Dropping unread fields

--- atlas-model/src/main/java/uk/ac/ebi/microarray/atlas/model/BioEntity.java
+++ atlas-model/src/main/java/uk/ac/ebi/microarray/atlas/model/BioEntity.java
@@ -7,13 +7,6 @@ import java.util.List;
 
 import static java.util.Collections.unmodifiableList;
 
-/**
- * Created by IntelliJ IDEA.
- * User: nsklyar
- * Date: 02/02/2011
- * Time: 12:47
- * To change this template use File | Settings | File Templates.
- */
 public class BioEntity {
     private long id;
     private String identifier;
