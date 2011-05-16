c70c2ad378dc0cb2d15be0ed4ac2c7127fb7e723
Alexey Filippov <alf@ebi.ac.uk>
2011-05-16 17:03:25 +0100

    When loading an external Experiment, reading id is a big no-no

--- atlas-model/src/main/java/uk/ac/ebi/microarray/atlas/model/Experiment.java
+++ atlas-model/src/main/java/uk/ac/ebi/microarray/atlas/model/Experiment.java
@@ -38,7 +38,7 @@ import static uk.ac.ebi.gxa.utils.DateUtil.copyOf;
 @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 public class Experiment {
     @Id
-    private long experimentid;
+    private Long experimentid;
     private String accession;
 
     private String description;
@@ -71,7 +71,7 @@ public class Experiment {
 
     @Deprecated
     @Temporary
-    public Experiment(long id, String accession) {
+    public Experiment(Long id, String accession) {
         this.accession = accession;
         this.experimentid = id;
     }
