ba6f6160d2e88ac1800515c092f3b14c3c901117
Robert Petryszak <rpetry@ebi.ac.uk>
2011-06-06 12:59:24 +0100

    Ticket #2968 - replaced each LF character with a single space - to sqlldr the full content of varchar2 fields containing new lines (instead of just the portion up to the first new line char)

--- sql/create_sql_for_table.sql
+++ sql/create_sql_for_table.sql
@@ -9,7 +9,10 @@ set wrap off
 set feedback off
 set verify off
 
-SELECT 'SELECT ' || WM_CONCAT(DECODE(Data_TYPE, 'DATE', 'TO_CHAR(' || column_name || ',''YYYY-MM-DD HH24:MI:SS'')', column_name)) 
-|| ' FROM ' || UPPER('A2_&1')
+SELECT 'SELECT ' || WM_CONCAT(
+  DECODE(data_type, 'DATE', 'TO_CHAR(' || column_name || ',''YYYY-MM-DD HH24:MI:SS'')',
+         DECODE(data_type, 'VARCHAR2',
+                  ' DECODE(' || column_name || ', null, ' || column_name || ', REPLACE(REPLACE(' || column_name ||', chr(10), chr(32)),''  '',chr(32)))', column_name)))
+ || ' FROM ' || UPPER('A2_&1')
 FROM user_tab_columns WHERE table_name=UPPER('A2_&1');
 exit;
