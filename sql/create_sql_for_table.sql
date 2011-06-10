674bfdeddd5667adca67c4556df3f643210cb7da
Robert Petryszak <rpetry@ebi.ac.uk>
2011-06-10 14:29:07 +0100

    This issue is blocking Atlas release 2.0.8 - short term fix to avoid replacing double spaces with single ones in columns that never had new lines in them in the first place.

--- sql/create_sql_for_table.sql
+++ sql/create_sql_for_table.sql
@@ -12,7 +12,7 @@ set verify off
 SELECT 'SELECT ' || WM_CONCAT(
   DECODE(data_type, 'DATE', 'TO_CHAR(' || column_name || ',''YYYY-MM-DD HH24:MI:SS'')',
          DECODE(data_type, 'VARCHAR2',
-                  ' DECODE(' || column_name || ', null, ' || column_name || ', REPLACE(REPLACE(' || column_name ||', chr(10), chr(32)),''  '',chr(32)))', column_name)))
+                  ' DECODE(' || column_name || ', null, ' || column_name || ', REPLACE(' || column_name ||', chr(10), chr(32)))', column_name)))
  || ' FROM ' || UPPER('A2_&1')
 FROM user_tab_columns WHERE table_name=UPPER('A2_&1');
 exit;
