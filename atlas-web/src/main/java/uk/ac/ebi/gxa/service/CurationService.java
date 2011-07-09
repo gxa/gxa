658fe554bed9ce42bd6febabbc9e4c2bb934eed3
Misha Kapushesky <ostolop@ebi.ac.uk>
2011-07-09 16:21:19 +0100

    Work in progress - experiment loading ... (part 5) :) - now with transactions.

---
+++ atlas-web/src/main/java/uk/ac/ebi/gxa/service/CurationService.java
@@ -0,0 +1,125 @@
+package uk.ac.ebi.gxa.service;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import uk.ac.ebi.gxa.dao.AtlasDAO;
+import uk.ac.ebi.gxa.dao.ExperimentDAO;
+import uk.ac.ebi.microarray.atlas.api.*;
+import uk.ac.ebi.microarray.atlas.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author Misha Kapushesky
+ */
+@Service
+public class CurationService {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private AtlasDAO atlasDAO;
+
+    @Autowired
+    private ExperimentDAO experimentDAO;
+
+    @Transactional
+    public void saveExperiment(final ApiExperiment apiExperiment) {
+        Experiment experiment = atlasDAO.getExperimentByAccession(apiExperiment.getAccession());
+
+        if(experiment != null) {
+//            log.info("Deleting experiment " + experiment.getAccession() + " in order to update");
+//            experimentDAO.delete(experiment);
+        }
+
+        experiment = new Experiment(apiExperiment.getAccession());
+
+        experiment.setAbstract(apiExperiment.getArticleAbstract());
+        experiment.setCurated(apiExperiment.isCurated());
+        experiment.setDescription(apiExperiment.getDescription());
+        experiment.setLab(apiExperiment.getLab());
+        experiment.setLoadDate(apiExperiment.getLoadDate());
+        experiment.setPerformer(apiExperiment.getPerformer());
+        experiment.setPrivate(apiExperiment.isPrivate());
+        experiment.setPubmedId(apiExperiment.getPubmedId());
+        experiment.setReleaseDate(apiExperiment.getReleaseDate());
+
+        Map<String,Assay> assays = Maps.newHashMap();
+        for (ApiAssay apiAssay : apiExperiment.getAssays()) {
+            Assay assay = new Assay(apiAssay.getAccession());
+            // TODO: create ArrayDesign
+            assay.setArrayDesign(atlasDAO.getArrayDesignShallowByAccession(apiAssay.getArrayDesign().getAccession()));
+
+            for (ApiAssayProperty apiAssayProperty : apiAssay.getProperties()) {
+                PropertyValue propertyValue =  atlasDAO.getOrCreatePropertyValue(
+                        apiAssayProperty.getPropertyValue().getProperty().getName(),
+                        apiAssayProperty.getPropertyValue().getValue());
+
+                Set<OntologyTerm> terms = Sets.newHashSet();
+                for (ApiOntologyTerm apiOntologyTerm : apiAssayProperty.getTerms()) {
+                    terms.add(atlasDAO.getOrCreateOntologyTerm(
+                            apiOntologyTerm.getAccession(),
+                            apiOntologyTerm.getTerm(),
+                            apiOntologyTerm.getDescription(),
+                            apiOntologyTerm.getOntology().getName(),
+                            apiOntologyTerm.getOntology().getDescription(),
+                            apiOntologyTerm.getOntology().getSourceUri(),
+                            apiOntologyTerm.getOntology().getVersion()));
+                }
+
+                assay.addOrUpdateProperty(propertyValue, terms);
+            }
+
+            assays.put(apiAssay.getAccession(), assay);
+        }
+
+        List<Sample> samples = Lists.newArrayList();
+        for (ApiSample apiSample : apiExperiment.getSamples()) {
+            Sample sample = new Sample(apiSample.getAccession());
+            sample.setChannel(apiSample.getChannel());
+
+            if(apiSample.getOrganism() != null){
+                sample.setOrganism(atlasDAO.getOrganismByName(apiSample.getOrganism().getName()));
+            }
+
+            for (ApiSampleProperty apiSampleProperty : apiSample.getProperties()) {
+                PropertyValue propertyValue =  atlasDAO.getOrCreatePropertyValue(
+                        apiSampleProperty.getPropertyValue().getProperty().getName(),
+                        apiSampleProperty.getPropertyValue().getValue());
+
+                Set<OntologyTerm> terms = Sets.newHashSet();
+                for (ApiOntologyTerm apiOntologyTerm : apiSampleProperty.getTerms()) {
+                    terms.add(atlasDAO.getOrCreateOntologyTerm(
+                            apiOntologyTerm.getAccession(),
+                            apiOntologyTerm.getTerm(),
+                            apiOntologyTerm.getDescription(),
+                            apiOntologyTerm.getOntology().getName(),
+                            apiOntologyTerm.getOntology().getDescription(),
+                            apiOntologyTerm.getOntology().getSourceUri(),
+                            apiOntologyTerm.getOntology().getVersion()));
+                }
+
+                sample.addOrUpdateProperty(propertyValue, terms);
+            }
+
+            for (ApiAssay apiAssay : apiSample.getAssays()) {
+                sample.addAssay(assays.get(apiAssay.getAccession()));
+            }
+
+            samples.add(sample);
+        }
+
+        experiment.setAssays(new ArrayList<Assay>(assays.values()));
+        experiment.setSamples(samples);
+
+        experimentDAO.save(experiment);
+    }
+}
--- atlas-web/src/main/java/uk/ac/ebi/gxa/web/controller/api/CurationApiController.java
+++ atlas-web/src/main/java/uk/ac/ebi/gxa/web/controller/api/CurationApiController.java
@@ -1,7 +1,5 @@
 package uk.ac.ebi.gxa.web.controller.api;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,15 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import uk.ac.ebi.gxa.dao.AssayDAO;
 import uk.ac.ebi.gxa.dao.AtlasDAO;
-import uk.ac.ebi.gxa.dao.ExperimentDAO;
+import uk.ac.ebi.gxa.service.CurationService;
 import uk.ac.ebi.gxa.web.controller.AtlasViewController;
 import uk.ac.ebi.gxa.web.controller.ResourceNotFoundException;
 import uk.ac.ebi.microarray.atlas.api.*;
 import uk.ac.ebi.microarray.atlas.model.*;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletResponse;
-import java.util.*;
+import java.util.Collection;
+import java.util.Set;
 
 /**
  * TODO
@@ -28,16 +26,14 @@ import java.util.*;
  */
 @Controller
 public class CurationApiController extends AtlasViewController {
-    final private AtlasDAO atlasDAO;
-    final private AssayDAO assayDAO;
-    private ExperimentDAO experimentDAO;
+    @Autowired
+    private AtlasDAO atlasDAO;
 
     @Autowired
-    public CurationApiController(AtlasDAO atlasDAO, ExperimentDAO experimentDAO, AssayDAO assayDAO) {
-        this.atlasDAO = atlasDAO;
-        this.experimentDAO = experimentDAO;
-        this.assayDAO = assayDAO;
-    }
+    private AssayDAO assayDAO;
+
+    @Autowired
+    private CurationService curationService;
 
     @RequestMapping(value = "/experiments/{experimentAccession}",
             method = RequestMethod.GET)
@@ -46,7 +42,7 @@ public class CurationApiController extends AtlasViewController {
         final Experiment experiment = atlasDAO.getExperimentByAccession(experimentAccession);
         if(experiment == null) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-            throw new ResourceNotFoundException("No records for experiment " + experimentAccession);
+            throw new ResourceNotFoundException("No record for experiment " + experimentAccession);
         }
 
         response.setStatus(HttpServletResponse.SC_FOUND);
@@ -58,92 +54,8 @@ public class CurationApiController extends AtlasViewController {
     public void putExperiment(@PathVariable("experimentAccession") final String experimentAccession,
                                        @RequestBody final ApiExperiment apiExperiment,
                                         HttpServletResponse response) throws ResourceNotFoundException {
-        if(atlasDAO.getExperimentByAccession(experimentAccession) != null) {
-            response.setStatus(HttpServletResponse.SC_CONFLICT);
-        }
 
-        Experiment experiment = new Experiment(apiExperiment.getAccession());
-
-        experiment.setAbstract(apiExperiment.getArticleAbstract());
-        experiment.setCurated(apiExperiment.isCurated());
-        experiment.setDescription(apiExperiment.getDescription());
-        experiment.setLab(apiExperiment.getLab());
-        experiment.setLoadDate(apiExperiment.getLoadDate());
-        experiment.setPerformer(apiExperiment.getPerformer());
-        experiment.setPrivate(apiExperiment.isPrivate());
-        experiment.setPubmedId(apiExperiment.getPubmedId());
-        experiment.setReleaseDate(apiExperiment.getReleaseDate());
-
-        Map<String,Assay> assays = Maps.newHashMap();
-        for (ApiAssay apiAssay : apiExperiment.getAssays()) {
-            Assay assay = new Assay(apiAssay.getAccession());
-            // TODO: create ArrayDesign
-            assay.setArrayDesign(atlasDAO.getArrayDesignShallowByAccession(apiAssay.getArrayDesign().getAccession()));
-
-            for (ApiAssayProperty apiAssayProperty : apiAssay.getProperties()) {
-                PropertyValue propertyValue =  atlasDAO.getOrCreatePropertyValue(
-                        apiAssayProperty.getPropertyValue().getProperty().getName(),
-                        apiAssayProperty.getPropertyValue().getValue());
-
-                Set<OntologyTerm> terms = Sets.newHashSet();
-                for (ApiOntologyTerm apiOntologyTerm : apiAssayProperty.getTerms()) {
-                    terms.add(atlasDAO.getOrCreateOntologyTerm(
-                            apiOntologyTerm.getAccession(),
-                            apiOntologyTerm.getTerm(),
-                            apiOntologyTerm.getDescription(),
-                            apiOntologyTerm.getOntology().getName(),
-                            apiOntologyTerm.getOntology().getDescription(),
-                            apiOntologyTerm.getOntology().getSourceUri(),
-                            apiOntologyTerm.getOntology().getVersion()));
-                }
-
-                assay.addOrUpdateProperty(propertyValue, terms);
-            }
-
-            assays.put(apiAssay.getAccession(), assay);
-        }
-
-        List<Sample> samples = Lists.newArrayList();
-        for (ApiSample apiSample : apiExperiment.getSamples()) {
-            Sample sample = new Sample(apiSample.getAccession());
-            sample.setChannel(apiSample.getChannel());
-
-            if(apiSample.getOrganism() != null){
-                sample.setOrganism(atlasDAO.getOrganismByName(apiSample.getOrganism().getName()));
-            }
-
-            for (ApiSampleProperty apiSampleProperty : apiSample.getProperties()) {
-                PropertyValue propertyValue =  atlasDAO.getOrCreatePropertyValue(
-                        apiSampleProperty.getPropertyValue().getProperty().getName(),
-                        apiSampleProperty.getPropertyValue().getValue());
-
-                Set<OntologyTerm> terms = Sets.newHashSet();
-                for (ApiOntologyTerm apiOntologyTerm : apiSampleProperty.getTerms()) {
-                    terms.add(atlasDAO.getOrCreateOntologyTerm(
-                            apiOntologyTerm.getAccession(),
-                            apiOntologyTerm.getTerm(),
-                            apiOntologyTerm.getDescription(),
-                            apiOntologyTerm.getOntology().getName(),
-                            apiOntologyTerm.getOntology().getDescription(),
-                            apiOntologyTerm.getOntology().getSourceUri(),
-                            apiOntologyTerm.getOntology().getVersion()));
-                }
-
-                sample.addOrUpdateProperty(propertyValue, terms);
-            }
-
-            for (ApiAssay apiAssay : apiSample.getAssays()) {
-                sample.addAssay(assays.get(apiAssay.getAccession()));
-            }
-
-            samples.add(sample);
-        }
-
-        experiment.setAssays(new ArrayList<Assay>(assays.values()));
-        experiment.setSamples(samples);
-
-        experimentDAO.save(experiment);
-        atlasDAO.flushCurrentSession();
+        curationService.saveExperiment(apiExperiment);
         response.setStatus(HttpServletResponse.SC_CREATED);
     }
 
