91986706149befcdd3dc93dbc146a27b250ed9be
Misha Kapushesky <ostolop@ebi.ac.uk>
2011-08-09 19:10:22 +0100

    Fix and tests for #3052

--- atlas-analytics/src/test/java/uk/ac/ebi/gxa/analytics/generator/TestExperimentAnalyticsGeneratorService.java
+++ atlas-analytics/src/test/java/uk/ac/ebi/gxa/analytics/generator/TestExperimentAnalyticsGeneratorService.java
@@ -22,14 +22,62 @@
 
 package uk.ac.ebi.gxa.analytics.generator;
 
-import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import uk.ac.ebi.gxa.R.AtlasRFactory;
+import uk.ac.ebi.gxa.R.AtlasRFactoryBuilder;
+import uk.ac.ebi.gxa.analytics.compute.AtlasComputeService;
+import uk.ac.ebi.gxa.analytics.generator.listener.AnalyticsGenerationEvent;
+import uk.ac.ebi.gxa.analytics.generator.listener.AnalyticsGeneratorListener;
+import uk.ac.ebi.gxa.analytics.generator.service.ExperimentAnalyticsGeneratorService;
+import uk.ac.ebi.gxa.dao.AtlasDAOTestCase;
+import uk.ac.ebi.gxa.data.AtlasDataDAO;
+import uk.ac.ebi.microarray.atlas.model.ArrayDesign;
+import uk.ac.ebi.microarray.atlas.model.Experiment;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class TestExperimentAnalyticsGeneratorService extends TestCase {
+public class TestExperimentAnalyticsGeneratorService extends AtlasDAOTestCase {
+    private final static String E_GEOD_5035 = "E-GEOD-5035";
+    private final static String A_AFFY_45 = "A-AFFY-45";
+
+    private AtlasDataDAO atlasDataDAO;
+    private ExperimentAnalyticsGeneratorService experimentAnalyticsGeneratorService;
+    private AtlasComputeService atlasComputeService;
+    private ExecutorService ste;
+
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+
+        atlasDataDAO = new AtlasDataDAO();
+        atlasDataDAO.setAtlasDataRepo(new File(getClass().getClassLoader().getResource("").getPath()));
+
+        AtlasRFactory rFactory = AtlasRFactoryBuilder.getAtlasRFactoryBuilder().buildAtlasRFactory();
+        // build service
+        atlasComputeService = new AtlasComputeService();
+        atlasComputeService.setAtlasRFactory(rFactory);
+
+        ste = Executors.newSingleThreadExecutor();
+
+        experimentAnalyticsGeneratorService =
+                new ExperimentAnalyticsGeneratorService(atlasDAO, atlasDataDAO, atlasComputeService, ste);
+    }
+
+    @After
+    @Override
+    public void tearDown() {
+        atlasComputeService.shutdown();
+        ste.shutdownNow();
+    }
+
+    @Test
     public void testGetRCodeFromResource() throws IOException {
         // open a stream to the resource
         InputStream in = getClass().getClassLoader().getResourceAsStream("R/analytics.R");
@@ -41,6 +89,40 @@ public class TestExperimentAnalyticsGeneratorService extends TestCase {
         String line;
         while ((line = reader.readLine()) != null) {
             sb.append(line);
+        }
+    }
+
+    @Test
+    public void testCreateAnalyticsForExperimentWithoutFactors () throws AnalyticsGeneratorException {
+        TestingAnalyticsGeneratorListener agl = new TestingAnalyticsGeneratorListener();
+        experimentAnalyticsGeneratorService.createAnalyticsForExperiment(E_GEOD_5035, agl);
+        assertTrue(agl.warnings.contains("No analytics were computed for " + E_GEOD_5035 + "/" +
+                            A_AFFY_45 + " as it contained no factors!"));
+    }
+
+    private class TestingAnalyticsGeneratorListener implements AnalyticsGeneratorListener {
+        public boolean wasSuccessful = false;
+        public Set<String> progresses = new HashSet<String>();
+        public Set<String> warnings   = new HashSet<String>();
+
+        @Override
+        public void buildSuccess() {
+            wasSuccessful = true;
+        }
+
+        @Override
+        public void buildError(AnalyticsGenerationEvent event) {
+            // ignore
+        }
+
+        @Override
+        public void buildProgress(String progressStatus) {
+            progresses.add(progressStatus);
+        }
+
+        @Override
+        public void buildWarning(String message) {
+            warnings.add(message);
         }
     }
 }
