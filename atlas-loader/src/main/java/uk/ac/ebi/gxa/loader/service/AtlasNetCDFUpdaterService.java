5fb6f0cfe52606c92fff144740c5a8d74a430018
Misha Kapushesky <ostolop@ebi.ac.uk>
2011-08-11 16:28:07 +0100

    "Final" Fix for #3057: disable analytics preservation -- too broken to be fixed easily for release.

--- atlas-loader/src/main/java/uk/ac/ebi/gxa/loader/service/AtlasNetCDFUpdaterService.java
+++ atlas-loader/src/main/java/uk/ac/ebi/gxa/loader/service/AtlasNetCDFUpdaterService.java
@@ -89,9 +89,12 @@ public class AtlasNetCDFUpdaterService {
                 }
             }
 
-            if (assayAccessions.length == data.getAssays().size()) {
-                data.matchValuePatterns(getValuePatterns(proxy, data.getAssays()));
-            }
+            // TODO: this is commented out because it is *broken* and needs to be rewritten
+            // behaviour after commenting code below: *any* netcdf update will result in analytics reset
+
+//            if (assayAccessions.length == data.getAssays().size()) {
+//                data.matchValuePatterns(getValuePatterns(proxy, data.getAssays()));
+//            }
 
             // Get unique values
             List<KeyValuePair> uniqueValues = proxy.getUniqueValues();
