7baaae82f46a8f2627c01783fdf27e86bdacfd3a
Alexey Filippov <alf@ebi.ac.uk>
2011-08-16 17:04:19 +0100

    `allocationSize = 1` in order to disable Hibernate's hilo approach (which is nice, but not ready for our DB yet)
    Conflicts:
    
    	atlas-model/src/main/java/uk/ac/ebi/microarray/atlas/model/ArrayDesign.java

--- atlas-dao/src/test/java/uk/ac/ebi/gxa/dao/TestAtlasDAO.java
+++ atlas-dao/src/test/java/uk/ac/ebi/gxa/dao/TestAtlasDAO.java
@@ -123,8 +123,8 @@ public class TestAtlasDAO extends AtlasDAOTestCase {
         String accession =
                 getDataSet().getTable("A2_ARRAYDESIGN").getValue(0, "accession")
                         .toString();
-        long id =
-                Long.parseLong(getDataSet().getTable("A2_ARRAYDESIGN")
+        Long id =
+                Long.valueOf(getDataSet().getTable("A2_ARRAYDESIGN")
                         .getValue(0, "arraydesignid")
                         .toString());
 
