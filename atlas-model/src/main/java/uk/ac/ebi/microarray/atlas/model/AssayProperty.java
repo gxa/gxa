e209f8ee5a4066669d21b23027052ba4b524f1a4
Nataliya Sklyar <nsklyar@ebi.ac.uk>
2011-09-07 15:08:18 +0100

    changed IDEA settings for import

--- atlas-model/src/main/java/uk/ac/ebi/microarray/atlas/model/AssayProperty.java
+++ atlas-model/src/main/java/uk/ac/ebi/microarray/atlas/model/AssayProperty.java
@@ -29,18 +29,7 @@ import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 
 import javax.annotation.Nonnull;
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
