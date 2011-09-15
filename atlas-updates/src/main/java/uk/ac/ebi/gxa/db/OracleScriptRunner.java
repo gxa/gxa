bac2526f3909ce01791975dbd9d51b5620259465
Alexey Filippov <alf@ebi.ac.uk>
2011-09-15 17:13:20 +0100

    From .startsWith to regexp (still ugly, I know)

--- atlas-updates/src/main/java/uk/ac/ebi/gxa/db/OracleScriptRunner.java
+++ atlas-updates/src/main/java/uk/ac/ebi/gxa/db/OracleScriptRunner.java
@@ -8,8 +8,6 @@ import java.io.LineNumberReader;
 import java.io.Reader;
 import java.sql.*;
 
-import static org.apache.commons.lang.StringUtils.startsWithIgnoreCase;
-
 /**
  * Oracle-aware script runner
  * <p/>
@@ -53,14 +51,14 @@ public class OracleScriptRunner {
     }
 
     private void doExecute(Connection connection, Reader reader) throws IOException, SQLException {
-        StringBuffer command = null;
+        StringBuilder sqlBuffer = null;
         try {
-            boolean ignoreDelimiter = false;
+            boolean plsqlMode = false;
             LineNumberReader lineReader = new LineNumberReader(reader);
             String line;
             while ((line = lineReader.readLine()) != null) {
-                if (command == null) {
-                    command = new StringBuffer();
+                if (sqlBuffer == null) {
+                    sqlBuffer = new StringBuilder();
                 }
 
                 line = line.trim();
@@ -70,10 +68,17 @@ public class OracleScriptRunner {
                 } else if (line.startsWith("--") || line.startsWith("#") || line.startsWith("//")) {
                     logger.debug(line);
                 } else if (line.matches("[/.]")) {
-                    executeStatement(connection, command.toString());
-                    ignoreDelimiter = false;
-                    command = null;
-                } else if (startsWithIgnoreCase(line, "begin") || startsWithIgnoreCase(line, "declare")) {
+                    /*
+                    http://download.oracle.com/docs/cd/B19306_01/server.102/b14357/ch4.htm#i1039663
+                    Terminate PL/SQL subprograms by entering a period (.) by itself on a new line.
+                    You can also terminate and execute a PL/SQL subprogram by entering a slash (/) by itself on a new line.
+                     */
+                    executeStatement(connection, sqlBuffer.toString());
+                    plsqlMode = false;
+                    sqlBuffer = null;
+                } else if (!plsqlMode && line.toLowerCase().matches("(" +
+                        "begin|declare" +
+                        ")\\s.*")) {
                     /*
                     * TODO: support also
                         CREATE FUNCTION
@@ -102,33 +107,33 @@ CREATE OR REPLACE PACKAGE BODY ATLASMGR AS
 
 etc.
                     */
-                    ignoreDelimiter = true;
-                    command.append(line);
-                    command.append(" ");
-                } else if (!ignoreDelimiter && line.contains(DEFAULT_DELIMITER)) {
+                    plsqlMode = true;
+                    sqlBuffer.append(line);
+                    sqlBuffer.append(" ");
+                } else if (!plsqlMode && line.contains(DEFAULT_DELIMITER)) {
                     if (line.endsWith(DEFAULT_DELIMITER)) {
-                        command.append(line.substring(0, line.lastIndexOf(DEFAULT_DELIMITER)));
-                        executeStatement(connection, command.toString());
-                        command = null;
+                        sqlBuffer.append(line.substring(0, line.lastIndexOf(DEFAULT_DELIMITER)));
+                        executeStatement(connection, sqlBuffer.toString());
+                        sqlBuffer = null;
                     }
                 } else {
-                    command.append(line);
-                    command.append(" \n");
+                    sqlBuffer.append(line);
+                    sqlBuffer.append(" \n");
                 }
             }
 
             // Check to see if we have an unexecuted statement in command.
-            if (command != null && command.length() > 0) {
+            if (sqlBuffer != null && sqlBuffer.length() > 0) {
                 logger.info("Last statement in script is missing a terminating delimiter, executing anyway.");
-                executeStatement(connection, command.toString());
+                executeStatement(connection, sqlBuffer.toString());
             }
         } catch (SQLException e) {
             e.fillInStackTrace();
-            logger.error("Error executing: " + command, e);
+            logger.error("Error executing: " + sqlBuffer, e);
             throw e;
         } catch (IOException e) {
             e.fillInStackTrace();
-            logger.error("Error executing: " + command, e);
+            logger.error("Error executing: " + sqlBuffer, e);
             throw e;
         }
     }
