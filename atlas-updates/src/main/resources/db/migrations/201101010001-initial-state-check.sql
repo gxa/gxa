eeb87f0564a68eb87598bd31bfe7d8a789746aae
Alexey Filippov <alf@ebi.ac.uk>
2011-09-16 15:38:58 +0100

    Typo

--- atlas-updates/src/main/resources/db/migrations/201101010001-initial-state-check.sql
+++ atlas-updates/src/main/resources/db/migrations/201101010001-initial-state-check.sql
@@ -1,6 +1,6 @@
 -- The script checking that the DB is in 2.0.9 state
 --
--- In 2.0.10 upwards, we expect DB in one of two states:
+-- From 2.0.10 upwards, we expect DB in one of two states:
 -- 1. Managed by MigrationManager, in which case this script is already run and logged
 -- 2. Not managed yet, in which case which script makes sure it is indeed a 2.0.9 database
 
