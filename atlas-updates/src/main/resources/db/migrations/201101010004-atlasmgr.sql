7ac4c8f4f1de2f90809c3a04393006dd10f460ea
Alexey Filippov <alf@ebi.ac.uk>
2011-09-20 14:47:02 +0100

    Maintaining visibility of code style, part 2.

--- atlas-updates/src/main/resources/db/migrations/201101010004-atlasmgr.sql
+++ atlas-updates/src/main/resources/db/migrations/201101010004-atlasmgr.sql
@@ -116,7 +116,7 @@ begin
 exception
   when NO_DATA_FOUND then
     dbms_output.put_line('No data for ' ||  seq_name || ' - please report');
-  WHEN TOO_MANY_ROWS THEN
+  when TOO_MANY_ROWS then
     dbms_output.put_line('Too many rows for ' ||  seq_name || ' - please report');
 end;
 
