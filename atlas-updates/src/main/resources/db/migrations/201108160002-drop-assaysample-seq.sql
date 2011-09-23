85ce65c23d03cf2d3835cd242218e042f6842b3a
Robert Petryszak <rpetry@ebi.ac.uk>
2011-09-23 14:03:48 +0100

    Added missing migrations code for the recent sql fixes

--- atlas-updates/src/main/resources/db/migrations/201108160002-drop-assaysample-seq.sql
+++ atlas-updates/src/main/resources/db/migrations/201108160002-drop-assaysample-seq.sql
@@ -13,3 +13,7 @@ ALTER TABLE "A2_ASSAYSAMPLE"
     PRIMARY KEY (ASSAYID,SAMPLEID)
     /*PK_TABLESPACE*/
     ENABLE;  
+
+CREATE OR REPLACE VIEW "VWSAMPLEASSAY" ("ASSAYID", "SAMPLEID", "ASSAYACCESSION") AS select asa.ASSAYID, asa.SAMPLEID, a.Accession AssayAccession
+from a2_assaysample asa
+join a2_assay a on a.AssayID = asa.AssayID;
