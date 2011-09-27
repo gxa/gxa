ff89c73c54acd6d86748c040613ab77fdd5f65c8
Alexey Filippov <alf@ebi.ac.uk>
2011-09-27 15:45:55 +0100

    Dropped unused import, converted comments to javadoc so that documentation lookup would work

--- atlas-web/src/main/java/uk/ac/ebi/gxa/web/controller/api/ApiVersionType.java
+++ atlas-web/src/main/java/uk/ac/ebi/gxa/web/controller/api/ApiVersionType.java
@@ -1,18 +1,22 @@
 package uk.ac.ebi.gxa.web.controller.api;
 
-import java.util.regex.Pattern;
-
 /**
  * This class stores an enumeration of valid version types for curation api calls.
  */
 public enum ApiVersionType {
-    // JSON/XML download buttons - change with caution
+    /**
+     * JSON/XML download buttons - change with caution
+     */
     vx,
-    // internal code - we are free to change this api (mapping to another handler/controller, obviously)
+    /**
+     * internal code - we are free to change this api (mapping to another handler/controller, obviously)
+     */
+    //
     v0,
-    //  External API - public contract, we MUST NOT remove calls or change response format.
-    // Adding calls is pretty much fine, but think twice before you add - it'll be painful to change later
-    v1;
-
+    /**
+     * External API - public contract, we MUST NOT remove calls or change response format.
+     * Adding calls is pretty much fine, but think twice before you add - it'll be painful to change later
+     */
+    v1
 }
 
