7adba7a438b7fe736b22e5d303aa4e540c768a04
Olga Melnichuk <olkin@ebi.ac.uk>
2011-10-03 14:19:16 +0100

    Ticket #3129  Left-right arrows don't work on exp page (images are not used in javascript directly any more; only in styles)

--- atlas-web/src/main/webapp/scripts/experiment.js
+++ atlas-web/src/main/webapp/scripts/experiment.js
@@ -591,9 +591,7 @@
             return new ZoomControls();
         }
 
-        var initialized = false;
-
-        var target = opts.target;
+        var target = opts.target || "";
         var zoomin = target + "_zoomin";
         var zoomout = target + "_zoomout";
         var panright = target + "_panright";
@@ -618,11 +616,8 @@
                 return;
             }
 
-            if (!initialized) {
-                drawZoomControls();
-                bindEvents();
-                initialized = true;
-            }
+            drawZoomControls();
+            bindEvents();
 
             if (mode.canZoom) {
                 $("#" + zoomin).show();
@@ -651,76 +646,45 @@
             $("#" + target).css({paddingLeft: 15});
             $("#" + target).html(contents.join(""));
 
-            $("#" + zoomin + " > img").hover(
-                    function() {
-                        $(this).attr("src", "images/zoominO.gif");
-                    },
-                    function() {
-                        $(this).attr("src", "images/zoomin.gif");
-                    }).mousedown(function() {
-                $(this).attr("src", "images/zoominC.gif");
-            }).mouseup(function() {
-                $(this).attr("src", "images/zoominO.gif");
-            });
-
-            $("#" + zoomout + " > img").hover(
-                    function() {
-                        $(this).attr("src", "images/zoomoutO.gif");
-                    },
-                    function() {
-                        $(this).attr("src", "images/zoomout.gif");
-                    }).mousedown(function() {
-                $(this).attr("src", "images/zoomoutC.gif");
-            }).mouseup(function() {
-                $(this).attr("src", "images/zoomoutO.gif");
-            });
-
-            $("#" + panright + " > img").hover(
-                    function() {
-                        $(this).attr("src", "images/panrightO.gif");
-                    },
-                    function() {
-                        $(this).attr("src", "images/panright.gif");
-                    }).mousedown(function() {
-                $(this).attr("src", "images/panrightC.gif");
-            }).mouseup(function() {
-                $(this).attr("src", "images/panrightO.gif");
-            });
-
-            $("#" + panleft + " > img").hover(
-                    function() {
-                        $(this).attr("src", "images/panleftO.gif");
-                    },
-                    function() {
-                        $(this).attr("src", "images/panleft.gif");
-                    }).mousedown(function() {
-                $(this).attr("src", "images/panleftC.gif");
-            }).mouseup(function() {
-                $(this).attr("src", "images/panleftO.gif");
-            });
+            $("#" + target).children()
+                .hover(
+                function(e) {
+                    $(this).addClass("hovered");
+                },
+                function(e) {
+                    $(this).removeClass("hovered");
+                })
+                .mousedown(
+                function(e) {
+                    $(this).addClass("clicked");
+                    return false;
+                })
+                .mouseup(
+                function(e) {
+                    $(this).removeClass("clicked");
+                    return false;
+                });
         }
 
         function bindEvents() {
-            $("#" + zoomin).bind("click", function() {
+            $("#" + zoomin).bind("click", function(e) {
                 triggerZoomInEvent();
             });
 
-            $("#" + zoomout).bind("click", function() {
-                triggerZoomOutEvent();
+            $("#" + zoomout).bind("click", function(e) {
+               triggerZoomOutEvent();
             });
 
             // zoom out completely on double click
-            $("#" + zoomout).bind("dblclick", function() {
+            $("#" + zoomout).bind("dblclick", function(e) {
                 triggerZoomOutEvent(true);
             });
 
-            $("#" + panright + " > img").unbind("click");
-            $("#" + panright + " > img").bind("click", function() {
+            $("#" + panright).bind("click", function(e) {
                 triggerPanRightEvent();
             });
 
-            $("#" + panleft + " > img").unbind("click");
-            $("#" + panleft + " > img ").bind("click", function() {
+            $("#" + panleft).bind("click", function(e) {
                 triggerPanLeftEvent();
             });
         }
