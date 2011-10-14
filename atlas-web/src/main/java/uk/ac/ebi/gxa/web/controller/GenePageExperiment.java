8b32228bedd5b8fe8217d45c957ef802a71157c7
Robert Petryszak <rpetry@ebi.ac.uk>
2011-10-14 16:02:37 +0100

    Ticket #3157 - Fixing the failed merge from develop

--- atlas-web/src/main/java/uk/ac/ebi/gxa/web/controller/GenePageExperiment.java
+++ atlas-web/src/main/java/uk/ac/ebi/gxa/web/controller/GenePageExperiment.java
@@ -4,13 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.ac.ebi.gxa.statistics.EfAttribute;
 import uk.ac.ebi.gxa.statistics.ExperimentResult;
-import uk.ac.ebi.microarray.atlas.model.Assay;
-import uk.ac.ebi.microarray.atlas.model.Asset;
 import uk.ac.ebi.microarray.atlas.model.Experiment;
-import uk.ac.ebi.microarray.atlas.model.Sample;
 
-import java.util.Date;
-import java.util.List;
 import java.util.Set;
 
 public class GenePageExperiment {
@@ -22,10 +17,6 @@ public class GenePageExperiment {
     public GenePageExperiment(Experiment experiment, ExperimentResult experimentInfo) {
         this.experiment = experiment;
         this.experimentInfo = experimentInfo;
-    }
-
-    public Experiment getExperiment() {
-        return experiment;
     }
 
     public EfAttribute getHighestRankAttribute() {
@@ -48,48 +39,8 @@ public class GenePageExperiment {
         return experiment.getDescription();
     }
 
-    public String getAbstract() {
-        return experiment.getAbstract();
-    }
-
-    public String getPerformer() {
-        return experiment.getPerformer();
-    }
-
-    public String getLab() {
-        return experiment.getLab();
-    }
-
-    public Date getLoadDate() {
-        return experiment.getLoadDate();
-    }
-
     public String getPubmedId() {
         return experiment.getPubmedId();
-    }
-
-    public List<Asset> getAssets() {
-        return experiment.getAssets();
-    }
-
-    public List<Assay> getAssays() {
-        return experiment.getAssays();
-    }
-
-    public List<Sample> getSamples() {
-        return experiment.getSamples();
-    }
-
-    public List<String> getSpecies() {
-        return experiment.getSpecies();
-    }
-
-    public boolean isPrivate() {
-        return experiment.isPrivate();
-    }
-
-    public boolean isCurated() {
-        return experiment.isCurated();
     }
 
     /**
