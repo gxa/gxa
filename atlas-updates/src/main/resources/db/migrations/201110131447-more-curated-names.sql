a37c6b6d591454af396c937e5f5c9bd3ba47756c
Alexey Filippov <alf@ebi.ac.uk>
2011-10-17 15:52:48 +0100

    Trac 2849 - Display Name for properties and values - dropping explicit definitions for factors which can be prettified automatically

--- atlas-updates/src/main/resources/db/migrations/201110131447-more-curated-names.sql
+++
@@ -1,8 +0,0 @@
-update a2_property set displayname='15 meta-groups' where name='15_meta_groups';
-update a2_property set displayname='369 meta-groups' where name='369_groups';
-update a2_property set displayname='4 meta-groups' where name='4_meta_groups';
-update a2_property set displayname='6 meta-groups' where name='6_meta_groups';
-update a2_property set displayname='Alive at endpoint' where name='alive_at_endpoint';
-update a2_property set displayname='Blood/non-blood meta-groups' where name='blood_nonblood_meta_groups';
-update a2_property set displayname='Expression' where name='expression';
-update a2_property set displayname='Parturition period' where name='parturition period';
