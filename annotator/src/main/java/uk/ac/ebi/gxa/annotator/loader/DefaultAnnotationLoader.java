d0840a634d48f2481d394676138fe10ea910834a
Nataliya Sklyar <nsklyar@ebi.ac.uk>
2011-10-20 15:56:35 +0100

    removed extra thread

--- annotator/src/main/java/uk/ac/ebi/gxa/annotator/loader/DefaultAnnotationLoader.java
+++ annotator/src/main/java/uk/ac/ebi/gxa/annotator/loader/DefaultAnnotationLoader.java
@@ -26,10 +26,7 @@ package uk.ac.ebi.gxa.annotator.loader;
 import uk.ac.ebi.gxa.annotator.loader.listner.AnnotationLoaderListener;
 import uk.ac.ebi.gxa.exceptions.LogUtil;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
 
 /**
  * User: nsklyar
@@ -49,23 +46,15 @@ public class DefaultAnnotationLoader implements AnnotationLoader {
     public void annotate(final AnnotationCommand annotationCommand, final AnnotationLoaderListener listener) {
         annotationCommand.setAnnotatorFactory(annotatorFactory);
 
-        final Future<Boolean> task = executor.submit(new Callable<Boolean>() {
-            public Boolean call() {
-                annotationCommand.execute(listener);
-                return true;
-            }
-        });
-
-        new Thread(new Runnable() {
+        executor.submit(new Runnable() {
             public void run() {
                 try {
-                    task.get();
-                } catch (InterruptedException e) {
-                    throw LogUtil.createUnexpected("Annotation/mapping update task failed! ", e);
-                } catch (ExecutionException e) {
-                    throw LogUtil.createUnexpected("Annotation/mapping update task failed! ", e.getCause());
+                    annotationCommand.execute(listener);
+                } catch (Throwable e) {
+                    listener.buildError(e);
+                    throw LogUtil.createUnexpected("Unexpected exception", e);
                 }
             }
-        }).start();
+        });
     }
 }
