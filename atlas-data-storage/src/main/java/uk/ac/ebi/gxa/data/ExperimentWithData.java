ead2f0e611654989d42c934c8e66a6421ca514b0
Alexey Filippov <afilippov@acm.org>
2011-11-01 11:51:52 +0000

    Unused method

--- atlas-data-storage/src/main/java/uk/ac/ebi/gxa/data/ExperimentWithData.java
+++ atlas-data-storage/src/main/java/uk/ac/ebi/gxa/data/ExperimentWithData.java
@@ -173,23 +173,6 @@ public class ExperimentWithData implements Closeable {
         return getProxy(arrayDesign).getSamplesToAssays();
     }
 
-    /**
-     * @param arrayDesign
-     * @param iAssay
-     * @return List of sample indexes corresponding to assay at index iAssay
-     * @throws AtlasDataException
-     */
-    public List<Integer> getSamplesForAssay(ArrayDesign arrayDesign, int iAssay) throws AtlasDataException {
-        int[][] samplesToAssayMap = getSamplesToAssays(arrayDesign);
-        ArrayList<Integer> result = new ArrayList<Integer>();
-        for (int i = 0; i != samplesToAssayMap.length; i++) {
-            if (1 == samplesToAssayMap[i][iAssay]) {
-                result.add(i);
-            }
-        }
-        return result;
-    }
-
     public String[] getDesignElementAccessions(ArrayDesign arrayDesign) throws AtlasDataException {
         String[] array = designElementAccessions.get(arrayDesign);
         if (array == null) {
