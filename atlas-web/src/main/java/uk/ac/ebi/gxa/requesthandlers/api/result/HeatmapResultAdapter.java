da5be9779f5dc705a088509892269b9a4a7ce028
Alexey Filippov <alf@ebi.ac.uk>
2011-11-10 14:53:07 +0000

    Trac 3222: Closing the EWD where it was open

--- atlas-web/src/main/java/uk/ac/ebi/gxa/requesthandlers/api/result/HeatmapResultAdapter.java
+++ atlas-web/src/main/java/uk/ac/ebi/gxa/requesthandlers/api/result/HeatmapResultAdapter.java
@@ -29,6 +29,8 @@ import com.google.common.base.Function;
 import com.google.common.base.Predicates;
 import com.google.common.collect.Collections2;
 import com.google.common.collect.Iterators;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uk.ac.ebi.gxa.dao.ExperimentDAO;
 import uk.ac.ebi.gxa.dao.exceptions.RecordNotFoundException;
 import uk.ac.ebi.gxa.data.AtlasDataDAO;
@@ -50,6 +52,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 
+import static com.google.common.io.Closeables.closeQuietly;
 import static uk.ac.ebi.gxa.utils.CollectionUtil.makeMap;
 
 /**
@@ -60,6 +63,8 @@ import static uk.ac.ebi.gxa.utils.CollectionUtil.makeMap;
  * @author pashky
  */
 public class HeatmapResultAdapter implements ApiQueryResults<HeatmapResultAdapter.ResultRow> {
+    private final static Logger log = LoggerFactory.getLogger(HeatmapResultAdapter.class);
+
     private final AtlasStructuredQueryResult r;
     private final ExperimentDAO experimentDAO;
     private final AtlasDataDAO atlasDataDAO;
@@ -121,18 +126,20 @@ public class HeatmapResultAdapter implements ApiQueryResults<HeatmapResultAdapte
                                 Iterators.filter(expiter(), Predicates.<Object>notNull()),
                                 new Function<ExperimentResult, ListResultRowExperiment>() {
                                     public ListResultRowExperiment apply(@Nonnull ExperimentResult e) {
+                                        ExperimentWithData ewd = null;
                                         try {
-                                            ExperimentWithData ewd = getExperiment(e.getAccession());
+                                            ewd = getExperiment(e.getAccession());
                                             PTRank ptRank = e.getPValTStatRank();
                                             UpDownExpression expression = toExpression(ptRank);
                                             float pVal = getPValueFromNcdf(ewd, e, (long) row.getGene().getGeneId(), ptRank.getPValue());
                                             // For up/down expressions replace that rounded pval from bitindex with the accurate pvalue from ncdfs
                                             return new ListResultRowExperiment(ewd.getExperiment(), pVal, null, expression);
-
                                         } catch (RecordNotFoundException rnfe) {
-                                            // Quiesce - no experiment matching e.getAccession() was found
+                                            log.debug("Quiesce - no experiment matching e.getAccession() was found");
+                                            return null;
+                                        } finally {
+                                            closeQuietly(ewd);
                                         }
-                                        return null;
                                     }
                                 }),
                         Predicates.<ListResultRowExperiment>notNull());
@@ -262,26 +269,22 @@ public class HeatmapResultAdapter implements ApiQueryResults<HeatmapResultAdapte
      * @return accurate pValue in ncdf corresponding to roundedPVal-bestEf-bestEfv-geneId in bit index
      */
     private float getPValueFromNcdf(ExperimentWithData ewd, ExperimentResult e, long geneId, float roundedPVal) {
-        try {
-            UpDownExpression expression = toExpression(e.getPValTStatRank());
-            EfAttribute attr = e.getHighestRankAttribute();
-            if (attr instanceof EfvAttribute) {
-                final String efv = ((EfvAttribute) attr).getEfv();
-                if (expression.isUp()) {
-                    final ExpressionAnalysis expressionAnalysis = ewd.getBestEAForGeneEfEfvInExperiment(geneId, attr.getEf(),
-                            efv, UpDownCondition.CONDITION_UP);
-                    return expressionAnalysis == null ? roundedPVal : expressionAnalysis.getPValAdjusted();
-                }
-                if (expression.isDown()) {
-                    final ExpressionAnalysis expressionAnalysis = ewd.getBestEAForGeneEfEfvInExperiment(geneId, attr.getEf(),
-                            efv, UpDownCondition.CONDITION_DOWN);
-                    return expressionAnalysis == null ? roundedPVal : expressionAnalysis.getPValAdjusted();
-                }
+        UpDownExpression expression = toExpression(e.getPValTStatRank());
+        EfAttribute attr = e.getHighestRankAttribute();
+        if (attr instanceof EfvAttribute) {
+            final String efv = ((EfvAttribute) attr).getEfv();
+            if (expression.isUp()) {
+                final ExpressionAnalysis expressionAnalysis = ewd.getBestEAForGeneEfEfvInExperiment(geneId, attr.getEf(),
+                        efv, UpDownCondition.CONDITION_UP);
+                return expressionAnalysis == null ? roundedPVal : expressionAnalysis.getPValAdjusted();
             }
-            // gave up
-            return roundedPVal;
-        } finally {
-            ewd.close();
+            if (expression.isDown()) {
+                final ExpressionAnalysis expressionAnalysis = ewd.getBestEAForGeneEfEfvInExperiment(geneId, attr.getEf(),
+                        efv, UpDownCondition.CONDITION_DOWN);
+                return expressionAnalysis == null ? roundedPVal : expressionAnalysis.getPValAdjusted();
+            }
         }
+        // gave up
+        return roundedPVal;
     }
 }
