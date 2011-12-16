4f5b681148e9f9e18f478868fbe4692c2084bdb5
Robert Petryszak <rpetry@ebi.ac.uk>
2011-12-16 17:15:05 +0000

    Configured to pass notification email and atlas url/port as parameters

--- scripts/cron/ebeye_export.sh
+++ scripts/cron/ebeye_export.sh
@@ -8,22 +8,28 @@
 # file will hopefully have been generated and is just sitting in a temp directory ready to be served by Atlas.
 # This script is run as cron by tomcat user on machine dukkha. 
 
+if [ $# -eq 0 ]; then
+        echo "Usage: $0 INTERNAL_ATLAS_PROD_INSTANCE_URL INTERNAL_ATLAS_PROD_INSTANCE_PORT ERROR_NOTIFICATION_EMAILADDRESS"
+        echo "e.g. $0 <atlas_url> <atlas_port> <notifcation_email_address>"
+        exit;
+fi
+
 for attempt_num in 1 2 3 4 5 
 do
-  curl http://www.ebi.ac.uk/gxa/download/ebeye_export.xml -o /nas/microarray/home/cronjobs/public/xml/gxa/ebeye_export.xml.zip.tmp
+  curl http://$1:$2/gxa/download/ebeye_export.xml -o /nas/microarray/home/cronjobs/public/xml/gxa/ebeye_export.xml.zip.tmp
   format=$(file /nas/microarray/home/cronjobs/public/xml/gxa/ebeye_export.xml.zip.tmp | awk '{ print $2}')
   s=$(ls -lah /nas/microarray/home/cronjobs/public/xml/gxa/ebeye_export.xml.zip.tmp | awk '{ print $5}')
   echo "attempt $attempt_num : Downloaded ebeye_export.xml.zip size: $s and format: $format"
   # Send an error email and re-try after 1 hr if ebeye_export.xml.zip.tmp generation is not completed yet (file cmd doesn't recognize it as being in zip format)
   if [ $format != 'Zip' ] ; then
-    mailx -s"attempt $attempt_num : `whoami`@`hostname` cron error : $0 downloaded ebeye_export.xml.zip in incorrect format: $format - re-trying after 1 hr" atlas-developers@ebi.ac.uk < /dev/null
+    mailx -s"attempt $attempt_num : `whoami`@`hostname` cron error : $0 downloaded ebeye_export.xml.zip in incorrect format: $format - re-trying after 1 hr" $3 < /dev/null
     sleep 3600 # sleep 1h
   else 
     mv /nas/microarray/home/cronjobs/public/xml/gxa/ebeye_export.xml.zip.tmp /nas/microarray/home/cronjobs/public/xml/gxa/ebeye_export.xml.zip
     success_msg="attempt $attempt_num: successfully saved /nas/microarray/home/cronjobs/public/xml/gxa/ebeye_export.xml.zip file"
     echo "$success_msg"
     if [ $attempt_num -gt 1 ] ; then # there had been a failure that's now been fixed - email $success_msg
-	mailx -s"$success_msg" atlas-developers@ebi.ac.uk < /dev/null
+	mailx -s"$success_msg" $3 < /dev/null
     fi
     break # break out of the loop - we're done
   fi
