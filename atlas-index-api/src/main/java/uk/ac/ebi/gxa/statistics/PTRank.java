fc0d6608bb01b22e195927a04841cae2acb7c7bd
Robert Petryszak <rpetry@ebi.ac.uk>
2011-12-21 13:00:59 +0000

    Ticket #3327 - Prevented NA p/t's being included in bit index and being treated as DOWN expression stats; modified logging to focus on slower parts on index building process.

--- atlas-index-api/src/main/java/uk/ac/ebi/gxa/statistics/PTRank.java
+++ atlas-index-api/src/main/java/uk/ac/ebi/gxa/statistics/PTRank.java
@@ -1,19 +1,22 @@
 package uk.ac.ebi.gxa.statistics;
 
+import uk.ac.ebi.gxa.exceptions.LogUtil;
+
 import javax.annotation.concurrent.Immutable;
 import java.io.Serializable;
 
 import static java.lang.Float.compare;
-import static java.lang.Float.isNaN;
 import static java.lang.Math.abs;
 import static java.lang.Math.round;
+import static uk.ac.ebi.microarray.atlas.model.UpDownExpression.isPvalValid;
+import static uk.ac.ebi.microarray.atlas.model.UpDownExpression.isTStatValid;
 
 /**
  * This class is used as a key in SortedMaps to achieve sorting (by pval/tstat rank) of experiments for an OR list attributes (c.f. Statistics class)
  */
 @Immutable
 public final class PTRank implements Serializable, Comparable<PTRank> {
-    private static final long serialVersionUID = 201106071204L;
+    private static final long serialVersionUID = 6825217040776538478L;
     public static final float PRECISION = 1e-3F;
     // pValue rounded off to 3 decimal places - c.f. PRECISION
     private final float pValue;
@@ -26,6 +29,10 @@ public final class PTRank implements Serializable, Comparable<PTRank> {
     }
 
     public static PTRank of(float p, float t) {
+        if (!isPvalValid(p))
+            throw LogUtil.createUnexpected("Invalid pValue: " + p);
+        if (!isTStatValid(t))
+            throw LogUtil.createUnexpected("Invalid tStatistic: " + t);
         return new PTRank(roundToPrecision(p), getTStatRank(t));
     }
 
@@ -54,45 +61,18 @@ public final class PTRank implements Serializable, Comparable<PTRank> {
     }
 
     /**
-     * 1. If absolute tStat ranks differ, return the higher tStatRank first
-     * 2. If absolute tStat ranks are the same:
-     * a. if pVals are different, return the lower pVal first
-     * b. if pVals are the same, return the lower actual (i.e. non-absolute) tStatRank first
+     * Defines natural order descending by absolute value of T first, then ascending by P
      * <p/>
-     * NB. The ordering maintained by a set must be consistent with equals if it is to correctly implement the Set interface. This is so because the Set i/f is
-     * defined in terms of the equals operation, but a TreeSet instance (c.f. Statistics.pValuesTStatRanks map) performs all element comparisons using its compareTo
-     * method, so two elements that are deemed equal by this method are, from the standpoint of the set, equal.
-     * <p/>
-     * 2c. serves to ensure that contract between compareTo() and equal() is preserved, but also ensures that two PvalTstatRanks (pVal=0 tStatRank=3) and (pVal=0 tStatRank=-3)
-     * sre stored as two distinct objects in a TreeSet. This is because PTRank class serves a dual function:
-     * - from UP/DOWN Atlas data ordering point of view, (pVal=0 tStatRank=3) and (pVal=0 tStatRank=-3) are equivalent
-     * - from UP vs DOWN point of view they are not.
-     * Hence, the only way I could think of for supporting both requirements in Statistics.pValuesTStatRanks was to store both PvalTstatRanks as distinct from each other,
-     * and yet co-collacted in the TreeSet used in the map.
+     * Note that there is one case when <code>compareTo(PTRank)</code>
+     * is NOT compatible with {@link #equals(Object)}: it happens is P values are equals, and T values are opposite.
      *
-     * @param o the object to be compared.
+     * @param o the DE candidate to be compared.
      * @return a negative integer, zero, or a positive integer as this object
-     *         is less than, equal to, or greater than the specified object.
-     * @throws ClassCastException if the specified object's type prevents it
-     *                            from being compared to this object.
+     *         is less than, equal to, or greater than the specified candidate.
      */
     public int compareTo(PTRank o) {
-        if (abs(getTStatRank()) != abs(o.getTStatRank())) {
-            return abs(o.getTStatRank()) - abs(getTStatRank()); // higher absolute value of tStatRank comes first
-        }
-
-        if (isNaN(getPValue()) || getPValue() > 1) // NA pVal for this experiment
-            return 1; // the other PTRank comes first
-
-        if (isNaN(o.getPValue()) || o.getPValue() > 1) // NA pVal for the compared experiment
-            return -1; // this PTRank comes first
-
-        if (compare(getPValue(), o.getPValue()) == 0)
-            // if pvals are different, return the lower actual value of tStatRank
-            // (arbitrary if it's lower or higher here - it's just that one of them has to come first)
-            return getTStatRank() - o.getTStatRank();
-
-        return compare(getPValue(), o.getPValue()); // lower pVals come first
+        int result = -compare(abs(getTStatRank()), abs(o.getTStatRank()));
+        return result != 0 ? result : compare(getPValue(), o.getPValue());
     }
 
 
