80b90fe63e3042346d575c7a1c4389dab3e05f70
Robert Petryszak <rpetry@ebi.ac.uk>
2012-01-16 17:46:26 +0000

    Ticket #2868 - Added additional data reporting to help analyze why an experiment has no statistics

--- scripts/cron/data_sanity/find_no_stats_exps.sh
+++ scripts/cron/data_sanity/find_no_stats_exps.sh
@@ -14,6 +14,7 @@ for stats_file in $(find $NCDF_DIR -name *_statistics.nc); do
    tstat=`ncdump -v PVAL $stats_file | egrep -v PVAL | egrep -v TSTAT`
    pval=`ncdump -v TSTAT $stats_file | egrep -v PVAL | egrep -v TSTAT`
    if [ "$pval" == "$tstat" ]; then
+
          # Now check if experiment has (potentially) not enough efvs to make a differential call
          data_file=`echo $stats_file | sed 's|_statistics|_data|'`
          num_efs=`ncdump -v EF $data_file | grep 'EF = ' | awk -F" = " '{print $2}' | sed 's|[\s;]||g'`
@@ -21,7 +22,18 @@ for stats_file in $(find $NCDF_DIR -name *_statistics.nc); do
          if [ $num_unique_efvs -le $num_efs ]; then
             echo "$stats_file: efs = $num_efs; unique efvs = $num_unique_efvs"
          else
-            echo "$stats_file"
+            # Now check how many DE's are being analysed in an experiment - it looks
+            # (c.f. http://comments.gmane.org/gmane.science.biology.informatics.conductor/30292) as if limma's lmFit function
+            # throws the following error if only one DE is being tested:
+            # Error in fit$effects[(fit$rank + 1):narrays, , drop = FALSE] :
+            # incorrect number of dimensions
+
+            num_des=`ncdump -v EF $data_file | grep 'DE = ' | awk -F" = " '{print $2}' | sed 's|[\s;]||g'`
+            if [ $num_des -eq 1 ]; then
+                echo "$stats_file: num of DE's = $num_des"
+            else
+                echo "$stats_file"
+            fi
          fi
    fi
 done
