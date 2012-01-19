987606798eef87cee804f3a941830765ffa66322
Olga Melnichuk <olkin@ebi.ac.uk>
2012-01-19 18:04:52 +0000

    MartBioEntitiesLoader test.. TODO

--- annotator/src/test/java/uk/ac/ebi/gxa/annotator/loader/biomart/MartBioEntitiesLoaderTest.java
+++ annotator/src/test/java/uk/ac/ebi/gxa/annotator/loader/biomart/MartBioEntitiesLoaderTest.java
@@ -23,55 +23,105 @@
 package uk.ac.ebi.gxa.annotator.loader.biomart;
 
 import org.junit.Test;
+import uk.ac.ebi.gxa.annotator.loader.data.BioEntityData;
 import uk.ac.ebi.gxa.annotator.loader.data.InvalidAnnotationDataException;
+import uk.ac.ebi.gxa.annotator.loader.util.InvalidCSVColumnException;
 import uk.ac.ebi.gxa.annotator.model.BioMartAnnotationSource;
+import uk.ac.ebi.microarray.atlas.model.bioentity.BioEntity;
+import uk.ac.ebi.microarray.atlas.model.bioentity.BioEntityType;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import static org.junit.Assert.assertTrue;
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+import static uk.ac.ebi.gxa.annotator.AnnotationSourceFactory.newBioMartAnnotationSource;
 
 /**
+ * ID_PROPERTY_1    ID_PROPERTY_2
+ * ENSBTAT00000057520  ENSBTAG00000039669
+ * ENSBTAT00000049990  ENSBTAG00000039669
+ * ENSBTAT00000015116  ENSBTAG00000025314
+ *
  * @author Olga Melnichuk
  */
 public class MartBioEntitiesLoaderTest {
 
+    private static Map<String, List<String>> TSV = new HashMap<String, List<String>>() {
+        {
+            put("type1", asList("ENSBTAT00000057520", "ENSBTAT00000049990", "ENSBTAT00000015116"));
+            put("type2", asList("ENSBTAG00000039669", "ENSBTAG00000039669", "ENSBTAG00000025314"));
+        }
+    };
+
     @Test
-    public void test() throws InvalidAnnotationDataException {
-        //TODO
-         assertTrue(true);
+    public void test() throws InvalidAnnotationDataException, BioMartException, IOException, InvalidCSVColumnException {
+        BioMartAnnotationSource annotSource = newAnnotationSource();
+
+        BioEntityData.Builder builder = new BioEntityData.Builder(annotSource.getOrganism());
+        new MartBioEntitiesLoader(annotSource, newMartClient()).load(builder);
+
+        BioEntityData data = builder.build(annotSource.getTypes());
+        assertEquals(annotSource.getOrganism(), data.getOrganism());
+
+        for (BioEntityType type : annotSource.getTypes()) {
+            Collection<BioEntity> items = data.getBioEntitiesOfType(type);
+            assertEquals(3, items.size());
+            //TODO
+        }
     }
 
     private BioMartAnnotationSource newAnnotationSource() {
-        //TODO
-        return null;
+        return newBioMartAnnotationSource()
+                .type("type1", "prop1", "prop2")
+                .type("type2", "prop3", "prop4")
+                .property("prop1", "extProp1")
+                .property("prop2", "extProp2")
+                .property("prop3", "extProp3")
+                .property("prop4", "extProp4")
+                .create();
     }
-    
+
     private MartServiceClient newMartClient() {
+
+        StringBuilder sb = new StringBuilder();
+        int ncol = TSV.size();
+        for (int i = 0; i < ncol; i++) {
+            if (i > 0) {
+                sb.append("\t");
+            }
+            sb.append("COLUMN_").append(i);
+        }
+        sb.append("\n");
+        //TODO
+        final String columns = sb.toString();
+        final int size = TSV.entrySet().iterator().next().getValue().size();
+
+
         return new MartServiceClient() {
             @Override
             public InputStream runQuery(Collection<String> attributes) throws BioMartException, IOException {
-                //TODO
-                return null;
+                return new ByteArrayInputStream(columns.getBytes("UTF-8"));
             }
 
             @Override
             public int runCountQuery(Collection<String> attributes) throws BioMartException, IOException {
-                //TODO
-                return 0;
+                return size;
             }
 
             @Override
             public InputStream runAttributesQuery() throws BioMartException, IOException {
-                //TODO
-                return null;
+                throw new UnsupportedOperationException();
             }
 
             @Override
             public InputStream runDatasetListQuery() throws BioMartException, IOException {
-                //TODO
-                return null;
+                throw new UnsupportedOperationException();
             }
         };
     }
