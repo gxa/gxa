82263074979c1a273a5c77083c54d9c0ee60b7fa
Robert Petryszak <rpetry@ebi.ac.uk>
2012-02-22 10:03:44 +0000

    Ticket 2868 - Exclude bam files by default

--- sql/package_ncdfs.sh
+++ sql/package_ncdfs.sh
@@ -2,13 +2,15 @@
 # Releasing Atlas2 NetCDF files
 
 if [ $# -ne 3 ]; then
-	echo "Usage: $0 ATLAS_CONNECTION ATLAS_NCDF_PATH ATLAS_RELEASE"
+	echo "Usage: $0 ATLAS_CONNECTION ATLAS_NCDF_PATH ATLAS_RELEASE (withbams)"
+	echo "'withbams' option includes assays and annotations directories (excluded by default)"
 	exit;
 fi
 
 ATLAS_CONNECTION=$1
 ATLAS_NCDF_PATH=$2
 ATLAS_RELEASE=$3
+WITH_BAMS=$4
 
 echo "Packing the NetCDFs"
 
@@ -19,6 +21,13 @@ echo "Packing the NetCDFs"
 # E-GEOD-5348
 # E-GEOD-7149
 # E-GEOD-7177
-sqlplus -S $ATLAS_CONNECTION @ncdfs-to-export.sql | \
-  awk '{ split($1, a, "-"); print "ncdf/" a[2] "/" (a[3] < 100 ? "" : int(a[3]/100)) "00/" $1 "/" }'  | \
-  xargs tar rv -C $ATLAS_NCDF_PATH/.. -f $ATLAS_RELEASE-ncdf.tar
+
+if [ ! -z "$WITH_BAMS" ]; then
+   sqlplus -S $ATLAS_CONNECTION @ncdfs-to-export.sql | \
+     awk '{ split($1, a, "-"); print "ncdf/" a[2] "/" (a[3] < 100 ? "" : int(a[3]/100)) "00/" $1 "/" }'  | \
+     xargs tar rv -C $ATLAS_NCDF_PATH/.. -f $ATLAS_RELEASE-ncdf.tar
+else
+   sqlplus -S $ATLAS_CONNECTION @ncdfs-to-export.sql | \
+     awk '{ split($1, a, "-"); print "ncdf/" a[2] "/" (a[3] < 100 ? "" : int(a[3]/100)) "00/" $1 "/" }'  | \
+     xargs tar rv -C $ATLAS_NCDF_PATH/.. --exclude 'assays' --exclude 'annotations' -f $ATLAS_RELEASE-ncdf.tar
+fi
