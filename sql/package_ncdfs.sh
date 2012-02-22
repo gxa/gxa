344296baa5df19a54fa076d95fde81d8e03c99a3
Robert Petryszak <rpetry@ebi.ac.uk>
2012-02-22 17:38:17 +0000

    Ticket 2868 - Added distination argument and changed to always gzip

--- sql/package_ncdfs.sh
+++ sql/package_ncdfs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Releasing Atlas2 NetCDF files
 
-if [ $# -ne 3 ]; then
-	echo "Usage: $0 ATLAS_CONNECTION ATLAS_NCDF_PATH ATLAS_RELEASE (withbams)"
+if [ $# -lt 4 ]; then
+	echo "Usage: $0 ATLAS_CONNECTION ATLAS_NCDF_PATH ATLAS_RELEASE DESTINATION_DIRECTORY (withbams)"
 	echo "'withbams' option includes assays and annotations directories (excluded by default)"
 	exit;
 fi
@@ -10,7 +10,8 @@ fi
 ATLAS_CONNECTION=$1
 ATLAS_NCDF_PATH=$2
 ATLAS_RELEASE=$3
-WITH_BAMS=$4
+DESTINATION_DIRECTORY=$4
+WITH_BAMS=$5
 
 echo "Packing the NetCDFs"
 
@@ -25,9 +26,9 @@ echo "Packing the NetCDFs"
 if [ ! -z "$WITH_BAMS" ]; then
    sqlplus -S $ATLAS_CONNECTION @ncdfs-to-export.sql | \
      awk '{ split($1, a, "-"); print "ncdf/" a[2] "/" (a[3] < 100 ? "" : int(a[3]/100)) "00/" $1 "/" }'  | \
-     xargs tar rv -C $ATLAS_NCDF_PATH/.. -f $ATLAS_RELEASE-ncdf.tar
+     xargs tar rvz -C $ATLAS_NCDF_PATH/.. -f $DESTINATION_DIRECTORY/$ATLAS_RELEASE-ncdf.tar
 else
    sqlplus -S $ATLAS_CONNECTION @ncdfs-to-export.sql | \
      awk '{ split($1, a, "-"); print "ncdf/" a[2] "/" (a[3] < 100 ? "" : int(a[3]/100)) "00/" $1 "/" }'  | \
-     xargs tar rv -C $ATLAS_NCDF_PATH/.. --exclude 'assays' --exclude 'annotations' -f $ATLAS_RELEASE-ncdf.tar
+     xargs tar rvz -C $ATLAS_NCDF_PATH/.. --exclude 'assays' --exclude 'annotations' -f $DESTINATION_DIRECTORY/$ATLAS_RELEASE-ncdf.tar
 fi
