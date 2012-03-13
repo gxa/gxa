5031b06e9abd598935ea0570ce2e3ac8d695bb40
Robert Petryszak <rpetry@ebi.ac.uk>
2012-03-13 08:25:19 +0000

    Ticket 2868 - Removed superfluous join

--- atlas-updates/src/main/resources/db/migrations/201203081255-remove-unused-property-values.sql
+++ atlas-updates/src/main/resources/db/migrations/201203081255-remove-unused-property-values.sql
@@ -2,10 +2,10 @@ delete from A2_propertyvalue pv
 where not exists
 (
 select 1
-from A2_assaypv apv, A2_Assay ass
-where ass.assayid = apv.assayid and apv.propertyvalueid = pv.propertyvalueid
+from A2_assaypv apv
+where apv.propertyvalueid = pv.propertyvalueid
     union all
 select 1
-from A2_samplepv apv, A2_Sample ass
-where ass.sampleid = apv.sampleid and apv.propertyvalueid = pv.propertyvalueid
+from A2_samplepv spv
+where spv.propertyvalueid = pv.propertyvalueid
 );
