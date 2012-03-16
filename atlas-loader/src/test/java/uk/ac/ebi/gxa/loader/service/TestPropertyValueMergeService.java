88646e4e3cc77ef1e944344f62ce05b2c5a3f79d
Nataliya Sklyar <nsklyar@ebi.ac.uk>
2012-03-16 11:48:01 +0000

    test moved to service package

--- atlas-loader/src/test/java/uk/ac/ebi/gxa/loader/service/TestPropertyValueMergeService.java
+++ atlas-loader/src/test/java/uk/ac/ebi/gxa/loader/service/TestPropertyValueMergeService.java
@@ -1,4 +1,26 @@
-package uk.ac.ebi.gxa.loader;
+/*
+ * Copyright 2008-2012 Microarray Informatics Team, EMBL-European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * For further details of the Gene Expression Atlas project, including source code,
+ * downloads and documentation, please see:
+ *
+ * http://gxa.github.com/gxa
+ */
+
+package uk.ac.ebi.gxa.loader.service;
 
 import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
@@ -6,15 +28,14 @@ import org.junit.Test;
 import uk.ac.ebi.arrayexpress2.magetab.datamodel.sdrf.node.attribute.CharacteristicsAttribute;
 import uk.ac.ebi.arrayexpress2.magetab.datamodel.sdrf.node.attribute.FactorValueAttribute;
 import uk.ac.ebi.arrayexpress2.magetab.datamodel.sdrf.node.attribute.UnitAttribute;
-import uk.ac.ebi.gxa.loader.service.PropertyValueMergeService;
+import uk.ac.ebi.gxa.loader.AtlasLoaderException;
+import uk.ac.ebi.gxa.loader.MockFactory;
 import uk.ac.ebi.gxa.utils.Pair;
 
 import java.util.Collections;
 import java.util.List;
 
-import static org.easymock.EasyMock.createMock;
-import static org.easymock.EasyMock.expect;
-import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
