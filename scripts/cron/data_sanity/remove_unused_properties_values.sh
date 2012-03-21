e04cf4e3e28438df1916467dd5d3fbbb70a65735
Robert Petryszak <rpetry@ebi.ac.uk>
2012-03-21 09:04:11 +0000

    Changed reporting of unused properties/values to their deletion

---
+++ scripts/cron/data_sanity/remove_unused_properties_values.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# @author: rpetry
+# @date:   15 Feb 2012
+
+if [ $# -lt 3 ]; then
+        echo "Usage: $0 ATLAS_URL CURAPI_USERNAME CURAPI_PASSWORD"
+        exit;
+fi
+
+ATLAS_URL=$1
+CURAPI_USERNAME=$2
+CURAPI_PASSWORD=$3
+
+# Remove properties not found in any assay/sample
+curl -s -X GET -u ${CURAPI_USERNAME}:${CURAPI_PASSWORD} -v "${ATLAS_URL}/api/curators/v1/properties/unused.json"
+
+# Remove property values not found in any assay/sample
+curl -s -X GET -u ${CURAPI_USERNAME}:${CURAPI_PASSWORD} -v "${ATLAS_URL}/api/curators/v1/properties/values/unused.json"
+
