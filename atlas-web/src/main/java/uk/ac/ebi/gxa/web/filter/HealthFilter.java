ad18964c6c7009b77c0a896286d1abb4e853bc1d
Robert Petryszak <rpetry@ebi.ac.uk>
2012-03-29 12:48:35 +0100

    Ticket 2868 - Minor autoboxing fix.

--- atlas-web/src/main/java/uk/ac/ebi/gxa/web/filter/HealthFilter.java
+++ atlas-web/src/main/java/uk/ac/ebi/gxa/web/filter/HealthFilter.java
@@ -27,7 +27,7 @@ public class HealthFilter implements Filter {
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        request.setAttribute("health", Boolean.valueOf(true));
+        request.setAttribute("health", Boolean.TRUE);
         chain.doFilter(request, response);
     }
 
