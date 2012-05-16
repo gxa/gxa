39d4e3e03b3d960c3c9f677b9a93d49b4d446354
Olga Melnichuk <olkin@ebi.ac.uk>
2012-05-16 16:42:36 +0100

    added ExperimentDesignTableDsvTest

--- atlas-web/src/main/java/uk/ac/ebi/gxa/export/dsv/ExperimentDesignTableDsv.java
+++ atlas-web/src/main/java/uk/ac/ebi/gxa/export/dsv/ExperimentDesignTableDsv.java
@@ -23,18 +23,53 @@
 package uk.ac.ebi.gxa.export.dsv;
 
 import com.google.common.base.Function;
+import uk.ac.ebi.gxa.utils.dsv.DsvColumn;
 import uk.ac.ebi.gxa.utils.dsv.DsvRowIterator;
 import uk.ac.ebi.gxa.web.controller.ExperimentDesignUI;
 
 import javax.annotation.Nullable;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author Olga Melnichuk
  */
 public class ExperimentDesignTableDsv {
+
+    private static enum Columns implements DsvColumn<ExperimentDesignUI.Row>{
+        Assay("assay", "") {
+            @Override
+            public String convert(ExperimentDesignUI.Row row) {
+                return row.getAssayAcc();
+            }
+        },
+        Array("array", "") {
+            @Override
+            public String convert(ExperimentDesignUI.Row row) {
+                return row.getArrayDesignAcc();
+            }
+        };
+
+        private final String name;
+        private final String description;
+
+        private Columns(String name, String description) {
+            this.name = name;
+            this.description = description;
+        }
+
+
+        public abstract String convert(ExperimentDesignUI.Row row);
+
+        @Override
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public String getDescription() {
+            return description;
+        }
+    }
 
     @SuppressWarnings("unchecked")
     public static DsvRowIterator<ExperimentDesignUI.Row> createDsvDocument(Map<String, Object> model) {
@@ -52,20 +87,14 @@ public class ExperimentDesignTableDsv {
             dsvIterator.addColumn(propName, "", propertyValueConverter(i++));
         }
 
-        dsvIterator
-                .addColumn("assay", "", new Function<ExperimentDesignUI.Row, String>() {
-                    @Override
-                    public String apply(@Nullable ExperimentDesignUI.Row row) {
-                        return row.getAssayAcc();
-                    }
-                })
-                .addColumn("array", "", new Function<ExperimentDesignUI.Row, String>() {
-                    @Override
-                    public String apply(@Nullable ExperimentDesignUI.Row row) {
-                        return row.getArrayDesignAcc();
-                    }
-                });
+        dsvIterator.addColumns(permanentColumns());
         return dsvIterator;
+    }
+
+    static List<DsvColumn<ExperimentDesignUI.Row>> permanentColumns() {
+        return Arrays.<DsvColumn<ExperimentDesignUI.Row>>asList(
+                Columns.Assay,
+                Columns.Array);
     }
 
     private static Function<ExperimentDesignUI.Row, String> propertyValueConverter(final int index) {
