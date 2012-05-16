d274ed5529643b56fee841f89e8b1eda6000d709
Robert Petryszak <rpetry@ebi.ac.uk>
2012-05-16 16:48:07 +0100

    Ticket 3489  - Fixed Ensembl query for gene synonyms to reflect changes on the Ensembl side for release 67

--- annotator/src/main/java/uk/ac/ebi/gxa/annotator/loader/biomart/BioMartDbDAO.java
+++ annotator/src/main/java/uk/ac/ebi/gxa/annotator/loader/biomart/BioMartDbDAO.java
@@ -53,12 +53,11 @@ class BioMartDbDAO {
             final String dbName = findSynonymsDBName(dbNameTemplate, version);
             final JdbcTemplate template = createTemplate(dbName);
             return template.query(
-                    "SELECT DISTINCT gene_stable_id.stable_id, external_synonym.synonym \n" +
-                            "FROM gene_stable_id, gene, xref, external_synonym \n" +
-                            "WHERE gene_stable_id.gene_id = gene.gene_id \n" +
-                            "AND gene.display_xref_id = xref.xref_id \n" +
+                    "SELECT DISTINCT gene.gene_id, external_synonym.synonym \n" +
+                            "FROM gene, xref, external_synonym \n" +
+                            "WHERE gene.display_xref_id = xref.xref_id \n" +
                             "AND external_synonym.xref_id = xref.xref_id \n" +
-                            "ORDER BY gene_stable_id.stable_id; ",
+                            "ORDER BY gene.gene_id; ",
                     new RowMapper<Pair<String, String>>() {
                         @Override
                         public Pair<String, String> mapRow(ResultSet rs, int rowNum) throws SQLException {
