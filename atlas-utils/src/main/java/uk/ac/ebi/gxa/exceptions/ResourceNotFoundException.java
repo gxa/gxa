d354278151fe022f09a0ef422e7bad3aac33d98c
bennyfio <bennyfio@bop.windows.ebi.ac.uk>
2012-07-13 13:32:51 +0100

    Added a new constructor that accepts a Throwable cause and set's exception message and cause

--- atlas-utils/src/main/java/uk/ac/ebi/gxa/exceptions/ResourceNotFoundException.java
+++ atlas-utils/src/main/java/uk/ac/ebi/gxa/exceptions/ResourceNotFoundException.java
@@ -2,9 +2,6 @@ package uk.ac.ebi.gxa.exceptions;
 
 /**
  * Thrown when the asked resource (e.g. experiment, gene or other data) not found.
- *
- * @author Olga Melnichuk
- *         Date: Dec 1, 2010
  */
 public class ResourceNotFoundException extends Exception {
     public ResourceNotFoundException(String message) {
@@ -14,4 +11,9 @@ public class ResourceNotFoundException extends Exception {
     public ResourceNotFoundException(String message, Throwable cause) {
         super(message, cause);
     }
+
+    public ResourceNotFoundException(Throwable cause) {
+        super(cause.getMessage(), cause);
+    }
+
 }
