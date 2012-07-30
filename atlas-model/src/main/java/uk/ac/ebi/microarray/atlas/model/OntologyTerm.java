f37deab113cde8448b5401003b21fa56beb4e3cf
bennyfio <bennyfio@ebi.ac.uk>
2012-07-30 10:06:48 +0100

    corrected wrong dependency for the EqualBuilder class

--- atlas-model/src/main/java/uk/ac/ebi/microarray/atlas/model/OntologyTerm.java
+++ atlas-model/src/main/java/uk/ac/ebi/microarray/atlas/model/OntologyTerm.java
@@ -1,7 +1,7 @@
 package uk.ac.ebi.microarray.atlas.model;
 
 import com.google.common.base.Objects;
-import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.EqualsBuilder;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 import uk.ac.ebi.gxa.Temporary;
