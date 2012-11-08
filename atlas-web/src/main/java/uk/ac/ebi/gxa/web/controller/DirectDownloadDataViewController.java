72c3f8e1f2ad80bc01e8d081572d9331dfacf783
jtaubert <jtaubert@ebi.ac.uk>
2012-11-08 12:11:36 +0000

    new direct download functionality as requested by Simon Jupp required for tab separated downloads

---
+++ atlas-web/src/main/java/uk/ac/ebi/gxa/web/controller/DirectDownloadDataViewController.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright 2008-2012 Microarray Informatics Team, EMBL-European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * For further details of the Gene Expression Atlas project, including source code,
+ * downloads and documentation, please see:
+ *
+ * http://gxa.github.com/gxa
+ */
+
+package uk.ac.ebi.gxa.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import uk.ac.ebi.gxa.dao.exceptions.RecordNotFoundException;
+import uk.ac.ebi.gxa.download.ExperimentDownloadData;
+import uk.ac.ebi.gxa.download.dsv.DsvDocumentCreateException;
+import uk.ac.ebi.gxa.download.dsv.DsvDocumentCreator;
+import uk.ac.ebi.gxa.service.DownloadDataService;
+import uk.ac.ebi.gxa.utils.dsv.DsvDocumentWriter;
+import uk.ac.ebi.gxa.utils.dsv.DsvRowIterator;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Collection;
+import java.util.UUID;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import static com.google.common.base.Joiner.on;
+import static com.google.common.io.Closeables.closeQuietly;
+import static uk.ac.ebi.gxa.utils.dsv.DsvFormat.tsv;
+
+/**
+ * Enabling programmatic access to experiment data downloads. Downloads are streamed to the client.
+ */
+@Controller
+public class DirectDownloadDataViewController extends AtlasViewController {
+
+    protected final static Logger log = LoggerFactory.getLogger(DirectDownloadDataViewController.class);
+
+    private ExperimentDownloadData experimentDownloadData;
+
+    @Autowired
+    public DirectDownloadDataViewController(ExperimentDownloadData experimentDownloadData) {
+        this.experimentDownloadData = experimentDownloadData;
+    }
+
+    private String newToken(String... parts) {
+        //TODO proper token generation needed
+        return on("").join(parts);
+    }
+
+    /**
+     * Writes a ZipOutputStream to a HttpServletResponse using DsvDocumentCreators
+     */
+    private void createZipOutput(
+            Collection<? extends DsvDocumentCreator> creators,
+            HttpServletResponse response, String token)
+            throws IOException, DsvDocumentCreateException {
+
+        // set content type and returned filename here
+        response.setContentType("application/zip");
+        response.setHeader("Content-Disposition", "attachment; filename=\"" + token + ".zip\"");
+        response.setHeader("Pragma", "public"); // so it works in IE6&7
+
+        ZipOutputStream zout = null;
+        boolean emptyZip = true;
+        try {
+            zout = new ZipOutputStream(response.getOutputStream());
+
+            for (DsvDocumentCreator docCreator : creators) {
+                DsvRowIterator doc = docCreator.create();
+                zout.putNextEntry(new ZipEntry(docCreator.getName() + ".tab"));
+
+                (new DsvDocumentWriter(tsv().newWriter(new OutputStreamWriter(zout)), null)).write(doc);
+            }
+            zout.closeEntry();
+            emptyZip = false;
+        } finally {
+            if (!emptyZip)
+                closeQuietly(zout);
+        }
+    }
+
+    @RequestMapping(value = "/directDownload/" + DownloadDataService.EXPERIMENT_ANALYTICS, method = RequestMethod.GET)
+    public void directDownloadExperimentAnalytics(
+            @RequestParam("eacc") String expAcc, HttpServletResponse response)
+            throws IOException, DsvDocumentCreateException, RecordNotFoundException {
+
+        Collection<? extends DsvDocumentCreator> creators = experimentDownloadData.newDsvCreatorForAnalytics(expAcc);
+
+        String token = newToken(DownloadDataService.EXPERIMENT_ANALYTICS, "_", expAcc, "_", UUID.randomUUID().toString());
+        log.info("directDownloadExperimentAnalytics(token={})", token);
+
+        createZipOutput(creators, response, token);
+    }
+
+    @RequestMapping(value = "/directDownload/" + DownloadDataService.EXPERIMENT_EXPRESSIONS, method = RequestMethod.GET)
+    public void directDownloadExperimentExpressions(
+            @RequestParam("eacc") String expAcc, HttpServletResponse response)
+            throws IOException, DsvDocumentCreateException, RecordNotFoundException {
+
+        Collection<? extends DsvDocumentCreator> creators = experimentDownloadData.newDsvCreatorForExpressions(expAcc);
+
+        String token = newToken(DownloadDataService.EXPERIMENT_EXPRESSIONS, "_", expAcc, "_", UUID.randomUUID().toString());
+        log.info("directDownloadExperimentExpressions(token={})", token);
+
+        createZipOutput(creators, response, token);
+    }
+
+    @RequestMapping(value = "/directDownload/" + DownloadDataService.EXPERIMENT_DESIGN, method = RequestMethod.GET)
+    public void directDownloadExperimentDesign(
+            @RequestParam("eacc") String expAcc, HttpServletResponse response)
+            throws IOException, DsvDocumentCreateException, RecordNotFoundException {
+
+        Collection<? extends DsvDocumentCreator> creators = experimentDownloadData.newDsvCreatorForDesign(expAcc);
+
+        String token = newToken(DownloadDataService.EXPERIMENT_DESIGN, "_", expAcc, "_", UUID.randomUUID().toString());
+        log.info("directDownloadExperimentDesign(token={})", token);
+
+        createZipOutput(creators, response, token);
+    }
+
+}
