5267adc39fc81d0777706fe8f5bec87761b21faf
jtaubert <jtaubert@ebi.ac.uk>
2012-11-09 15:45:10 +0000

    fix for leaking NetCDF FileCache when streaming for direct download

--- atlas-web/src/main/java/uk/ac/ebi/gxa/web/controller/DirectDownloadDataViewController.java
+++ atlas-web/src/main/java/uk/ac/ebi/gxa/web/controller/DirectDownloadDataViewController.java
@@ -30,23 +30,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import uk.ac.ebi.gxa.dao.exceptions.RecordNotFoundException;
+import uk.ac.ebi.gxa.data.AtlasDataException;
+import uk.ac.ebi.gxa.data.ExperimentWithData;
 import uk.ac.ebi.gxa.download.ExperimentDownloadData;
 import uk.ac.ebi.gxa.download.dsv.DsvDocumentCreateException;
 import uk.ac.ebi.gxa.download.dsv.DsvDocumentCreator;
+import uk.ac.ebi.gxa.exceptions.ResourceNotFoundException;
+import uk.ac.ebi.gxa.export.dsv.ExperimentExpressionDataTableDsv;
 import uk.ac.ebi.gxa.service.DownloadDataService;
+import uk.ac.ebi.gxa.service.experiment.ExperimentDataService;
 import uk.ac.ebi.gxa.utils.dsv.DsvDocumentWriter;
 import uk.ac.ebi.gxa.utils.dsv.DsvRowIterator;
+import uk.ac.ebi.gxa.web.filter.ResourceWatchdogFilter;
+import uk.ac.ebi.microarray.atlas.model.ArrayDesign;
+import uk.ac.ebi.microarray.atlas.model.Experiment;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.Closeable;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 import static com.google.common.base.Joiner.on;
 import static com.google.common.io.Closeables.closeQuietly;
+import static uk.ac.ebi.gxa.data.ExperimentPart.find;
 import static uk.ac.ebi.gxa.utils.dsv.DsvFormat.tsv;
 
 /**
@@ -55,13 +67,60 @@ import static uk.ac.ebi.gxa.utils.dsv.DsvFormat.tsv;
 @Controller
 public class DirectDownloadDataViewController extends AtlasViewController {
 
+    /**
+     * Local copy from ExperimentDownloadData
+     */
+    private class ExperimentExpressionsDocCreator implements DsvDocumentCreator {
+
+        private final String expAcc;
+        private final String adAcc;
+
+        private ExperimentExpressionsDocCreator(String expAcc, String adAcc) {
+            this.adAcc = adAcc;
+            this.expAcc = expAcc;
+        }
+
+        @Override
+        public String getName() {
+            return "ExpRawData-" + expAcc + "-" + adAcc;
+        }
+
+        @Override
+        public DsvRowIterator create() throws DsvDocumentCreateException {
+            try {
+                // ExperimentWithData needs to be closed eventually
+                // otherwise the Netcdf FileCache will leak
+                final ExperimentWithData experimentWithData = expDataService.getExperimentWithData(expAcc);
+                ResourceWatchdogFilter.register(new Closeable() {
+                    public void close() {
+                        closeQuietly(experimentWithData);
+                    }
+                });
+                return ExperimentExpressionDataTableDsv.createDsvDocument(find(experimentWithData, adAcc).getExpressionData());
+            } catch (AtlasDataException e) {
+                throw documentCreateException(e);
+            } catch (ResourceNotFoundException e) {
+                throw documentCreateException(e);
+            } catch (RecordNotFoundException e) {
+                throw documentCreateException(e);
+            }
+        }
+
+        private DsvDocumentCreateException documentCreateException(Throwable e) {
+            return new DsvDocumentCreateException("Can't create experiment expression data dsv doc: acc = " + expAcc + ", ad = " + adAcc, e);
+        }
+    }
+
     protected final static Logger log = LoggerFactory.getLogger(DirectDownloadDataViewController.class);
 
     private ExperimentDownloadData experimentDownloadData;
 
+    private ExperimentDataService expDataService;
+
     @Autowired
-    public DirectDownloadDataViewController(ExperimentDownloadData experimentDownloadData) {
+    public DirectDownloadDataViewController(ExperimentDownloadData experimentDownloadData, ExperimentDataService expDataService) {
         this.experimentDownloadData = experimentDownloadData;
+        this.expDataService = expDataService;
     }
 
     private String newToken(String... parts) {
@@ -119,7 +178,13 @@ public class DirectDownloadDataViewController extends AtlasViewController {
             @RequestParam("eacc") String expAcc, HttpServletResponse response)
             throws IOException, DsvDocumentCreateException, RecordNotFoundException {
 
-        Collection<? extends DsvDocumentCreator> creators = experimentDownloadData.newDsvCreatorForExpressions(expAcc);
+        // copied from ExperimentDownloadData to allow for using custom ExperimentExpressionDocCreator
+        Experiment exp = expDataService.getExperiment(expAcc);
+        List<DsvDocumentCreator> creators = new ArrayList<DsvDocumentCreator>();
+        for (final ArrayDesign ad : exp.getArrayDesigns()) {
+            log.debug("new ExperimentExpressionsDocCreator(eacc=" + expAcc + ", ad=" + ad.getAccession() + ")");
+            creators.add(new ExperimentExpressionsDocCreator(expAcc, ad.getAccession()));
+        }
 
         String token = newToken(DownloadDataService.EXPERIMENT_EXPRESSIONS, "_", expAcc, "_", UUID.randomUUID().toString());
         log.info("directDownloadExperimentExpressions(token={})", token);
