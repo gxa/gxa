cf5e0a3d338980eaee4c413ce9004f9d412dccde
Benedetto Fiorelli <bennyfio@ebi.ac.uk>
2013-01-11 17:19:20 +0000

    added untested method that connect to array express synonyms service

--- annotator/src/main/java/uk/ac/ebi/gxa/annotator/annotationsrc/arraydesign/ArrayExpressConnection.java
+++ annotator/src/main/java/uk/ac/ebi/gxa/annotator/annotationsrc/arraydesign/ArrayExpressConnection.java
@@ -23,6 +23,7 @@
 package uk.ac.ebi.gxa.annotator.annotationsrc.arraydesign;
 
 import au.com.bytecode.opencsv.CSVReader;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.RandomStringUtils;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
@@ -30,9 +31,12 @@ import org.slf4j.LoggerFactory;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.text.MessageFormat;
 
 import static com.google.common.io.Closeables.closeQuietly;
 
@@ -44,19 +48,24 @@ class ArrayExpressConnection {
     final private Logger log = LoggerFactory.getLogger(this.getClass());
 
     private static final String ACC_TEML = "$ACC";
+    //ToDo: test if this services is also accessible from peaches, if so replace the host with peaches...
     private static final String ADF_URL_TEMPLATE = "http://www.ebi.ac.uk/arrayexpress/files/" + ACC_TEML + "/" + ACC_TEML + ".adf.txt";
+
+    private static final String ARRAYDESIGN_SYNONYMS_URL_TEMPLATE = "http://peach.ebi.ac.uk:8480/api/arrays-secondary-accessions.txt?acc={0}";
 
     private static final String AD_NAME = "Array Design Name";
     private static final String PROVIDER = "Provider";
     private static final String TYPE = "Technology Type";
 
+    private String accession;
     private String name = StringUtils.EMPTY;
     private String provider = StringUtils.EMPTY;
     private String type = StringUtils.EMPTY;
     private String accessionMaster = StringUtils.EMPTY;
 
     public ArrayExpressConnection(String accession) {
-        fetchArrayDesignData(accession);
+        this.accession = accession;
+        fetchArrayDesignData();
     }
 
     public String getName() {
@@ -64,7 +73,7 @@ class ArrayExpressConnection {
     }
 
     public String getAccessionMaster() {
-        return accessionMaster;
+        return fetchSynonym();
     }
 
     public String getProvider() {
@@ -75,7 +84,39 @@ class ArrayExpressConnection {
         return type;
     }
 
-    private void fetchArrayDesignData(String accession) {
+    String fetchSynonym() {
+        InputStream inputStream = null;
+        try {
+            URL arrayDesignSynonymsServiceURL = new URL(MessageFormat.format(ARRAYDESIGN_SYNONYMS_URL_TEMPLATE, accession));
+            URLConnection urlConnection = arrayDesignSynonymsServiceURL.openConnection();
+            inputStream = urlConnection.getInputStream();
+            String encoding = urlConnection.getContentEncoding();
+            encoding = encoding == null ? "UTF-8" : encoding;
+            String body = IOUtils.toString(inputStream, encoding);
+
+            log.debug("<fetchSynonym> " + body);
+
+            if (StringUtils.isNotBlank(body)){
+                String[] accessions = body.trim().split("\\s");
+                if (accessions.length > 1){
+                    return accessions[1];
+                }
+            }
+
+            return StringUtils.EMPTY;
+
+        } catch (MalformedURLException e){
+            log.error(e.getMessage(), e);
+            throw new IllegalStateException(e);
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+            return "UNKNOWN";
+        }finally{
+            closeQuietly(inputStream);
+        }
+    }
+
+    private void fetchArrayDesignData() {
         log.info("Fetching Array Design data from ArrayExpress " + accession);
 
         CSVReader csvReader = null;
@@ -101,8 +142,6 @@ class ArrayExpressConnection {
                 //We don't need to read more then 10 line to find data we are interested in
                 if (count++ > 10) break;
 
-                //ToDo: this is just to temporary Mock... will be removed!
-                accessionMaster = RandomStringUtils.randomAlphanumeric(8);
             }
         } catch (FileNotFoundException e) {
             log.warn("Cannot fetch ADF for array design " + accession, e.getMessage());
