d48df12524ee8324bc1d8ce76a64313b76489f4f
Robert Petryszak <rpetry@ebi.ac.uk>
2013-04-29 16:55:45 +0100

    Remove not updated from Ensembl and unused array designs; Removed bad quality micrRNA array designs

---
+++ atlas-updates/src/main/resources/db/migrations/201304291652-remove-unused-or-badquality-arraydesigns.sql
@@ -0,0 +1,2 @@
+delete from A2_arraydesign 
+where accession in ('A-MEXP-121','A-MEXP-27','A-MEXP-28','A-MEXP-294','A-MEXP-585','A-MEXP-935','A-MIRB-02','A-MIRB-03','A-MIRB-04','A-SNGR-1','A-SNGR-11','A-SNGR-13','A-SNGR-15','A-SNGR-2','A-SNGR-7','A-SNGR-8','A-SNGR-9','A-UMCU-1','A-UMCU-4','E-HD4D-1','E-HD4D-2','E-HD4D-3','A-SNGR-10','A-AFFY-40','A-MEXP-86','A-MEXP-258','A-MEXP-620');
