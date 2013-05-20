db459dd5896334462fcff73a7f9423ec9efeb351
nsklyar <nsklyar@ebi.ac.uk>
2013-05-20 14:14:24 +0100

    created method dao method to get all genes and proteins to use in new index builder

--- indexbuilder/src/main/java/uk/ac/ebi/gxa/index/builder/service/NewGeneAtlasIndexBuilderService.java
+++ indexbuilder/src/main/java/uk/ac/ebi/gxa/index/builder/service/NewGeneAtlasIndexBuilderService.java
@@ -82,9 +82,9 @@ public class NewGeneAtlasIndexBuilderService extends IndexBuilderService {
         getLog().info(status);
         progressUpdater.update(status);
 
-        bioEntityTypeDAO.setUseForIndexEnsprotein(true);
-        List<BioEntity> allIndexedBioEntities = bioEntityDAO.getAllGenesFast();
-        bioEntityTypeDAO.setUseForIndexEnsprotein(false);
+//        bioEntityTypeDAO.setUseForIndexEnsprotein(true);
+        List<BioEntity> allIndexedBioEntities = bioEntityDAO.getAllGenesAndProteinsFast();
+//        bioEntityTypeDAO.setUseForIndexEnsprotein(false);
         indexGenes(progressUpdater, allIndexedBioEntities);
     }
 
