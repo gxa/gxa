000c56b774b5ce30f061055b0fd0a9d6b72fb8c1
Robert Petryszak <rpetry@ebi.ac.uk>
2013-07-31 12:26:14 +0100

    Fixed log file name; enhanced logging

--- scripts/cron/web_monitoring/bounce_if_unresponsive.sh
+++ scripts/cron/web_monitoring/bounce_if_unresponsive.sh
@@ -12,15 +12,14 @@ HOSTNAME=$1
 MAXTIME=$2
 ATLAS_NAGIOS_URL="http://${HOSTNAME}:8080/gxa/das/s4/features?segment=ENSG00000139618"
 
-today="`eval date +%Y-%m-%d`"
-log="/tmp/gxa_bounces.$today"
+log="/tmp/gxa_bounces.log"
 
 # Nagios query
 responseTime=`curl -o /dev/null -X GET -s --max-time $MAXTIME -w %{time_total} "$ATLAS_NAGIOS_URL" | awk -F"." '{print $1}'`
 echo "responseTime = $responseTime"
 if [ "$responseTime" -ge "$MAXTIME" ]; then
     touch $log
-    echo `eval date +%k:%M:%S`" - bouncing $HOSTNAME ..." >> $log
+    echo `eval date +%Y-%m-%d %k:%M:%S`" - Response time for $HOSTNAME exceeded $MAXTIME - bouncing $HOSTNAME..." >> $log
     ssh `whoami`@${HOSTNAME} /nfs/public/rw/webadmin/tomcat/bases/fg/tc-fg-gxa/bin/stop
     ssh `whoami`@${HOSTNAME} /nfs/public/rw/webadmin/tomcat/bases/fg/tc-fg-gxa/bin/start
     curl -o /dev/null -s -I 'http://${HOSTNAME}:8080/gxa/fval?q=brca&factor=&type=gene&limit=15'
