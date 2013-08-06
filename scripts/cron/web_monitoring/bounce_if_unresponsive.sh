28b383c5dbd92a163b85ee2221762a43c3995bba
Robert Petryszak <rpetry@ebi.ac.uk>
2013-08-06 16:27:23 +0100

    Modified to also bounce if http code returned != 200

--- scripts/cron/web_monitoring/bounce_if_unresponsive.sh
+++ scripts/cron/web_monitoring/bounce_if_unresponsive.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
-# @author: rpetry
-# @date:   31 Jul 2013
-# This script bounces $HOSTNAME if response time exceeds allowed maximum
+#!/bin/bash                                                                                                                                                                                                                                                  
+# @author: rpetry                                                                                                                                                                                                                                            
+# @date:   31 Jul 2013                                                                                                                                                                                                                                       
+# This script bounces $HOSTNAME if response time exceeds allowed maximum                                                                                                                                                                                      
 
 if [ $# -lt 2 ]; then
         echo "Usage: $0 HOSTNAME MAXTIME"
@@ -15,13 +15,14 @@ ATLAS_NAGIOS_URL="http://${HOSTNAME}:8080/gxa/das/s4/features?segment=ENSG000001
 log="/tmp/gxa_bounces.log"
 
 # Nagios query
-responseTime=`curl -o /dev/null -X GET -s --max-time $MAXTIME -w %{time_total} "$ATLAS_NAGIOS_URL" | awk -F"." '{print $1}'`
-if [ "$responseTime" -ge "$MAXTIME" ]; then
+response=`curl -o /dev/null -X GET -s --max-time $MAXTIME -w %{http_code}:%{time_total} "$ATLAS_NAGIOS_URL" | awk -F"." '{print $1}'`
+httpCode=`echo $response | awk -F":" '{print $1}'`
+responseTime=`echo $response | awk -F":" '{print $2}'`
+if [ "$responseTime" -ge "$MAXTIME" -o "$httpCode" -ne "200" ]; then
     touch $log
-    echo `eval date "+%Y-%m-%d\ %k:%M:%S"`" - Response time for $HOSTNAME exceeded $MAXTIME - bouncing $HOSTNAME..." >> $log
+    echo `eval date "+%Y-%m-%d\ %k:%M:%S"`" - Response time for $HOSTNAME exceeded $MAXTIME or httpCode: $httpCode <> 200 - bouncing $HOSTNAME..." >> $log
     ssh `whoami`@${HOSTNAME} /nfs/public/rw/webadmin/tomcat/bases/fg/tc-fg-gxa/bin/stop
     ssh `whoami`@${HOSTNAME} /nfs/public/rw/webadmin/tomcat/bases/fg/tc-fg-gxa/bin/start
     curl -o /dev/null -s -I 'http://${HOSTNAME}:8080/gxa/fval?q=brca&factor=&type=gene&limit=15'
     curl -o /dev/null -s -I 'http://${HOSTNAME}:8080/gxa/fval?q=canc&factor=&type=efoefv&limit=15'
 fi
-
